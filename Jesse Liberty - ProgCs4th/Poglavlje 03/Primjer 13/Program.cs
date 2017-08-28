b53ccbbe4b3d3399c913cdd23c9f8e49655197b2
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 18:05:34 +0200

    primjer03-13

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_13
+{
+    class ForLoop
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", i);
+
+                if(i%10 == 0)
+                {
+                    Console.WriteLine("\t{0}", i);
+                }
+            }
+
+            return;
+        }
+    }
+}
