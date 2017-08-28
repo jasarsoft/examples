c304ee16097cbef8e8feaa0f0180af5e1f45f6c6
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:29:06 +0200

    primjer03-04

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_04
+{
+    class SymbolicConstants
+    {
+        static void Main(string[] args)
+        {
+            const int FreezingPoint = 32; //stupnjevi Fehrenheita
+            const int BoilingPoint = 212;
+
+            System.Console.WriteLine("Freezing point of water: {0}", FreezingPoint);
+            System.Console.WriteLine("Boiling point of water: {0}", BoilingPoint);
+
+            //BoilingPoint = 21;
+        }
+    }
+}
