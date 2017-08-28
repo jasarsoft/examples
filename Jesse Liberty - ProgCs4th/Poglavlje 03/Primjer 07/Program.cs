36f9a954368193d5566a367ddf32d38250cf16af
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:43:00 +0200

    primjer03-07

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_07
+{
+    class Values
+    {
+        static void Main(string[] args)
+        {
+            int valueOne = 10;
+            int valueTwo = 20;
+
+            if(valueOne > valueTwo)
+            {
+                Console.WriteLine("ValueOne: {0} larger then ValueTwo: {1}", valueOne, valueTwo);
+            }
+            else
+            {
+                Console.WriteLine("ValueTwo: {0} larger then valueOne: {1}", valueTwo, valueOne);
+            }
+
+            valueOne = 30; //postavlje valueOne vise
+            if(valueOne > valueTwo)
+            {
+                valueTwo = valueOne++;
+                Console.WriteLine("\nSetting valueTwo to valueOne value, ");
+                Console.WriteLine("and increment valueOne.\n");
+                Console.WriteLine("ValueOne: {0} ValueTwo: {1}", valueOne, valueTwo);
+            }
+            else
+            {
+                valueOne = valueTwo;
+                Console.WriteLine("Setting them equal. ");
+                Console.WriteLine("valueOne: {0} ValueTwo: {1}", valueOne, valueTwo);
+            }
+        }
+    }
+}
