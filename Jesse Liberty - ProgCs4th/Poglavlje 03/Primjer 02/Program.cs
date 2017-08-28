96cc2132f5cdd8668623c03dbbbc8b5dd4250d71
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:24:34 +0200

    primjer03-02

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_02
+{
+    class UnitializedVariable
+    {
+        static void Main(string[] args)
+        {
+            int myInt;
+            System.Console.WriteLine("Uninitialized myInt: {0}", myInt);
+
+            myInt = 5;
+            System.Console.WriteLine("Assigned, myInt: {0}", myInt);
+        }
+    }
+}
