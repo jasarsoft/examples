82233fd820b76f98635ab4bc63ede175c2cf5ee9
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 22:16:16 +0200

    primjer03-18

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_18
+{
+    class TernaryOperator
+    {
+        static void Main(string[] args)
+        {
+            int valueOne = 10;
+            int valueTwo = 20;
+
+            int valueMax = valueOne > valueTwo ? valueOne : valueTwo;
+
+            Console.WriteLine("ValueOne: {0}, ValueTwo: {1}, ValueMax: {2}", valueOne, valueTwo, valueMax);
+        }
+    }
+}
