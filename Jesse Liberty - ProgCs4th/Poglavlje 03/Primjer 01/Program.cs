c828bed3f19ad8340cb0b89dceebb432faaa1d6c
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 15:48:47 +0200

    primjer03-01

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace InitializingVariables
+{
+    class Values
+    {
+        static void Main()
+        {
+            int myInt = 7;
+            System.Console.WriteLine("Initialized, myInt: {0}", myInt);
+
+            myInt = 5;
+            System.Console.WriteLine("Affter assigment, myInt: {0}", myInt);
+        }
+    }
+}
