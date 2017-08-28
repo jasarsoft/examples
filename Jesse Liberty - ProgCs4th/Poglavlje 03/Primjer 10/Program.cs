cd979968ff7378a5461143bd0d0204881fcc2cfd
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:58:15 +0200

    primjer03-10

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_10
+{
+    class UsingGoto
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            repeat: //oznaka
+            Console.WriteLine("i: {0}", i);
+            i++;
+            if (i < 10)
+                goto repeat; //zlodjelo
+            return;
+        }
+    }
+}
