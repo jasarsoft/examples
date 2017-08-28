1924a273ebb30a481b927bd50ab386124a281ad0
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 18:00:24 +0200

    primjer03-11

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_11
+{
+    class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 10)
+            {
+                Console.WriteLine("i: {0}", i);
+                i++;
+            }
+            return;
+        }
+    }
+}
