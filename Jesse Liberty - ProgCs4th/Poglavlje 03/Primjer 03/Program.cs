437ff55110195c4d07a7e7ca0d7572c3cc40ecd4
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:26:40 +0200

    primjer03-03

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_03
+{
+    class AssigningWithoutInitializing
+    {
+        static void Main(string[] args)
+        {
+            int myInt;
+            myInt = 7;
+            System.Console.WriteLine("Assigned myInt: {0}", myInt);
+
+            myInt = 5;
+            System.Console.WriteLine("Assigned myInt: {0}", myInt);
+        }
+    }
+}
