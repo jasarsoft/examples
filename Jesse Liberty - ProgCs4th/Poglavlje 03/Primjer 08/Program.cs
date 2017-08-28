56f78436ffda1ead862ca9b79cf16b7e407e5aa2
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:46:49 +0200

    primjer03-08

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_08
+{
+    class NestedIf
+    {
+        static void Main(string[] args)
+        {
+            int temp = 32;
+
+            if(temp <= 32)
+            {
+                Console.WriteLine("Warning! Ice on road.");
+                if(temp == 32)
+                {
+                    Console.WriteLine("temp exactly freezing, beware of water.");
+                }
+                else
+                {
+                    Console.WriteLine("Watch for back ice! Temp: {0}", temp);
+                }//zavrsava else
+            }//zavrsava if (temp <= 32)
+        }//zavrsava main
+    }//zavrsava class
+}//zavrsava namespace
