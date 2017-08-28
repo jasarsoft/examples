40de254e5fc24783368afd7bb0e2522edfcda892
Edin Jašarević <edinjasar14@gmail.com>
2017-08-29 01:16:11 +0200

    primjer04-02

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Primjer 02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PassingValues
+{
+    public class MyClass
+    {
+        public void SomeMethod(int firstParam, float secondParam)
+        {
+            Console.WriteLine("Here are the parameters received: {0}, {1}", firstParam, secondParam);
+        }
+    }
+
+    class Tester
+    {
+        static void Main()
+        {
+            int howManyPeople = 5;
+            float pi = 3.14F;
+
+            MyClass mc = new MyClass();
+            mc.SomeMethod(howManyPeople, pi);
+        }
+    }
+}
