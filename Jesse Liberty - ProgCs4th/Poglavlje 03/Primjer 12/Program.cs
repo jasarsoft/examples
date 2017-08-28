2a9b45ac89551882ac3539a7a01340bbeac1b8a9
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 18:02:50 +0200

    primjer03-12

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_12
+{
+    class DoWhile
+    {
+        static void Main(string[] args)
+        {
+            int i = 11;
+            do
+            {
+                Console.WriteLine("i: {0}", i);
+                i++;
+            } while (i < 10);
+
+            return;
+        }
+    }
+}
