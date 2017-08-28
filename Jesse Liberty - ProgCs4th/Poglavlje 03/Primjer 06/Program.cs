f29d08b0150eb79e12130915eafc23299c9f609b
Edin Jašarević <edinjasar14@gmail.com>
2017-08-28 17:34:56 +0200

    primjer03-06

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 03/Primjer 06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_06
+{
+    class CallingAMethod
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In Main! Calling SomeMethod()...");
+            SomeMethod();
+            Console.WriteLine("Back in Main()");
+        }
+
+        static void SomeMethod()
+        {
+            Console.WriteLine("Grettings form SomeMethod!");
+        }
+    }
+}
