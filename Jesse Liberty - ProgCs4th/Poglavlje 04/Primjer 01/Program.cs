389dd9fa7a7f3926054102b7b56af23a6e273075
Edin Jašarević <edinjasar14@gmail.com>
2017-08-29 01:10:36 +0200

    primjer04-01

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Primjer 01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_01
+{
+    public class Time
+    {
+        //privatne varijable
+        int Year;
+        int Month;
+        int Date;
+        int Hour;
+        int Minute;
+        int Second;
+
+        //javne metode
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine("stub for DisplayCurrentTime");
+        }
+    }
+
+    public class Tester
+    {
+        static void Main()
+        {
+            Time t = new Time();
+            t.DisplayCurrentTime();
+        }
+    }
+}
