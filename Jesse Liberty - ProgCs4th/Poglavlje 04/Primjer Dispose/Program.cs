7197d1abfc10f2cbeed3623ac651872072a10c73
Edin Jašarević <edinjasar14@gmail.com>
2017-08-29 02:16:21 +0200

    primjer04-Dispose

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Primjer Dispose/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_Dispose
+{
+    class Testing : IDisposable
+    {
+        bool is_disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!is_disposed)//odlaze samo jednom
+
+            {
+                if(disposing)
+                {
+                    Console.WriteLine("Not in descructor, OK to reference other object");
+                }
+                //izvodi ciscenje za ovja objekat
+                Console.WriteLine("Disposing...");
+            }
+
+            this.is_disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            //govori sakupljacu otpada da ne finalizira
+            GC.SuppressFinalize(this);
+        }
+
+        ~Testing()
+        {
+            Dispose(false);
+            Console.WriteLine("In desctructor.");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Testing t = new Testing())
+            {
+
+            }
+            Console.WriteLine("Main");
+        }
+    }
+}
