9b072ca4c051c64de0bee8bd4044eca600e08acf
Edin Jašarević <edinjasar14@gmail.com>
2017-08-29 17:20:49 +0200

    primjer04-07

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Primjer 07/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReturningValuesInParams
+{
+    public class Time
+    {
+        //privatne varijable clanice
+        private int Year;
+        private int Month;
+        private int Date;
+        private int Hour;
+        private int Minute;
+        private int Second;
+
+        //javne metode za pristupanje
+        public void DisplayCurrentTime()
+        {
+            System.Console.WriteLine("{0}/{1}/{2} {3}:{4}:{5}", Month, Date, Year, Hour, Minute, Second);
+        }
+
+        public int GetHour()
+        {
+            return Hour;
+        }
+
+        public void GetTime(ref int h, ref int m, ref int s)
+        {
+            h = Hour;
+            m = Minute;
+            s = Second;
+        }
+
+        //konstruktor
+        public Time(System.DateTime dt)
+        {
+            Year = dt.Year;
+            Month = dt.Month;
+            Date = dt.Day;
+            Hour = dt.Hour;
+            Minute = dt.Minute;
+            Second = dt.Second;
+        }
+    }
+
+    class Tester
+    {
+        static void Main()
+        {
+            System.DateTime currentTime = System.DateTime.Now;
+            Time t = new Time(currentTime);
+            t.DisplayCurrentTime();
+
+            int theHour = 0;
+            int theMinute = 0;
+            int theSecond = 0;
+            t.GetTime(ref theHour, ref theMinute, ref theSecond);
+            System.Console.WriteLine("Current time: {0}:{1}:{2}", theHour, theMinute, theSecond);
+        }
+    }
+}
