2d28220721942d8d75f48ef4c312be350a85aa98
Edin Jašarević <edinjasar14@gmail.com>
2017-08-29 18:03:54 +0200

    primjer04-12

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Poglavlje 12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StaticPublicConstants
+{
+    public class RightNow
+    {
+        //javne varijable clanice
+        public static readonly int Year;
+        public static readonly int Month;
+        public static readonly int Date;
+        public static readonly int Hour;
+        public static readonly int Minute;
+        public static readonly int Second;
+
+        static RightNow()
+        {
+            DateTime dt = DateTime.Now;
+            Year = dt.Year;
+            Month = dt.Month;
+            Date = dt.Day;
+            Hour = dt.Hour;
+            Minute = dt.Minute;
+            Second = dt.Second;
+        }
+    }
+
+    class Tester
+    {
+        static void Main()
+        {
+            Console.WriteLine("This year: {0}", RightNow.Year.ToString());
+            //RightNow.Year = 2006;
+            Console.WriteLine("This year: {0}", RightNow.Year.ToString());
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Primjer 11/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingAProperty
+{
+    public class Time
+    {
+        //privatne varijable clanice
+        private int year;
+        private int month;
+        private int date;
+        private int hour;
+        private int minute;
+        private int second;
+
+        //javne metode za pristupanje
+        public void DisplayCurrentTime()
+        {
+            Console.WriteLine("Time\t: {0}/{1}/{2} {3}:{4}:{5}", month, date, year, hour, minute, second);
+        }
+
+        //konstruktori
+        public Time(DateTime dt)
+        {
+            year = dt.Year;
+            month = dt.Month;
+            date = dt.Day;
+            hour = dt.Hour;
+            minute = dt.Minute;
+            second = dt.Second;
+        }
+
+        //svojstva
+        public int Hour
+        {
+            get
+            {
+                return hour;
+            }
+            set
+            {
+                hour = value;
+            }
+        }
+    }
+
+    class Tester
+    {
+        static void Main()
+        {
+            DateTime currentTime = DateTime.Now;
+            Time t = new Time(currentTime);
+            t.DisplayCurrentTime();
+
+            int theHour = t.Hour;
+            Console.WriteLine("\nRetrived the hour: {0}\n", theHour);
+
+            theHour++;
+            t.Hour = theHour;
+            Console.WriteLine("Update the hour: {0}\n", theHour);
+        }
+    }
+}
