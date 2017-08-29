c352c5e07aae0d24da0b90903a155662e5e6cbff
Edin Jašarević <edinjasar14@gmail.com>
2017-08-29 17:44:18 +0200

    primjer04-10

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 04/Primjer 10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VaryingReturnType
+{
+    class Tester
+    {
+        private int Triple(int val)
+        {
+            return 3 * val;
+        }
+
+        private long Triple(long val)
+        {
+            return 3 * val;
+        }
+
+        public void Test()
+        {
+            int x = 5;
+            int y = Triple(x);
+            Console.WriteLine("x: {0}  y: {1}", x, y);
+
+            long lx = 10;
+            long ly = Triple(lx);
+            Console.WriteLine("lx:{0} y:{1}", lx, ly);
+        }
+
+        static void Main()
+        {
+            Tester t = new Tester();
+            t.Test();
+        }
+    }
+}
