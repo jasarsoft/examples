7c73ebaad470019e1c48af9d35b176e0beb30966
Edin Jašarević <edinjasar14@gmail.com>
2017-08-31 14:33:09 +0200

    primjer05-05

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 05/Primjer 05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NestedClass
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int num, int den)
+        {
+            this.numerator = num;
+            this.denominator = den;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}/{1}", numerator, denominator);
+        }
+
+        internal class FractionArtist
+        {
+            public void Draw(Fraction f)
+            {
+                Console.WriteLine("Drawing the numerator: {0}", f.numerator);
+                Console.WriteLine("Drawing the denominator: {0}", f.denominator);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Fraction f1 = new Fraction(3, 4);
+            Console.WriteLine("f1: {0}", f1.ToString());
+
+            Fraction.FractionArtist fa = new Fraction.FractionArtist();
+            fa.Draw(f1);
+        }
+    }
+}
