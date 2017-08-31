87bdeedd5aaf8f7df722414549d5a262ac839ab2
Edin Jašarević <edinjasar14@gmail.com>
2017-08-31 14:23:47 +0200

    primjer05-04

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 05/Primjer 04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 123;
+
+            //pakiranje (implicitno)
+            object o = i;
+
+            //raspakiravanje (mora biti eksplicitno)
+            int j = (int)o;
+            Console.WriteLine("j: {0}", j);
+        }
+    }
+}
