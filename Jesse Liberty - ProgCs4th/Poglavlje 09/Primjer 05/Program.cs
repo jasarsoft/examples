7d2bb61a07c6c4866f5ffa2789392c41082b7124
Edin Jašarević <edinjasar14@gmail.com>
2017-09-07 22:19:01 +0200

    primjer09-05

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InitializingMultiDimensionalArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int rows = 4;
+            const int columns = 3;
+
+            //zakljucuje da je rijec o polju 4x3;
+            int[,] rectangularArray =
+            {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}
+            };
+
+            //ispisuje sadrzaj polja
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine("rectangularArray[{0},{1}] = {2}", i, j, rectangularArray[i, j]);
+                }
+            }
+        }
+    }
+}
