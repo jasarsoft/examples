9f9b359f6aabc128efb9278c79786ea2a5eb8869
Edin Jašarević <edinjasar14@gmail.com>
2017-09-07 21:54:33 +0200

    primjer09-01

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 01/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_01
+{
+    public class Employee
+    {
+        private int empID;
+
+        public Employee(int empID)
+        {
+            this.empID = empID;
+        }
+
+        public override string ToString()
+        {
+            return empID.ToString();
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] intArray;
+            Employee[] empArray;
+
+            intArray = new int[5];
+            empArray = new Employee[3];
+
+            //ispunjava polje
+            for (int i = 0; i < empArray.Length - 1; i++)
+            {
+                empArray[i] = new Employee(i + 5);
+            }
+
+            for(int i = 0; i < intArray.Length - 1; i++)
+            {
+                Console.WriteLine(intArray[i].ToString());
+            }
+
+            for(int i = 0; i < empArray.Length - 1; i++)
+            {
+                Console.WriteLine(empArray[i].ToString());
+            }
+        }
+    }
+}
