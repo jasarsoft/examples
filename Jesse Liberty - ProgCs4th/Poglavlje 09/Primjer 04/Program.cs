b2b7da7074ff0c6d6bacfb33752d79e7dbc1f24d
Edin Jašarević <edinjasar14@gmail.com>
2017-09-07 22:15:29 +0200

    primjer09-04

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RectangularArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int rows = 4;
+            const int columns = 3;
+
+            //deklarira cjelobrojno poslje 4x3
+            int[,] rectangularArray = new int[rows, columns];
+
+            //ispunjava polje
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    rectangularArray[i, j] = i + j;
+                }
+            }
+
+            //ispisuje sadrzaj polja
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine("rectangularArray[{0},{1}] = {2}", i, j, rectangularArray[i, j]);
+                }
+            }
+        }
+    }
+}
