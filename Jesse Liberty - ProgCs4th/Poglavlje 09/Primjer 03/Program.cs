2fa034dc037d0dd37a3d4bb2489122f3eda17d03
Edin Jašarević <edinjasar14@gmail.com>
2017-09-07 22:09:08 +0200

    primjer09-03

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingParams
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program p = new Program();
+            p.DisplayVals(5, 6, 7, 8);
+            int[] explicitArray = new int[5] { 1, 2, 3, 4, 5 };
+            p.DisplayVals(explicitArray);
+        }
+
+        public void DisplayVals(params int[] intVals)
+        {
+            foreach(int i in intVals)
+            {
+                Console.WriteLine("DisplayVals {0}", i);
+            }
+        }
+    }
+}
