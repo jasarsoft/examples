8d6ef1f44ba6a6cdd4f004bc6bd59480f023708c
Edin Jašarević <edinjasar14@gmail.com>
2017-09-07 23:32:34 +0200

    primjer09-08

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertingArrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            //pravi polje Employee objektata
+            Employee[] myEmployeeArray = new Employee[3];
+
+            //inicijalizira vrijednost svakog Employee
+            for(int i = 0; i < 3; i++)
+            {
+                myEmployeeArray[i] = new Employee(i + 5);
+            }
+
+            //prikazuje vrijednosti
+            Tester.PrintArray(myEmployeeArray);
+
+            //pravi polje od dva niza
+            string[] array =
+            {
+                "hello", "world"
+            };
+
+            //ispisuje vrijednost nizova
+            Tester.PrintArray(array);
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 08/Tester.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertingArrays
+{
+    public class Tester
+    {
+        //ova metoda uzima polje objekata
+        //proslijedit cemo polje Employee a zatim polje nizova
+        //pretvorba je implicitna jer i Employee i nizovi izvdeni iz objekta
+        public static void PrintArray(object[] theArray)
+        {
+            Console.WriteLine("Contents of the Array {0}", theArray.ToString());
+            
+            //prolazi kroz petlje i ispisuje vrijednost
+            foreach(object obj in theArray)
+            {
+                Console.WriteLine("Value: {0}", obj);
+            }
+        }
+    }
+}
