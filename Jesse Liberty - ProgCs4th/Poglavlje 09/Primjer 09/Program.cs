5511b0a7e09db9f3a2e87881c764b91ed35c11ba
Edin Jašarević <edinjasar14@gmail.com>
2017-09-07 23:56:56 +0200

    primjer09-09

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 09/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraySortAndReverse
+{
+    class Program
+    {
+        static void Main()
+        {
+            String[] myArray =
+            {
+                "Who", "is", "John", "Galt"
+            };
+
+            Tester.PrintArray(myArray);
+            Array.Reverse(myArray);
+            Tester.PrintArray(myArray);
+
+            String[] myOtherArray =
+            {
+                "We", "Hold", "These", "Truths",
+                "To", "Be", "Self", "Evident"
+            };
+
+            Console.WriteLine();
+            Tester.PrintArray(myOtherArray);
+            Array.Sort(myOtherArray);
+            Tester.PrintArray(myOtherArray);
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 09/Tester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraySortAndReverse
+{
+    public class Tester
+    {
+        public static void PrintArray(object[] theArray)
+        {
+            foreach(object obj in theArray)
+            {
+                Console.WriteLine("Value: {0}", obj);
+            }
+        }
+    }
+}
