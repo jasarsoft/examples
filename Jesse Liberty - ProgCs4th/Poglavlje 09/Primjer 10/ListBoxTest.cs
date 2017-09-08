6644907b01bbd890b894176aacd390e159edce8a
Edin Jašarević <edinjasar14@gmail.com>
2017-09-08 22:06:42 +0200

    primjer09-10

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 10/ListBoxTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleIndexer
+{
+    public class ListBoxTest
+    {
+        private string[] strings;
+        private int ctr = 0;
+
+        //inicijalizira popis sa nizovima
+        public ListBoxTest(params string[] initialString)
+        {
+            //dodjeljuje prostor za nizove
+            strings = new string[256];
+
+            //kopira nizove proslijedjene konstruktoru
+            foreach(string s in initialString)
+            {
+                strings[ctr++] = s;
+            }
+        }
+
+        //dodaje niz na kraju popisa
+        public void Add(string theString)
+        {
+            if (ctr >= strings.Length)
+            {
+                //obradjuje los index
+            }
+            else
+                strings[ctr++] = theString;
+        }
+
+        //dozvoljava pristup kao kod polja
+        public string this[int index]
+        {
+            get
+            {
+                if(index < 0 || index >= strings.Length)
+                {
+                    //obradjuje los index
+                }
+
+                return strings[index];
+            }
+            set
+            {
+                //dodaje samo kroz metod za dodavanje
+                if (index >= ctr)
+                {
+                    //obradjuje pogreske
+                }
+                else
+                    strings[index] = value;
+            }
+        }
+
+        //objavljuje koliko ima nizova
+        public int GetNumEntries()
+        {
+            return ctr;
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleIndexer
+{
+    class Program
+    {
+        static void Main()
+        {
+            //pravi novi popis i inicijalizira ga
+            ListBoxTest lbt = new ListBoxTest("Hello", "World");
+
+            //dodaje nekoliko nizova
+            lbt.Add("Who");
+            lbt.Add("Is");
+            lbt.Add("John");
+            lbt.Add("Galt");
+
+            //testira pristup
+            string subst = "Universe";
+            lbt[1] = subst;
+
+            //pristupa svim nizovima
+            for(int i = 0; i < lbt.GetNumEntries(); i++)
+            {
+                Console.WriteLine("lbt[{0}]: {1}", i, lbt[i]);
+            }
+        }
+    }
+}
