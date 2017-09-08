7362bb2efb93bcb0068f4b8f7128096892edcf5a
Edin Jašarević <edinjasar14@gmail.com>
2017-09-08 22:34:29 +0200

    primjer09-11

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 11/ListBoxTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OverloadedIndexer
+{
+    //pojednostavljena ListBox kontrola
+    public class ListBoxTest
+    {
+        private string[] strings;
+        private int ctr = 0;
+
+        //inicijalizira popis sa nizovima
+        public ListBoxTest(params string[] initialString)
+        {
+            //dodjeljuje prostor za nizove
+            strings = new string[256];
+
+            //kopira nizove proslijedjene konstruktoru
+            foreach(string s in initialString)
+            {
+                strings[ctr++] = s;
+            }
+        }
+
+        //dodaje niz na kraju popisa
+        public void Add(string theString)
+        {
+            strings[ctr] = theString;
+            ctr++;
+        }
+
+        //dozvoljava pristup kao kod polja
+        public string this[int index]
+        {
+            get
+            {
+                if(index < 0 || index >= strings.Length)
+                {
+                    //obradjuje los index
+                }
+
+                return strings[index];
+            }
+            set
+            {
+                strings[index] = value;
+            }
+        }
+
+        private int findString(string searchString)
+        {
+            for(int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].StartsWith(searchString))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        //indeksiranje nizom
+        public string this[string index]
+        {
+            get
+            {
+                if(index.Length == 0){ /*obradjuje los index*/}
+                return this[findString(index)];
+            }
+            set
+            {
+                strings[findString(index)] = value;
+            }
+        }
+
+        //objavljuje koliko ima nizova
+        public int GetNumEntries()
+        {
+            return ctr;
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OverloadedIndexer
+{
+    class Program
+    {
+        static void Main()
+        {
+            //pravi novi popis i inicijalizira ga
+            ListBoxTest lbt = new ListBoxTest("Hello", "World");
+
+            //dodaje nekoliko nizova
+            lbt.Add("Who");
+            lbt.Add("Is");
+            lbt.Add("John");
+            lbt.Add("Galt");
+
+            //testira pristup
+            string subst = "Universe";
+            lbt[1] = subst;
+            lbt["Hel"] = "GoodBay";
+            //lbt["xyz"]= "oops";
+
+            //pristupa svim nizovima
+            for (int i = 0; i < lbt.GetNumEntries(); i++)
+            {
+                Console.WriteLine("lbt[{0}]: {1}", i, lbt[i]);
+            }
+        }
+    }
+}
