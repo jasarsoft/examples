d58b8e6f44a54321d8a62bb6a4f7f71babea3900
Edin Jašarević <edinjasar14@gmail.com>
2017-09-09 00:52:53 +0200

    primjer09-14

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 14/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListCollection
+{
+    //jednostavna klasa koja ce se cuvati u List
+    class Employee
+    {
+        private int empID;
+
+        public Employee(int empID)
+        {
+            this.empID = empID;
+        }
+
+        public override string ToString()
+        {
+            return empID.ToString();
+        }
+
+        public int EmpID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListCollection
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> intList = new List<int>();
+            List<Employee> empList = new List<Employee>();
+
+            //popunjava List
+            for (int i = 0; i < 5; i++)
+            {
+                empList.Add(new Employee(i + 100));
+                intList.Add(i * 5);
+            }
+
+            //ispisuje sav sadrzaj
+            for(int i = 0; i < intList.Count; i++)
+            {
+                Console.Write("{0} ", intList[i].ToString());
+            }
+
+            Console.WriteLine();
+
+            //ispisuje sav sadzaj Employee List
+            for(int i = 0; i < empList.Count; i++)
+            {
+                Console.Write("{0} ", empList[i].ToString());
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("empList.Capacity: {0}", empList.Capacity);
+        }
+    }
+}
