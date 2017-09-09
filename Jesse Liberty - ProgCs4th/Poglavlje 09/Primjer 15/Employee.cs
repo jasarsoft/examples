823999565a4e58998459cc61743e3c2f60f5b04b
Edin Jašarević <edinjasar14@gmail.com>
2017-09-09 02:33:56 +0200

    primjer09-15

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 15/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IComperable
+{
+    //jednostavna klasa koja ce se cuvati u List
+    class Employee : IComparable<Employee>
+    {
+        private int empID;
+
+        public Employee(int empID)
+        {
+            this.empID = empID;
+        }
+
+        public int EmpID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public override string ToString()
+        {
+            return empID.ToString();
+        }
+
+        public bool Equals(Employee other)
+        {
+            if (this.empID == other.empID)
+                return true;
+            else
+                return false;
+        }
+
+        //CompareTo delegira natrag do employee
+        //Employee koristi podrazumjevnu metodu CompareTo cjelobrojnih vrijednosti
+        public int CompareTo(Employee rhs)
+        {
+            return this.empID.CompareTo(rhs.empID);
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 15/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IComperable
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Int32> intArray = new List<int>();
+            List<Employee> empArray = new List<Employee>();
+
+            //generira slucajne brojeve za cjelobrojne vrijednosti
+            //i identifikatore zaposlenika
+            Random r = new Random();
+
+            //popunjava polje
+            for(int i = 0; i < 5; i++)
+            {
+                //dodaje slucajni indentifikator zaposlenika
+                empArray.Add(new Employee(r.Next(10) + 100));
+
+                //dodaje slucajnu cjelobrojnu vrijednost
+                intArray.Add(r.Next(10));
+            }
+
+            //prikazuje sav sadrzaj polja cjelobrojnih vrijednosti
+            for(int i = 0; i < intArray.Count; i++)
+            {
+                Console.Write("{0} ", intArray[i].ToString());
+            }
+            Console.WriteLine();
+
+            //prikazuje sav sadzaj polja Employee
+            for(int i = 0; i < empArray.Count; i++)
+            {
+                Console.Write("{0} ", empArray[i].ToString());
+            }
+            Console.WriteLine("\n");
+
+            //sortira i prikazuje polje cjelobrojnih vrijednosti
+            intArray.Sort();
+            for(int i = 0; i < intArray.Count; i++)
+            {
+                Console.Write("{0} ", intArray[i].ToString());
+            }
+            Console.WriteLine();
+
+            //sortira i prikazuje polje zapozlenika
+            empArray.Sort();
+            for(int i = 0; i < empArray.Count; i++)
+            {
+                Console.Write("{0} ", empArray[i].ToString());
+            }
+            Console.WriteLine();
+        }
+    }
+}
