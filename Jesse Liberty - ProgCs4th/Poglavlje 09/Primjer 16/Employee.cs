12d59a6f8f7e1d5df6f8b9e7b8ee62feb7770f43
Edin Jašarević <edinjasar14@gmail.com>
2017-09-10 22:52:15 +0200

    primjer09-16

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 16/Employee.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IComparer
+{
+    public class Employee : IComparable<Employee>
+    {
+        private int empID;
+        private int yearsOfSvc = 1;
+
+        public Employee(int empID)
+        {
+            this.empID = empID;
+        }
+
+        public Employee(int empID, int yearsOfSvc)
+        {
+            this.empID = empID;
+            this.yearsOfSvc = yearsOfSvc;
+        }
+
+        public int EmpID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + empID.ToString() + ". Years of Svc: " + yearsOfSvc;
+        }
+
+        public bool Equals(Employee other)
+        {
+            if (this.empID == other.empID)
+                return true;
+            else
+                return false;
+        }
+
+        //staticka metoda za uzimanje objekta Comparer
+        public static EmployeeComparer GetComparer()
+        {
+            return new Employee.EmployeeComparer();
+        }
+
+        //CompareTo delegira natrag employee
+        //Employee koristi podrazumjevnu metodu CompareTo cjelobrojnih vrijednosti
+        public int CompareTo(Employee rhs)
+        {
+            return this.empID.CompareTo(rhs.empID);
+        }
+
+        //posebna implementacija koju ce pozvati prilagodjeni usporedjivac
+        public int CompareTo(Employee rhs, EmployeeComparer.ComparasionType which)
+        {
+            switch(which)
+            {
+                case Employee.EmployeeComparer.ComparasionType.EmpID:
+                    return this.empID.CompareTo(rhs.empID);
+                case EmployeeComparer.ComparasionType.Yrs:
+                    return this.yearsOfSvc.CompareTo(rhs.yearsOfSvc);
+            }
+
+            return 0;
+        }
+
+        //ugnjezdena klasa koja implementira IComparer
+        public class EmployeeComparer : IComparer<Employee>
+        {
+            //privatna varijabla stanja
+            private ComparasionType whichComparasion;
+
+            //enumeracija tipova za usporedjivanje
+            public enum ComparasionType
+            {
+                EmpID,
+                Yrs
+            };
+
+
+            public ComparasionType WhichComparasion
+            {
+                get { return whichComparasion; }
+                set { whichComparasion = value; }
+            }
+
+
+            public bool Equals(Employee lhs, Employee rhs)
+            {
+                return this.Compare(lhs, rhs) == 0;
+            }
+
+            public int GetHashCode(Employee e)
+            {
+                return e.GetHashCode();
+            }
+
+            //govori objektima employee da se usporede
+            public int Compare(Employee lhs, Employee rhs)
+            {
+                return lhs.CompareTo(rhs, whichComparasion);
+            } 
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 16/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IComparer
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> empArray = new List<Employee>();
+
+            //generira slucajne brojeve za cijelobrojne vrijednosti 
+            //i identifikatore zaposlenika
+            Random r = new Random();
+
+            //popunjava polje
+            for (int i = 0; i < 5; i++)
+            {
+                empArray.Add(new Employee(r.Next(10) + 100, r.Next(20)));
+            }
+
+            //prikazuje sad sadrzaj polja Employee
+            for(int i = 0; i < empArray.Count; i++)
+            {
+                Console.Write("\n{0} ", empArray[i].ToString());
+            }
+            Console.WriteLine("\n");
+
+            Employee.EmployeeComparer c = Employee.GetComparer();
+            c.WhichComparasion = Employee.EmployeeComparer.ComparasionType.EmpID;
+            empArray.Sort(c);
+
+            //prikazuje sav sadrzaj Employee
+            for (int i = 0; i < empArray.Count; i++)
+            {
+                Console.Write("\n{0} ", empArray[i].ToString());
+            }
+            Console.WriteLine("\n");
+
+            c.WhichComparasion = Employee.EmployeeComparer.ComparasionType.Yrs;
+            empArray.Sort(c);
+
+            for (int i = 0; i < empArray.Count; i++)
+            {
+                Console.Write("\n{0} ", empArray[i].ToString());
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
