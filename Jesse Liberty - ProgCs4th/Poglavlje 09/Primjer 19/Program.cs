a91ca9dd7e070e6fbf64cdd31ec35d707bf7b556
Edin Jašarević <edinjasar14@gmail.com>
2017-09-11 01:49:30 +0200

    primjer09-19

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 09/Primjer 19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            //pravi i inicjalizira novi rjecnik
+            Dictionary<string, string> Dictionary = new Dictionary<string, string>();
+
+            Dictionary.Add("000440312", "Jesse Liberty");
+            Dictionary.Add("000123949", "Stacey Liberty");
+            Dictionary.Add("000145938", "John Galt");
+            Dictionary.Add("000773394", "Ayn Rand");
+
+            //pristupa zadnjoj stavci
+            Console.WriteLine("myDictonary[\"000145938\"]: {0}", Dictionary["000145938"]);
+        }
+    }
+}
