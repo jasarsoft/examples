6c00a0d013fce7206581b445ed49df22cf619fff
Edin Jašarević <edinjasar14@gmail.com>
2017-09-11 20:50:44 +0200

    primjer10-05

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 10/Primjer 05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UsingRegEx
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s1 = "One,Two,Three Liberty Associates, Inc";
+
+            Regex theRegex = new Regex(" |, |,");
+            StringBuilder sBuilder = new StringBuilder();
+            int id = 1;
+
+            foreach(string substring in theRegex.Split(s1))
+            {
+                sBuilder.AppendFormat("{0}: {1}\n", id++, substring);
+            }
+
+            Console.WriteLine("{0}", sBuilder);
+        }
+    }
+}
