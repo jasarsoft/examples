545e9cfb733cf2282599d97c297479ebfe4540ec
Edin Jašarević <edinjasar14@gmail.com>
2017-09-11 20:53:34 +0200

    primjer10-06

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 10/Primjer 06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexSplit
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s1 = "One,Two,Three Liberty Associates, Inc";
+
+            int id = 1;
+            StringBuilder sBuilder = new StringBuilder();
+            
+            foreach (string substring in Regex.Split(s1, " |, |,"))
+            {
+                sBuilder.AppendFormat("{0}: {1}\n", id++, substring);
+            }
+
+            Console.WriteLine("{0}", sBuilder);
+        }
+    }
+}
