b04201bb63b959c5669b900befe25783f0028686
Edin Jašarević <edinjasar14@gmail.com>
2017-09-11 19:58:29 +0200

    primjer10-03

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 10/Primjer 03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringSplit
+{
+    class Program
+    {
+        static void Main()
+        {
+            //prave nizove s kojim cemo raditi
+            string s1 = "One,Two,Three Liberty Associates, Inc.";
+
+            //konstante za znakove razmak i zarez
+            const char Space = ' ';
+            const char Comma = ',';
+
+            //polje granicnika za djeljenje recenice
+            char[] delimeters = new char[]
+            {
+                Space,
+                Comma
+            };
+
+            string output = "";
+            int ctr = 1;
+
+            //dijeli niz i zatim prolazi kroz rezultujuce polje nizova
+            foreach(string substring in s1.Split(delimeters))
+            {
+                output += ctr++;
+                output += ": ";
+                output += substring;
+                output += "\n";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
