b7e220fb624908635c64747b307e88980edf42ab
Edin Jašarević <edinjasar14@gmail.com>
2017-09-11 21:18:30 +0200

    primjer10-07

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 10/Primjer 07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UsingMatchCollection
+{
+    class Program
+    {
+        static void Main()
+        {
+            string string1 = "This is a test string";
+
+            //trazi bilo koji znak iza kojeg slijedi bijeli prostor
+            // \S - pronalazi znak koji nije bjelina, + oznacava jedan ili vise
+            // \s - oznacava bijelinu, poslije poslije skupa slova nadjenih prvim regularnim izrazom
+            Regex theReg = new Regex(@"(\S+)\s");
+
+            //uzima kolekciju pronadjenih poklapanja
+            MatchCollection theMatchs = theReg.Matches(string1);
+
+            //prolazi kroz kolekciju
+            foreach(Match theMatch in theMatchs)
+            {
+                Console.WriteLine("theMatch.Lenght: {0}", theMatch.Length);
+                if (theMatch.Length != 0)
+                    Console.WriteLine("theMatch: {0}", theMatch.ToString());
+            }
+        }
+    }
+}
