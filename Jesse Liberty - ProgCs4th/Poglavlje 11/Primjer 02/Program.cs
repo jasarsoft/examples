41b4135fc06e1b7818cd1b796ed5b13b2aa378c0
Edin Jašarević <edinjasar14@gmail.com>
2017-09-13 16:48:14 +0200

    primjer11-02

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 11/Primjer 02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatchingAnException
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Main...");
+            Program p = new Program();
+            p.Func1();
+            Console.WriteLine("Exit Main...");
+        }
+
+        public void Func1()
+        {
+            Console.WriteLine("Enter Func1...");
+            Func2();
+            Console.WriteLine("Exit Func1...");
+        }
+
+        public void Func2()
+        {
+            Console.WriteLine("Enter Func2...");
+            try
+            {
+                Console.WriteLine("Enter try block...");
+                throw new System.Exception();
+                Console.WriteLine("Exiting try block...");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception caught and handled.");
+            }
+            
+            Console.WriteLine("Exit Func2...");
+        }
+    }
+}
