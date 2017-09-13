6f865a0c55d13e15d17563ae6e8dc5a46974ba51
Edin Jašarević <edinjasar14@gmail.com>
2017-09-13 19:47:53 +0200

    primjer11-08

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 11/Primjer 08/MyCustomException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RethrowingExceptions
+{
+    public class MyCustomException : ApplicationException
+    {
+        public MyCustomException(string message, Exception inner) : base(message, inner)
+        {
+            
+        }
+    }
+}
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 11/Primjer 08/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RethrowingExceptions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program p = new Program();
+            p.TestFunc();
+        }
+
+        //pokusaj djeljenja dva broja i obrade moguce iznimke
+        public void TestFunc()
+        {
+            try
+            {
+                DangerousFunc1();
+            }
+            //ako uhvati prilagodjenu iznimku ispisuje povjest iznimke
+            catch (MyCustomException e)
+            {
+                Console.WriteLine("Msg: {0}", e.Message);
+                Console.WriteLine("Retriving exception history...");
+                Exception inner = e.InnerException;
+                while(inner != null)
+                {
+                    Console.WriteLine("{0}", inner.Message);
+                    inner = inner.InnerException;
+                }
+            }
+        }
+
+        public void DangerousFunc1()
+        {
+            try
+            {
+                DangerousFunc2();
+            }
+            //ako ovdje ulovi iznimku, izbacuje prilagodjenu iznimku
+            catch (Exception e)
+            {
+                MyCustomException ex = new MyCustomException("E3 - Custom Exception Situation!", e);
+                throw ex;
+            }
+        }
+
+        public void DangerousFunc2()
+        {
+            try
+            {
+                DangerousFunc3();
+            }
+            //ako uhvati DivideByZeroException poduzima korektivne akcije i izbacuje opce iznimke
+            catch (DivideByZeroException e)
+            {
+                Exception ex = new Exception("E2 - Func2 caught divide by zero", e);
+                throw ex;
+            }
+        }
+
+        public void DangerousFunc3()
+        {
+            try
+            {
+                DangerousFunc4();
+            }
+            catch (System.ArithmeticException)
+            {
+                Console.WriteLine("Arihemtic excpetion caught in DF3, and rethrow...");
+                throw;
+            }
+            catch(System.Exception)
+            {
+                Console.WriteLine("Exception handled here.");
+            }
+        }
+
+        public void DangerousFunc4()
+        {
+            throw new DivideByZeroException("E1 - DivideByZero Excpetion");
+        }
+    }
+}
