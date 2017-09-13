985c82ff072819a90c44b2500c4b66446d89e4f5
Edin Jašarević <edinjasar14@gmail.com>
2017-09-13 17:46:07 +0200

    primjer11-05

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 11/Primjer 05/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingFinally
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program p = new Program();
+            p.TestFunc();
+        }
+
+        //pokusaj djeljenja dva broja i obrade moguce iznimke
+        public void TestFunc()
+        {
+            try
+            {
+                Console.WriteLine("Open file here");
+                double a = 5;
+                double b = 0;
+                Console.WriteLine("{0} / {1} = {2}", a, b, DoDivide(a, b));
+                Console.WriteLine("This line may or may not print");
+            }
+            //provo ide tip najcesce izvodjene iznimke
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZeroException caught!");
+            }
+            catch (System.ArithmeticException)
+            {
+                Console.WriteLine("ArithmeticException caught!");
+            }
+            //genericki tip iznimke
+            catch
+            {
+                Console.WriteLine("Unknown exception caught");
+            }
+            finally
+            {
+                Console.WriteLine("Close file here");
+            }
+        }
+
+        //djeli ako je dozvoljeno
+        public double DoDivide(double a, double b)
+        {
+            if (b == 0)
+                throw new System.DivideByZeroException();
+            if (a == 0)
+                throw new System.ArithmeticException();
+
+            return a / b;
+        }
+    }
+}
