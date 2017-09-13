24012f12ee10c85c461cde861a8a5f453f8a4ca7
Edin Jašarević <edinjasar14@gmail.com>
2017-09-13 16:40:54 +0200

    primjer11-01

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 11/Primjer 01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter Main...");
+            Program p = new Program();
+            p.Func1();
+            Console.WriteLine("Exit Main...");
+        }
+
+        public void Func1()
+        {
+            Console.WriteLine("Enter Func1...");
+            Func2();
+            Console.WriteLine("Exit Func2...");
+        }
+
+        public void Func2()
+        {
+            Console.WriteLine("Enter Func2...");
+            throw new System.Exception();
+            Console.WriteLine("Exit Func2...");
+        }
+    }
+}
