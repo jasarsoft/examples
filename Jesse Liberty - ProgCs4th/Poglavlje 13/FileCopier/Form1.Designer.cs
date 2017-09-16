169a74d8de4182546ff26ed4b1395feeb65e60e3
Edin Jašarević <edinjasar14@gmail.com>
2017-09-16 07:06:26 +0200

    primjer13-01

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 13/FileCopier/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace FileCopier
+{
+    partial class frmFileCopier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblTarget = new System.Windows.Forms.Label();
+            this.tvwSource = new System.Windows.Forms.TreeView();
+            this.tvwTargetDir = new System.Windows.Forms.TreeView();
+            this.btnChar = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chkOverwrite = new System.Windows.Forms.CheckBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtTargetDir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(12, 27);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(68, 13);
+            this.lblSource.TabIndex = 0;
+            this.lblSource.Text = "Source Files:";
+            // 
+            // lblTarget
+            // 
+            this.lblTarget.AutoSize = true;
+            this.lblTarget.Location = new System.Drawing.Point(285, 27);
+            this.lblTarget.Name = "lblTarget";
+            this.lblTarget.Size = new System.Drawing.Size(65, 13);
+            this.lblTarget.TabIndex = 1;
+            this.lblTarget.Text = "Target Files:";
+            // 
+            // tvwSource
+            // 
+            this.tvwSource.CheckBoxes = true;
+            this.tvwSource.Location = new System.Drawing.Point(15, 43);
+            this.tvwSource.Name = "tvwSource";
+            this.tvwSource.Size = new System.Drawing.Size(250, 238);
+            this.tvwSource.TabIndex = 2;
+            this.tvwSource.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.tvwSource_AfterCheck);
+            this.tvwSource.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvwExpand);
+            // 
+            // tvwTargetDir
+            // 
+            this.tvwTargetDir.Location = new System.Drawing.Point(288, 69);
+            this.tvwTargetDir.Name = "tvwTargetDir";
+            this.tvwTargetDir.Size = new System.Drawing.Size(250, 212);
+            this.tvwTargetDir.TabIndex = 3;
+            this.tvwTargetDir.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tvwExpand);
+            this.tvwTargetDir.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvwTargetDir_AfterSelect);
+            // 
+            // btnChar
+            // 
+            this.btnChar.Location = new System.Drawing.Point(15, 287);
+            this.btnChar.Name = "btnChar";
+            this.btnChar.Size = new System.Drawing.Size(75, 23);
+            this.btnChar.TabIndex = 4;
+            this.btnChar.Text = "Char";
+            this.btnChar.UseVisualStyleBackColor = true;
+            this.btnChar.Click += new System.EventHandler(this.btnChar_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(463, 287);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(463, 316);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(463, 345);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // chkOverwrite
+            // 
+            this.chkOverwrite.AutoSize = true;
+            this.chkOverwrite.Location = new System.Drawing.Point(288, 287);
+            this.chkOverwrite.Name = "chkOverwrite";
+            this.chkOverwrite.Size = new System.Drawing.Size(92, 17);
+            this.chkOverwrite.TabIndex = 8;
+            this.chkOverwrite.Text = "Overwrite files";
+            this.chkOverwrite.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 326);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "label3";
+            // 
+            // txtTargetDir
+            // 
+            this.txtTargetDir.Location = new System.Drawing.Point(288, 43);
+            this.txtTargetDir.Name = "txtTargetDir";
+            this.txtTargetDir.Size = new System.Drawing.Size(250, 20);
+            this.txtTargetDir.TabIndex = 10;
+            // 
+            // frmFileCopier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 422);
+            this.Controls.Add(this.txtTargetDir);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.chkOverwrite);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnChar);
+            this.Controls.Add(this.tvwTargetDir);
+            this.Controls.Add(this.tvwSource);
+            this.Controls.Add(this.lblTarget);
+            this.Controls.Add(this.lblSource);
+            this.Name = "frmFileCopier";
+            this.Text = "FileCopier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblTarget;
+        private System.Windows.Forms.TreeView tvwSource;
+        private System.Windows.Forms.TreeView tvwTargetDir;
+        private System.Windows.Forms.Button btnChar;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox chkOverwrite;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtTargetDir;
+    }
+}
+
---
+++ Jesse Liberty - ProgCs4th/Poglavlje 13/FileCopier/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/// <summary>
+/// File Copier - Windows Forms demostracijski program
+/// (c) Copyright 2005 Liberty Association, Inc.
+/// </summary>
+namespace FileCopier
+{
+    /// <summary>
+    /// Obrazac za demonstraciju Windows Forms implementacije
+    /// </summary>
+    public partial class frmFileCopier : Form
+    {
+        private const int MaxLevel = 2;
+
+        public frmFileCopier()
+        {
+            InitializeComponent();
+
+            FillDirectoryTree(tvwSource, true);
+            FillDirectoryTree(tvwTargetDir, false);
+        }
+
+        /// <summary>
+        /// Ugnjezdena klasa koja zna kako uspordjivati dvije datoteke
+        /// koje zelimo sortirati od vece prema manjoj
+        /// </summary>
+        public class FileComparer : IComparer<FileInfo>
+        {
+            public int Compare(FileInfo file1, FileInfo file2)
+            {
+                if (file1.Length > file2.Length)
+                    return -1;
+                else if (file1.Length < file2.Length)
+                    return 1;
+                else
+                    return 0;
+            }
+
+            public bool Equals(FileInfo x, FileInfo y)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int GetHashCode(FileInfo x)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void FillDirectoryTree(TreeView tvw, bool isSource)
+        {
+            //popunjava tvwSource, the Source TreeView sa sadrzajem
+            //lokalnog tvrdog diska; najprije brise sve cvorove;
+            tvw.Nodes.Clear();
+
+            //uzima logicke diskove i stavlja ih u korijenske covorove
+            //popunjava polje s logicnkim diskovima na stroju
+            string[] strDrives = Environment.GetLogicalDrives();
+
+            //prolazi kroz diskove i dodaje ih stablu
+            //koristi blok try/catch pa ako disk nije spreman npr prazna disketa ili CD
+            //nece biti dodan na popis
+            foreach (string rootDirectoryName in strDrives)
+            {
+                try
+                {
+                    //popunjava polje sa mapama prve razine
+                    //ako disk nije spreman bit ce izbacena iznimka
+                    DirectoryInfo dir = new DirectoryInfo(rootDirectoryName);
+                    dir.GetDirectories(); //forsira iznimku ako disk nije spreman
+                    TreeNode ndRoot = new TreeNode(rootDirectoryName);
+                    //dodaje cvor za svaku korijensku mapu
+                    tvw.Nodes.Add(ndRoot);
+                    //dodaje cvorove pod mapa
+                    //ako je TreeView izvor onda uzima i imena datoteka
+                    if(isSource)
+                    {
+                        GetSubDirectoryNodes(ndRoot, ndRoot.Text, true, 1);
+                    }
+                    else
+                    {
+                        GetSubDirectoryNodes(ndRoot, ndRoot.Text, false, 1);
+                    }
+                }
+                //hvata pogreske poput diska koji nije spreman
+                catch {}
+
+                Application.DoEvents();   
+            }
+        }
+
+        /// <summary>
+        /// uzima sve pod mape ispod prosliedjenog cvora mape
+        /// dodaje ih u stablo mapa;
+        /// proslijedjeni parametri su roditeljski cvor za tu podmapu, i Boolen zastavica koja govori
+        /// da li treba uzeti datoteke iz podmape.
+        /// </summary>
+        /// <param name="parentNode"></param>
+        /// <param name="fullName"></param>
+        /// <param name="getFileNames"></param>
+        /// <param name="level"></param>
+        private void GetSubDirectoryNodes(TreeNode parentNode, string fullName, bool getFileNames, int level)
+        {
+            DirectoryInfo dir = new DirectoryInfo(fullName);
+            DirectoryInfo[] dirSubs = dir.GetDirectories();
+
+            //dodaje cvor-potomak za svaku mapu
+            foreach (DirectoryInfo dirSub in dirSubs)
+            {
+                //ne prikazuje skrivene mape
+                if ((dirSub.Attributes & FileAttributes.Hidden) != 0) continue;
+
+                ///<summary>
+                /// Svaka mapa ima punu putanju.
+                /// Trebamo ju podjelite na kosim crtama, i upotrijebit samo zadnji cvor u stablu.
+                /// Potrebno je dvaput napisati kosu crtu jer je ona inace znak za prekid
+                ///</summary>
+                TreeNode subNode = new TreeNode(dirSub.Name);
+                parentNode.Nodes.Add(subNode);
+
+                //rekurzivno poziva GetSubDirectoryNodes
+                if (level < MaxLevel)
+                    GetSubDirectoryNodes(subNode, dirSub.FullName, getFileNames, level + 1);
+            }
+            if(getFileNames)
+            {
+                //uzima datoteke za svoj cvor
+                FileInfo[] files = dir.GetFiles();
+                //nakon spremanja cvorova, sada sprema datoteke iz mape
+                foreach(FileInfo file in files)
+                {
+                    TreeNode fileNode = new TreeNode(file.Name);
+                    parentNode.Nodes.Add(fileNode);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Uzima cvor i vraca punu putanju
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        private string GetParentString(TreeNode node)
+        {
+            //ako je ovo korijenski cvor (c:\) vraca tekst
+            if (node.Parent == null)
+                return node.Text;
+            else
+            {
+                //vraca se gore i uzima putanju zatim dodaje ovaj cvor i kosu crtu
+                //ako je ovaj cvor list, ne dodaje kosu crtu
+                return GetParentString(node.Parent) + node.Text + (node.Nodes.Count == 0 ? "" : "\\");
+            }
+        }
+
+        /// <summary>
+        /// S popisom i polje na raspolaganju popunjava popis imena svih odabranih datoteka
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="fileNames"></param>
+        /// Popunjava ArrayList sa punim putanjama odabranih datoteka
+        private void GetCheckedFiles(TreeNode node, List<string> fileNames)
+        {
+            //ako je list...
+            if(node.Nodes.Count == 0)
+            {
+                //ako je cvor odabran
+                if(node.Checked)
+                {
+                    //uzima punu putanju i dodaje je na arrayList
+                    string fullPath = GetParentString(node);
+                    fileNames.Add(fullPath);
+                }
+            }
+            else //ako ovaj cvor nije list
+            {
+                //ako ovaj cvor nije list
+                foreach(TreeNode n in node.Nodes)
+                {
+                    GetCheckedFiles(n, fileNames);
+                }
+            }
+        }
+
+        private List<FileInfo> GetFileList()
+        {
+            //stvara nesortirano polje punih putanja datoteka
+            List<string> fileNames = new List<string>();
+
+            //ArrayList fileNames = new ArrayList();
+
+            //popunjava fileNames ArrayList s punom putanjom svake datoteke za kopiranje
+            foreach(TreeNode theNode in tvwSource.Nodes)
+            {
+                GetCheckedFiles(theNode, fileNames);
+            }
+
+            //pravi popis za cuvanje FileInfo objekata
+            List<FileInfo> fileList = new List<FileInfo>();
+            //ArrayList fileList = new ArrayList();
+
+            //za svako ime datoteke iz nesortiranog polja ako ime odgovara datoteci (a ne mapi) dodaje ga na popis datoteka
+            foreach(string fileName in fileNames)
+            {
+                //stvara datoteku s imenom
+                FileInfo file = new FileInfo(fileName);
+                //provjerava postoji li na disku i ne uspjeva ako je mapa
+                if(file.Exists)
+                {
+                    //i kljuc i vrijednost je datoteka
+                    //da li bi bilo lakse kad bi imali praznu vrijednost
+                    fileList.Add(file);
+                }
+            }
+
+            //stvara instancu sucelja IComparer
+            IComparer<FileInfo> comparer = (IComparer<FileInfo>)new FileComparer();
+
+            //prosljedjuje komparator metodi za sortiranje tako da je popis
+            //sortiran s metodom za sortiranje
+            fileList.Sort(comparer);
+            return fileList;
+        }
+
+        /// <summary>
+        /// Rekurzija postavlja ili brise oznake za potvrdu
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="check"></param>
+        private void SetCheck(TreeNode node, bool check)
+        {
+            //trazi sve cvorove potomke ovog cvora
+            foreach(TreeNode n in node.Nodes)
+            {
+                n.Checked = check; //potvrdjuje cvor
+                //ako je to cvor u stablu, rekurzivno prolazi
+                if(n.Nodes.Count != 0)
+                {
+                    SetCheck(n, check);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ako korisnik pritisne Cancel zatvara se
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// Pravi sortirani popis svih datoteka odabranih za kopiranje u odredisnu mapu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            //uzima popis
+            List<FileInfo> fileList = GetFileList();
+
+            //kopira datoteke
+            foreach(FileInfo file in fileList)
+            {
+                try
+                {
+                    //osvjezava natpis da prikazuje napredak
+                    lblStatus.Text = "Copying " + txtTargetDir.Text + "\\" + file.Name + "...";
+                    Application.DoEvents();
+
+                    //kopira datoteku u odredisnu mapu
+                    file.CopyTo(txtTargetDir.Text + "\\" + file.Name, chkOverwrite.Checked);
+                }
+                catch (Exception ex)
+                {
+                    //mozda zelite nesto vise od prikazivanja poruke
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            lblStatus.Text = "Done.";
+            Application.DoEvents();
+        }
+
+        /// <summary>
+        /// Govori korijenu svakog stabla da ponisti odabir svih cvorova ispod
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChar_Click(object sender, EventArgs e)
+        {
+            //uzima cvor na najvisem polozaju za svaki disk i govori mu da se rekurzivno ocisti
+            foreach(TreeNode node in tvwSource.Nodes)
+            {
+                SetCheck(node, false);
+            }
+        }
+
+        /// <summary>
+        /// Provjerava da li korisnik zaista zeli brisanje.
+        /// Pravi popis i brise svaku datoteku
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //pita korisnika je li siguran
+            System.Windows.Forms.DialogResult result = 
+                MessageBox.Show("Are you quite sure?",  //poruka
+                "Delete files",                         //natpis
+                MessageBoxButtons.OKCancel,             //gumbi
+                MessageBoxIcon.Exclamation,             //ikone
+                MessageBoxDefaultButton.Button2);       //podrazumni gumb
+
+            //ako je korisnik siguran
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                //prolazi kroz popis i brise datoteke
+                //uzima popis odabranih datoteka
+                List<FileInfo> fileNames = GetFileList();
+
+                foreach(FileInfo file in fileNames)
+                {
+                    try
+                    {
+                        //azurira natpis da prikazuje napredak
+                        lblStatus.Text = "Deleting " + file.Name + "...";
+                        Application.DoEvents();
+
+                        //brisanje
+                        file.Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        //mozda zelite nesto vise od prikazivanja poruke
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+            lblStatus.Text = "Done.";
+            Application.DoEvents();
+        }
+
+        /// <summary>
+        /// Uzima punu putanju mape odabrane mape i kopira je u txtTargetDir
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tvwTargetDir_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            //uzima punu putanju za odabranu mapu
+            string theFullPath = GetParentString(e.Node);
+
+            //ako nije list zavrsit ce sa obrnutom kosom crtom
+            //brise kosu crtu
+            if(theFullPath.EndsWith("\\"))
+            {
+                theFullPath = theFullPath.Substring(0, theFullPath.Length - 1);
+            }
+
+            //dodaje putanju u polje za tekst
+            txtTargetDir.Text = theFullPath;
+        }
+
+        /// <summary>
+        /// Obiljezava svaki cvor ispod tekuceg s trenutnom vrijednosti obiljezenog cvora
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tvwSource_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            //poziva metodu za rekurzivni prolaz
+            //e.node je cvor koji korisnik je odabrao
+            //stanje oznake za potvrdu je vec promjenjeno dok se stiglo do ovog mjesta
+            //zbog toga zelimo proslijediti stanje e.node.Checked
+            if(e.Action != TreeViewAction.Unknown)
+            {
+                SetCheck(e.Node, e.Node.Checked);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tvwExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeView tvw = (TreeView)sender;
+            bool getFiles = tvw == tvwSource;
+            TreeNode currentNode = e.Node;
+            string fullName = currentNode.FullPath;
+            currentNode.Nodes.Clear();
+            GetSubDirectoryNodes(currentNode, fullName, getFiles, 1);
+        }
+    }
+}
