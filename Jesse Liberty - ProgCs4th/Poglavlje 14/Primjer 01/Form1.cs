c4ba4d9880fe6cc47d6452f9a225d388016dc32a
Edin Jašarević <edinjasar14@gmail.com>
2017-09-17 18:51:58 +0200

    primjer14-01-3

--- Jesse Liberty - ProgCs4th/Poglavlje 14/Primjer 01/Form1.cs
+++ Jesse Liberty - ProgCs4th/Poglavlje 14/Primjer 01/Form1.cs
@@ -21,7 +21,13 @@ namespace WorkingWithADONET
 
             //uzima zapise iz tablica kupca
             //string commandString = "SELECT CompanyName, ContactName FROM Customers";
-            string commandString = "SELECT CustomerID, CompanyName FROM Customers WHERE city='London'";
+            //string commandString = "SELECT CustomerID, CompanyName FROM Customers WHERE city='London'";
+            string commandString = "SELECT o.OrderID, ProductName FROM [Order Details] od " +
+                                   "JOIN Orders o on o.OrderID = od.OrderID " + 
+                                   "JOIN Products p on p.ProductID = od.ProductID " + 
+                                   "JOIN Customers c on o.CustomerID = c.CustomerID "+ 
+                                   "WHERE c.CompanyName = 'Vins et alcools Chevalier' " + 
+                                   "AND OrderDate >= '7/1/1996' and OrderDate <= '7/31/1996'";
 
             //stvara naredbeni objekat za skup podataka i DataSet
             SqlDataAdapter dataAdapter = new SqlDataAdapter(commandString, connectionString);
@@ -29,7 +35,8 @@ namespace WorkingWithADONET
             DataSet dataSet = new DataSet();
 
             //popunjava objekat dataset
-            dataAdapter.Fill(dataSet, "Customers");
+            //dataAdapter.Fill(dataSet, "Customers");
+            dataAdapter.Fill(dataSet, "Order Details");
 
             //uzima jednu tablicu iz dataSet
             DataTable dataTable = dataSet.Tables[0];
@@ -38,7 +45,8 @@ namespace WorkingWithADONET
             foreach (DataRow dataRow in dataTable.Rows)
             {
                 //listBox1.Items.Add(dataRow["CompanyName"] + " (" + dataRow["ContactName"] + ")");
-                listBox1.Items.Add(dataRow["CustomerID"] + "\t" + dataRow["CompanyName"]);
+                //listBox1.Items.Add(dataRow["CustomerID"] + "\t" + dataRow["CompanyName"]);
+                listBox1.Items.Add(dataRow["OrderID"] + "\t" + dataRow["ProductName"]);
             }
         }
     }
