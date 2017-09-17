df3f0e49d24d4ad12bf62c55da691174aef24c45
Edin Jašarević <edinjasar14@gmail.com>
2017-09-17 21:42:12 +0200

    primjer14-03-1

---
+++ Jesse Liberty - ProgCs4th/Poglavlje 14/Primjer 03/ProgrammaticDataDisply.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace Primjer_03
+{
+    public partial class ProgrammaticDataDisply : Form
+    {
+        public ProgrammaticDataDisply()
+        {
+            InitializeComponent();
+
+            string connectionString = "server=localhost;" +
+                                      "Trusted_Connection=yes;" +
+                                      "database = northwind";
+
+            string commandString = "SELECT CompanyName, ContactName, ContactTitle, Phone, Fax FROM Customers";
+
+            //pravi DataSet i popunjava ga
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(commandString, connectionString);
+
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, "Customers");
+
+            //povezuje DataSet sa mrezom
+            dataGridView1.DataSource = dataSet.Tables["Customers"].DefaultView;
+        }
+    }
+}
