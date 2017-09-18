33a9d7235af12659c9d8dfe0481c05c63bc38c86
Edin Jašarević <edinjasar14@gmail.com>
2017-09-18 21:23:09 +0200

    python prog01

---
+++ python/uups/program01.py
@@ -0,0 +1,7 @@
+godinaUpisa = input("Unesite godinu upisa studenta: ")
+godinaUpisa = int(godinaUpisa)
+godinaDiplomiranja = godinaUpisa + 4
+print("Godina diplomiranja bi trebalo da bude ", godinaDiplomiranja)
+godinaDiplomiranja = str(godinaDiplomiranja)
+print("Godina diplomiranja bi trebalo da bude " + godinaDiplomiranja)
+
