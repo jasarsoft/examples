33943e51dc011b8166222b8f40dafb93f0bb1ff2
Edin Jašarević <edinjasar14@gmail.com>
2017-09-19 00:41:19 +0200

    py ops ignore

--- python/uups/program01.py
+++
@@ -1,7 +0,0 @@
-godinaUpisa = input("Unesite godinu upisa studenta: ")
-godinaUpisa = int(godinaUpisa)
-godinaDiplomiranja = godinaUpisa + 4
-print("Godina diplomiranja bi trebalo da bude ", godinaDiplomiranja)
-godinaDiplomiranja = str(godinaDiplomiranja)
-print("Godina diplomiranja bi trebalo da bude " + godinaDiplomiranja)
-
