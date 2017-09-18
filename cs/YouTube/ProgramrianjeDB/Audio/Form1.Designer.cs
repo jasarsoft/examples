ecf22e2b15c3a52ea21bdbb3467dec05f4b96235
Edin Jašarević <edinjasar14@gmail.com>
2017-09-18 18:03:30 +0200

    primjer audio

---
+++ cs/YouTube/ProgramrianjeDB/Audio/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Audio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.play = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // play
+            // 
+            this.play.Appearance = System.Windows.Forms.Appearance.Button;
+            this.play.AutoSize = true;
+            this.play.Location = new System.Drawing.Point(111, 23);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(37, 23);
+            this.play.TabIndex = 0;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.CheckedChanged += new System.EventHandler(this.play_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.play);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox play;
+    }
+}
+
---
+++ cs/YouTube/ProgramrianjeDB/Audio/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Audio
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            player = new SoundPlayer("audio.wav");
+        }
+
+        private void play_CheckedChanged(object sender, EventArgs e)
+        {
+            if(play.Checked)
+            {
+                play.Text = "Stop";
+                player.Play();
+            }
+            else
+            {
+                play.Text = "Play";
+                player.Stop();
+            }
+        }
+    }
+}
