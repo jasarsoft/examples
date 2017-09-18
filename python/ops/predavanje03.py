8b9e913d78664defcba00daf7be594663ca62460
Edin Jašarević <edinjasar14@gmail.com>
2017-09-19 00:39:13 +0200

    py ops pred03

---
+++ python/ops/predavanje03.py
@@ -0,0 +1,22 @@
+import math
+
+a = 10
+b = 3
+c = a + b
+print(c)
+c = a - b
+print(c)
+c = a * b
+print(c)
+c = a / b
+print(c)
+c = a ** b
+print(c)
+c = a // b
+print(c)
+c = math.sqrt(a)
+print(c)
+c = round(c, 2)
+print(c)
+c = math.pi
+print(c)
