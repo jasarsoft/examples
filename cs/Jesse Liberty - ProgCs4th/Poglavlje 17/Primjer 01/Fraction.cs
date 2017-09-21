1b67ecf81d817132394a56e63266eb200b9dd1f1
Edin Jašarević <edinjasar14@gmail.com>
2017-09-21 22:32:39 +0200

    primjer17-01

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 17/Primjer 01/Fraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgCS
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Fraction Add(Fraction rhs)
+        {
+            if(rhs.denominator != this.denominator)
+            {
+                return new Fraction(rhs.denominator * numerator + rhs.numerator * denominator, denominator * rhs.denominator);
+            }
+            return new Fraction(this.numerator + rhs.numerator, this.numerator);
+        }
+
+        public override string ToString()
+        {
+            return numerator + "/" + denominator;
+        }
+    }
+}
