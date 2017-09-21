1093c03da3faf4031119d61c2a7d6c714a054285
Edin Jašarević <edinjasar14@gmail.com>
2017-09-22 00:00:52 +0200

    primjer17-module

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 17/Module/Calc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgCS
+{
+    public class MyCalc
+    {
+        public int Add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+        public int Mult(int val1, int val2)
+        {
+            return val1 * val2;
+        }
+    }
+}
