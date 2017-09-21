d5374ff854675fc77de2e36eec89a3f231af649b
Edin Jašarević <edinjasar14@gmail.com>
2017-09-21 23:20:55 +0200

    primjer17-02

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 17/Primjer 02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Primjer_02
+{
+    class Program
+    {
+        //main nece ucitat dijeljeni sklop
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.UseCS();
+            p.UseFraction();
+        }
+
+        //ovaj poziv ucitavva sklop Calc ali i sklop mySharredAssambly
+        public void UseCS()
+        {
+            ProgCS.MyCalc calc = new ProgCS.MyCalc();
+            Console.WriteLine("3 + 5 = {0}\n3 * 5 = {1}", calc.Add(3, 5), calc.Mult(3, 5));
+        }
+
+        public void UseFraction()
+        {
+            ProgCS.Fraction frac1 = new ProgCS.Fraction(3, 5);
+            ProgCS.Fraction frac2 = new ProgCS.Fraction(1, 5);
+            ProgCS.Fraction frac3 = frac1.Add(frac2);
+
+            Console.WriteLine("{0} + {1} = {2}", frac1, frac2, frac3);
+        }
+    }
+}
