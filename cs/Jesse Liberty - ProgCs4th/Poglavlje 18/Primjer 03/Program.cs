4db3c47f49e13afcf8965f7468af8607c90b492f
Edin Jašarević <edinjasar14@gmail.com>
2017-09-22 19:03:09 +0200

    cs pog18 pr3

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 18/Primjer 03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ReflectingAnAssembly
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //sta je u sklopu
+            Assembly a = Assembly.Load("Mscorlib");
+            Type[] types = a.GetTypes();
+
+            foreach(Type t in types)
+            {
+                Console.WriteLine("Type is {0}", t);
+            }
+            Console.WriteLine("{0} types found", types.Length);
+        }
+    }
+}
