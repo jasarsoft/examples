29bab55ee436628d41d12e0dea0d80f42eb8568f
Edin Jašarević <edinjasar14@gmail.com>
2017-09-22 19:08:30 +0200

    cs pog18 pr4

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 18/Primjer 04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ReflectingOnAType
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //ispitivanje tipa
+            Type theType = Type.GetType("System.Reflection.Assembly");
+            Console.WriteLine("Single Type is {0}", theType);
+        }
+    }
+}
