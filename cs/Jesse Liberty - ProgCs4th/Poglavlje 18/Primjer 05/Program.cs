fedaee452ecb4293b47764c476262d8d44645685
Edin Jašarević <edinjasar14@gmail.com>
2017-09-22 19:12:53 +0200

    cs pog18 pr5

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 18/Primjer 05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ReflectingOnMembersOfType
+{
+    class Program
+    {
+        static void Main()
+        {
+            //ispitivanje objekata
+            Type theType = Type.GetType("System.Reflection.Assembly");
+            Console.WriteLine("Single Type is {0}", theType);
+
+            //uzima sve clanove
+            MemberInfo[] mbInfoArray = theType.GetMembers();
+            foreach(MemberInfo mbInfo in mbInfoArray)
+            {
+                Console.WriteLine("{0} is a {1}", mbInfo, mbInfo.MemberType);
+            }
+        }
+    }
+}
