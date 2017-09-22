ae896aa84bb08ca84db717d270ade04ece115088
Edin Jašarević <edinjasar14@gmail.com>
2017-09-23 01:42:59 +0200

    cs pog19 pr2

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 19/Primjer 02/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    public interface ICalc
+    {
+        double Add(double x, double y);
+        double Sub(double x, double y);
+        double Mult(double x, double y);
+        double Div(double x, double y);
+    }
+}
