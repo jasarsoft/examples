dc96e3279a18515f755266d0eddcef3bd7d0a8ae
Edin Jašarević <edinjasar14@gmail.com>
2017-09-22 19:20:52 +0200

    cs pog18 pr6

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 18/Primjer 06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace FindingPracticularMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ispitivanje jedan objekat
+            Type theType = Type.GetType("System.Reflection.Assembly");
+
+            //samo clanovi koji su metode na Get
+            MemberInfo[] mbInfoArray = theType.FindMembers(MemberTypes.Method,
+                BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly,
+                Type.FilterName, "Get*");
+
+            foreach(MemberInfo mbInfo in mbInfoArray)
+            {
+                Console.WriteLine("{0} is a {1}", mbInfo, mbInfo.MemberType);
+            }
+        }
+    }
+}
