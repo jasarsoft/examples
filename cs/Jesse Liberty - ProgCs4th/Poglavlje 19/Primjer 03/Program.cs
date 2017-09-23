c383bd30db3dc61a12da39824173d2527df38328
Edin Jašarević <edinjasar14@gmail.com>
2017-09-23 02:32:27 +0200

    cs pog19 pr3

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 19/Primjer 03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+
+namespace CalculatorServerNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stvara kanal i registrira ga
+            HttpChannel chan = new HttpChannel(65100);
+            ChannelServices.RegisterChannel(chan, false);
+
+            Type calcType = Type.GetType("CalculatorServerNS.CalculatorServer");
+
+            //registrira tip dobro poznatog objekta i govori posluzitelju
+            //da poveze tip sa zavrsom tackom "theEndPoint"
+            RemotingConfiguration.RegisterWellKnownServiceType(calcType, "theEndPoint", WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Press [ENTER] to exit...");
+            Console.ReadKey();
+        }
+    }
+}
