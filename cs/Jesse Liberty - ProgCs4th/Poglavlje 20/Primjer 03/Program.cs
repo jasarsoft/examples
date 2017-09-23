954013ef1197f3865df16608290c7db3b3c1ae78
Edin Jašarević <edinjasar14@gmail.com>
2017-09-23 21:56:20 +0200

    cs pog20 pr3

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 20/Primjer 03/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace SharedResource
+{
+    class Program
+    {
+        private int counter = 0;
+
+        static void Main()
+        {
+            //stvara instancu ove klase
+            Program p = new Program();
+
+            //izvodi se izvan saticke main klase
+            p.DoTest();
+        }
+
+        public void DoTest()
+        {
+            Thread t1 = new Thread(new ThreadStart(Incrementer));
+            t1.IsBackground = true;
+            t1.Name = "ThreadOne";
+            t1.Start();
+            Console.WriteLine("Stared thread {0}", t1.Name);
+
+            Thread t2 = new Thread(new ThreadStart(Incrementer));
+            t2.IsBackground = true;
+            t2.Name = "ThreadTwo";
+            t2.Start();
+            Console.WriteLine("Stared thread {0}", t2.Name);
+
+            t1.Join();
+            t2.Join();
+
+            //kada su ove dretve gotove ispisuje poruku
+            Console.WriteLine("All my threads are done.");
+        }
+
+        //pokazna metoda broji do 1k
+        public void Incrementer()
+        {
+            try
+            {
+                while(counter < 1000)
+                {
+                    int temp = counter;
+                    temp++; //povecanje
+
+                    //simulira obavljanje posla
+                    Thread.Sleep(1);
+
+                    //pridruzuje povecanu vrijednost i prikazuje rezultat
+                    counter = temp;
+                    Console.WriteLine("{0} Incrementer: {1}", Thread.CurrentThread.Name, counter);
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                Console.WriteLine("{0} interrupted.", Thread.CurrentThread.Name);
+            }
+            finally
+            {
+                Console.WriteLine("{0} exiting.", Thread.CurrentThread.Name);
+            }
+        }
+    }
+}
