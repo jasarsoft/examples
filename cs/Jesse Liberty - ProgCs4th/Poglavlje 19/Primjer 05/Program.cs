d017c96cdd846b48c22d532fced99812bdc18adb
Edin Jašarević <edinjasar14@gmail.com>
2017-09-23 15:10:56 +0200

    cs pog19 pr5

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 19/Primjer 05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+
+
+namespace Primjer_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpChannel chan = new HttpChannel();
+            ChannelServices.RegisterChannel(chan, true);
+
+            CalculatorServerNS.CalculatorServer calculator = new CalculatorServerNS.CalculatorServer();
+            RemotingServices.Marshal(calculator, "theEndPoint");
+
+            //Posluzij i one koji stoje i cekaju (Milton)
+            Console.WriteLine("Press [enter] to exist...");
+            Console.ReadKey();
+        }
+    }
+}
