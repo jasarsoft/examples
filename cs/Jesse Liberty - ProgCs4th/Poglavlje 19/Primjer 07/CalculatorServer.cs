ab8bd7658492865863913bacf042b8fc382c6338
Edin Jašarević <edinjasar14@gmail.com>
2017-09-23 16:24:47 +0200

    cs pog19 pr7

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 19/Primjer 07/CalculatorServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Primjer_07
+{
+    class CalculatorServer : MarshalByRefObject, Calculator.ICalc
+    {
+        public CalculatorServer()
+        {
+            Console.WriteLine("CalculatorServer constructior");
+        }
+
+        //implementira cetiri metode
+        public double Add(double x, double y)
+        {
+            Console.WriteLine("Add {0} + {1}", x, y);
+            return x + y;
+        }
+        public double Sub(double x, double y)
+        {
+            Console.WriteLine("Sub {0} - {1}", x, y);
+            return x - y;
+        }
+        public double Mult(double x, double y)
+        {
+            Console.WriteLine("Mult {0} * {1}", x, y);
+            return x * y;
+        }
+        public double Div(double x, double y)
+        {
+            Console.WriteLine("Div {0} / {1}", x, y);
+            return x / y;
+        }
+    }
+}
---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 19/Primjer 07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+
+namespace Primjer_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stvara kanal i registrira ga
+            HttpChannel chan = new HttpChannel(65100);
+            ChannelServices.RegisterChannel(chan, false);
+
+            //napravite vlastitu instancu i izravno pozovite Marshal metodu
+            CalculatorServer calculator = new CalculatorServer();
+            ObjRef objRef = RemotingServices.Marshal(calculator);
+            FileStream fileStream = new FileStream("calculatorSoap.txt", FileMode.Create);
+            SoapFormatter soapFormater = new SoapFormatter();
+            soapFormater.Serialize(fileStream, objRef);
+            fileStream.Close();
+
+            //posluzuj i one koji stoje i cekaju
+            Console.WriteLine("Exported to CalculatorSoap.txt. Press ENTER to exit...");
+            Console.ReadKey();
+        }
+    }
+}
