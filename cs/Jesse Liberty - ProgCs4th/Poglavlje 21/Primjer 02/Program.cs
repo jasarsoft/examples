1ee8e703b1888d58f6903f6984645218e3b5e5ef
Edin Jašarević <edinjasar14@gmail.com>
2017-09-25 23:58:45 +0200

    cs pog21 pr2

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace ExploringFilesAndSubdirectories
+{
+    class Program
+    {
+        //staticke varijable clanise za pracenje ukupnih vrijednosti razina uvlacenja
+        static int dirCounter = 1;
+        static int indentLevel = -1; //provi pomak je 0
+
+        static int fileCounter = 0;
+
+        static void Main()
+        {
+            Program p = new Program();
+
+            //odabir pocetne podmape
+            string theDirectory = Environment.GetEnvironmentVariable("SystemRoot");
+            //korisnici Monoa i Shared source CLI-ja pod Linixom, Unixom ili MacOS X-om
+            //predhodna dva reda trebaju staviti u komentar i izdvojiti iz komentara sljedece
+            //string theDirectory = "/tmp";
+
+            //poziva metodu za ispitivanje mape, prikazivanje datuma pristupanja i svih podmapa.
+            DirectoryInfo dir = new DirectoryInfo(theDirectory);
+
+            p.ExploreDirectory(dir);
+
+            //kompletirano, ispisuje statistike
+            Console.WriteLine("\n{0} directories found.", dirCounter);
+        }
+
+        //pokrece ju sa DirectoryInfo objektom
+        //za svaku mapu koju pronadje rekurzivno ce se pozvati
+        private void ExploreDirectory(DirectoryInfo dir)
+        {
+            indentLevel++; //pomak razine mape
+
+            //stvar uvlake za mape
+            for (int i = 0; i < indentLevel; i++)
+                Console.Write("    "); //dva razmaka po razini
+
+            //ispisuje mapu i vrijeme kad joj je zadnji put pristupano
+            Console.WriteLine("[{0}] {1} [{2}]", indentLevel, dir.Name, dir.LastAccessTime);
+
+            //uzima sve datoteke iz mape i ispisuje imena, vrijeme zadnjeg pristupa i velicinu
+            FileInfo[] filesInDir = dir.GetFiles();
+            foreach (FileInfo file in filesInDir)
+            {
+                //jos jedna uvlaka kako bih datoteke bile ispod mapa
+                for (int i = 0; i < indentLevel + 1; i++)
+                    Console.Write("    "); //4 razmaka po razini
+
+                Console.WriteLine("{0} [{1}] Size: {2} bytes", file.Name, file.LastWriteTime, file.Length);
+                fileCounter++;
+            }
+        }
+    }
+}
