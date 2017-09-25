02d1d61695f0a205653ab0ba148da46250a01c2f
Edin Jašarević <edinjasar14@gmail.com>
2017-09-26 00:38:43 +0200

    cs pog21 pr3

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace CreatingSubdirectoryManipulationFile
+{
+    class Tester
+    {
+        static void Main()
+        {
+            //stvara instancu i pokrece je
+            Tester t = new Tester();
+
+            string theDirectory = @".\Media";
+            DirectoryInfo dir = new DirectoryInfo(theDirectory);
+            t.ExploreDirectory(dir);
+        }
+
+        //pokrece je sa imenom mape
+        private void ExploreDirectory(DirectoryInfo dir)
+        {
+            //stvara novu pod mapu
+            string newDirectory = "newTest";
+            DirectoryInfo newSubDir = dir.CreateSubdirectory(newDirectory);
+
+            //uzima sve datoteke iz mape i kopira ih u novu mapu
+            FileInfo[] filesInDir = dir.GetFiles();
+            foreach (FileInfo file in filesInDir)
+            {
+                string fullName = newSubDir.FullName + "\\" + file.Name;
+                file.CopyTo(fullName);
+                Console.WriteLine("{0} copied to newTest", file.FullName);
+            }
+
+            //uzima kolekciju kopiranih datoteka
+            filesInDir = newSubDir.GetFiles();
+
+            //neke brise u nekima mjenja imena
+            int counter = 0;
+            foreach (FileInfo file in filesInDir)
+            {
+                string fullName = file.FullName;
+                if(counter++ % 2 == 0)
+                {
+                    file.MoveTo(fullName + ".bak");
+                    Console.WriteLine("{0} renamed to {1}", fullName, file.FullName);
+                }
+                else
+                {
+                    file.Delete();
+                    Console.WriteLine("{0} deleted.", fullName);
+                }
+            }
+
+            newSubDir.Delete(true); //brise pod mapu
+        }
+    }
+}
