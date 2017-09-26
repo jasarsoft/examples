dabba5aab6e2b39a5dc865af825ba2ef027da8a5
Edin Jašarević <edinjasar14@gmail.com>
2017-09-27 01:21:22 +0200

    cs pog21 pr6

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingWritingToTextFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            //otvara datoteku
+            FileInfo theSourceFile = new FileInfo(@"input.txt");
+
+            //stvara citac teksta za tu datoteku
+            StreamReader reader = theSourceFile.OpenText();
+
+            //stvara pisac teksta za novu datoteku
+            StreamWriter writer = new StreamWriter(@"output.txt", false);
+
+            //stvara tekstualnu varijablu za cuvanje svakog reda
+            string text;
+
+            //prolazi kroz datoteku cita svaki red i upisuje ih u datoteku
+            do
+            {
+                text = reader.ReadLine();
+                writer.WriteLine(text);
+                Console.WriteLine(text);
+            } while (text != null);
+
+            //spremanje i zatvaranje
+            reader.Close();
+            writer.Close();
+        }
+    }
+}
