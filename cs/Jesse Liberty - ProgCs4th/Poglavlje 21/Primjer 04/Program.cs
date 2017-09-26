a31d74ffd1a5a91f92171e7951ffd1edaef9a5d5
Edin Jašarević <edinjasar14@gmail.com>
2017-09-27 00:42:18 +0200

    cs pog21 pr4 rev1

--- cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 04/Program.cs
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace ImplementingBinaryReadWriteToFile
 {
@@ -11,16 +12,23 @@ namespace ImplementingBinaryReadWriteToFile
         {
             //stvara instancu i pokrece je
             Program p = new Program();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             p.Run();
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds.ToString() + " ms");
         }
 
         //pokretanje sa imenom mape
         private void Run()
         {
             //datoteka za citanje
-            Stream inputStream = File.OpenRead(@"input.txt");
+            Stream inputStream = File.OpenRead(@"D:\Games\Shollym Multi-Patch 2016\sound.pack");
             //datoteka za upisivanje
-            Stream outputStrem = File.OpenWrite(@"output.bak");
+            Stream outputStrem = File.OpenWrite(@"D:\Games\Shollym Multi-Patch 2016\sound4.pack.bak");
             //stvara medjuspremnik zacuvanje bajtova
             byte[] buffer = new byte[sizeBuff];
             int bytesRead;
