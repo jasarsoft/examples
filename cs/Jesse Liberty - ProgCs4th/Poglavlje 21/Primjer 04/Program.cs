cd7c3fef1f748625882a9b8322c7c7bdad1b211f
Edin Jašarević <edinjasar14@gmail.com>
2017-09-26 23:53:04 +0200

    cs pog21 pr4

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ImplementingBinaryReadWriteToFile
+{
+    class Program
+    {
+        const int sizeBuff = 1024;
+
+        static void Main()
+        {
+            //stvara instancu i pokrece je
+            Program p = new Program();
+            p.Run();
+        }
+
+        //pokretanje sa imenom mape
+        private void Run()
+        {
+            //datoteka za citanje
+            Stream inputStream = File.OpenRead(@"input.txt");
+            //datoteka za upisivanje
+            Stream outputStrem = File.OpenWrite(@"output.bak");
+            //stvara medjuspremnik zacuvanje bajtova
+            byte[] buffer = new byte[sizeBuff];
+            int bytesRead;
+
+            //dok metoda vraca bajtove nastavlja ih upisivati u islazni tok
+            while((bytesRead = inputStream.Read(buffer, 0, sizeBuff)) > 0)
+            {
+                outputStrem.Write(buffer, 0, bytesRead);
+            }
+
+            //posprema i zatvara fajlove prije zavrsetka
+            inputStream.Close();
+            outputStrem.Close();
+        }
+    }
+}
