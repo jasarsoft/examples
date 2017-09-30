5736cea0ae21889b9bb0b135aef72cfa9d6be3c4
Edin Jašarević <edinjasar14@gmail.com>
2017-09-30 20:18:45 +0200

    cs mp v4

---
+++ cs/Mihalo Pupin/Vjezba 04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IskazIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string komparacija;
+
+            Console.Write("Unesi prvi cijeli broj: ");
+            double prom1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Unesi drugi cijeli broj: ");
+            double prom2 = Convert.ToDouble(Console.ReadLine());
+
+            if (prom1 < prom2)
+                komparacija = "manji";
+            else
+            {
+                if (prom1 == prom2)
+                    komparacija = "jednak";
+                else
+                    komparacija = "veci";
+            }
+
+            Console.WriteLine("Prvi broj je {0} od drugog broja.", komparacija);
+        }
+    }
+}
