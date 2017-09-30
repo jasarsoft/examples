ec96c84f8a41bd6885b42053becb4637fd2bdb36
Edin Jašarević <edinjasar14@gmail.com>
2017-09-30 16:11:07 +0200

    cs mp v2

---
+++ cs/Mihalo Pupin/Vjezba 02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vjezba_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mojInteger;
+            string mojString;
+
+            mojInteger = 17;
+            mojString = "Moj cjeli broj \"mojInteger\" je: ";
+            Console.WriteLine("{0} {1}.", mojString, mojInteger);
+        }
+    }
+}
