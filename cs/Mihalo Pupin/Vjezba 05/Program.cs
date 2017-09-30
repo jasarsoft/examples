e95eac2f878c7690bb903b743d36fb1d55e8a616
Edin Jašarević <edinjasar14@gmail.com>
2017-09-30 20:32:55 +0200

    cs mp v5

---
+++ cs/Mihalo Pupin/Vjezba 05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IskazSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string mojeIme = "petar";
+            const string mojePrezime = "petrovic";
+            const string mojNadimak = "petko";
+
+            string ime;
+            Console.Write("Koje je tvoje ime: ");
+            ime = Console.ReadLine();
+
+            switch(ime.ToLower())
+            {
+                case mojeIme:
+                    Console.WriteLine("Ti imas isto ime kao moje.");
+                    break;
+                case mojePrezime:
+                    Console.WriteLine("Ti imas isto prezme kao moje.");
+                    break;
+                case mojNadimak:
+                    Console.WriteLine("Ovo je veoma smjesan nadima.");
+                    break;
+            }
+
+            Console.WriteLine("Pozdrav {0}!", ime);
+        }
+    }
+}
