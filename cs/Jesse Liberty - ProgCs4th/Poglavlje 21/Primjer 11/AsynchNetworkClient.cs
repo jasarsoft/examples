8a4dd1ae973ea952229181af31a906e258df7317
Edin Jašarević <edinjasar14@gmail.com>
2017-09-30 23:02:17 +0200

    cs jl-pog21-pr11

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 11/AsynchNetworkClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsynchNetworkClientNM
+{
+    class AsynchNetworkClient
+    {
+        private NetworkStream streamToServer;
+
+        public AsynchNetworkClient()
+        {
+            string serverName = "localhost";
+            Console.WriteLine("Connecting to {0}", serverName);
+            TcpClient tcpSocket = new TcpClient(serverName, 65000);
+            streamToServer = tcpSocket.GetStream();
+        }
+
+        public int Run()
+        {
+            string message = "Hello Programming C#";
+            Console.WriteLine("Sending {0} to server.", message);
+
+            //stvara streamWriter i koristi ga za zapisivanje niza na poluzitelju
+            StreamWriter writer = new StreamWriter(streamToServer);
+            writer.WriteLine(message);
+            writer.Flush();
+
+            StreamReader reader = new StreamReader(streamToServer);
+            string strResponse = reader.ReadLine();
+            Console.WriteLine("Recieved: {0}", strResponse);
+            streamToServer.Close();
+            return 0;
+        }
+    }
+}
---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsynchNetworkClientNM
+{
+    class Program
+    {
+        static void Main()
+        {
+            AsynchNetworkClient client = new AsynchNetworkClient();
+            client.Run();
+        }
+    }
+}
