792810eceb4dc3a7f0dcac3810949ebfc393d21f
Edin Jašarević <edinjasar14@gmail.com>
2018-01-15 13:51:59 +0100

    us-opp pog02 pr00

---
+++ python/us-opp/poglavlje02/primjer00.py
@@ -0,0 +1,122 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+3*2-2**3+1
+1
+>>> 5/2
+2.5
+>>> 5//2
+2
+>>> 5 % 2
+1
+>>> 9 % 3
+0
+>>> 9.5 % 3
+0.5
+>>> 9 % 3.5
+2.0
+>>> "termo" + "metar"
+'termometar'
+>>> "Snezana i" + " patuljak" * 7
+'Snezana i patuljak patuljak patuljak patuljak patuljak patuljak patuljak'
+>>> 2>3
+False
+>>> 3 != 2
+True
+>>> 3 == 2
+False
+>>> 3 >= 3
+True
+>>> 3 == 3
+True
+>>> brojac = 0; brojac = brojac + 1; brojac
+1
+>>> brojac += 1; brojac
+2
+>>> 
+a = True
+>>> not a
+False
+>>> a and True
+True
+>>> a and False
+False
+>>> a or True
+True
+>>> a or False
+True
+>>> not (a and True or False)
+False
+>>> a = 8
+>>> bin(a)
+'0b1000'
+>>> b = ¸a
+SyntaxError: invalid character in identifier
+>>> b = ~a
+>>> bin(b)
+'-0b1001'
+>>> b
+-9
+>>> c = a & b
+>>> bin(c)
+'0b0'
+>>> d = a | b
+>>> bin(d)
+'-0b1'
+>>> d
+-1
+>>> c
+0
+>>> e = a
+>>> e
+8
+>>> e = a << 2
+>>> bin(e)
+'0b100000'
+>>> e
+32
+>>> print(2^3, 2**3)
+1 8
+>>> 10 in (10, 20, 30)
+True
+>>> "plavo" in ("crveno", "zeleno", "plavo")
+True
+>>> 20 not in (10, 20, 30)
+False
+>>> "spanska" in "Dolazi spanska inkvizicija"
+True
+>>> a = "a"
+>>> "aa" is a+a
+False
+>>> "aa" == a+a
+True
+>>> float("1234.567")
+1234.567
+>>> bool("1234")
+True
+>>> int("1234")
+1234
+>>> str("1234.567")
+'1234.567'
+>>> round(2.71, 1)
+2.7
+>>> round(2.71, 2)
+2.71
+>>> round(3.14)
+3
+>>> round(2.71)
+3
+>>> round(3.5)
+4
+>>> round(-3.5)
+-4
+>>> round(-2.71)
+-3
+>>> round(2.675, 2)
+2.67
+>>> int(3.5)
+3
+>>> int(2.71)
+2
+>>> int(-3.5)
+-3
+>>> 
