9ed7f1a5d30aa084625590ebef240200bb4ff05a
Edin Jašarević <edinjasar14@gmail.com>
2018-01-15 13:52:14 +0100

    us-opp pog02 pr01

---
+++ python/us-opp/poglavlje02/primjer01.py
@@ -0,0 +1,8 @@
+#ucitavanje vrijednosti poluprecnika
+poluprecnik = float(input("Unesi poluprecnik: "))
+
+#racunanje povrsine kruga
+povrsina = poluprecnik * poluprecnik * 3.14159
+
+#prikaz rezultata
+print("Provrsina kruga R=", poluprecnik, " je ", povrsina)
