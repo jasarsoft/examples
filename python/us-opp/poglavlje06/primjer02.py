0cde1d9cb7638885055c99cd37cbd711f0908f11
Edin Jašarević <edinjasar14@gmail.com>
2018-01-16 13:05:14 +0100

    py us-opp pog06 pr02

---
+++ python/us-opp/poglavlje06/primjer02.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.pendown()
+
+#pravougaonik
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+#setougao
+for i in range(6):
+    turtle.forward(100)
+    turtle.right(60)
+
+    
