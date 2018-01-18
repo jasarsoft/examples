837bbb45d9cbf772fe09b40824ac1da4158bc6f6
Edin Jašarević <edinjasar14@gmail.com>
2018-01-18 14:18:17 +0100

    py us-opp pog12 test_indekstm

---
+++ python/us-opp/poglavlje12/test_indeksTM.py
@@ -0,0 +1,8 @@
+# test index_tm
+
+from indeks_tm import IndeksTM
+
+ind1 = IndeksTM("Jovana Jovanovic", 18, 66, 178)
+print("Indeks tjelesne mase za ", ind1.getIme(), \
+      " je ", ind1.getIndeksTM(), ind1.getStatus())
+
