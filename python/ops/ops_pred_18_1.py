821cfae3bbb5d49d1e748924b71950ff8cd27c1c
Edin Jašarević <edinjasar14@gmail.com>
2018-01-19 14:50:10 +0100

    py ops pred18-test

---
+++ python/ops/ops_pred_18_1.py
@@ -0,0 +1,12 @@
+import ops_pred_18_kvadar
+
+k = ops_pred_18_kvadar.Kvadar(10, 20, 30)
+
+p = ops_pred_18_kvadar.povrsina(k)
+p1 = ops_pred_18_kvadar.povrsina(30) #vraca 0
+z = ops_pred_18_kvadar.zapremina(k)
+
+print(k)
+print(p)
+print(p1) #vraca 0
+print(z)
