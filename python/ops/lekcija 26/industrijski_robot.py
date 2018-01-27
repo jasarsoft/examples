8f1e81010e1d6df9d17f0866434c082a9800a56b
Edin Jašarević <edinjasar14@gmail.com>
2018-01-27 22:07:52 +0100

    py ops lekcija26 industrijski robot

---
+++ python/ops/lekcija 26/industrijski_robot.py
@@ -0,0 +1,73 @@
+import math
+
+def IndustrijskiRobot(proizvodjac, model, godina, sslk, potrosnja, sati, vp):
+    if godina < 2000 or sslk < 1 or potrosnja < 1 or sati < 1:
+        return -1
+
+    if vp not in ["hidraulicni pogon", "elektromotorni pogon", "magnetni linearni aktuator"]:
+        return -1
+
+    return {
+        "proizvodjac"                       : proizvodjac,
+        "model"                             : model,
+        "godina_proizvodnje"               : godina,
+        "broj_stepeni_slobode_kretanja"     : sslk,
+        "potrosnja_elektricne_energije"     : potrosnja,
+        "maksimalan_broj_sati_rada"         : sati,
+        "vrsta_pogona_pokretnih_djelova"    : vp
+        }
+
+def efikasnost(ir):
+    if type(ir) != dict:
+        return -1
+
+    if ir['vrsta_pogona_pokretnih_djelova'] == "hidraulicni pogon":
+        L = 1.33
+    elif ir['vrsta_pogona_pokretnih_djelova'] == "elektromotorni pogon":
+        L = 2.00
+    else:
+        L = 1.00
+
+    return (ir['potrosnja_elektricne_energije'] * L) / ((ir['broj_stepeni_slobode_kretanja'] - 2) * math.sqrt(ir['maksimalan_broj_sati_rada']))
+
+def load(imeDatoteke):
+    lista = []
+
+    dat = open(imeDatoteke, "r")
+
+    while True:
+        try:
+            potrosnja = float(dat.read(8).strip())
+            sati = int(dat.read(8).strip())
+            godina = int(dat.read(5).strip())
+            proizvodjac = dat.readline().strip()
+            sslk = int(dat.read(8).strip())
+            vrstaPogonaAkronim = dat.read(8).strip()
+            model = dat.readline().strip()
+
+            if vrstaPogonaAkronim == "MLA":
+                vp = "magnetni linearni aktuator"
+            elif vrstaPogonaAkronim == "EMP":
+                vp = "elektromotorni pogon"
+            else:
+                vp = "hidraulicni pogon"
+
+            ir = IndustrijskiRobot(proizvodjac, model, godina, sslk, potrosnja, sati, vp)
+
+            if (ir == -1):
+                print("Doslo je do greske prilikom ucitavanja jedno industrijskog robota")
+            else:
+                lista.append(ir)
+        except ValueError:
+            break
+
+    dat.close()
+    
+    for j in range(len(lista)-1):
+        for i in range(len(lista)-1):
+            if efikasnost(lista[i]) > efikasnost(lista[i+1]):
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+                
+    return lista
