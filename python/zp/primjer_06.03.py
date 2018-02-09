078805ada21ffb8e514f5192be5a2b9e528adc69
Edin Jašarević <edinjasar14@gmail.com>
2018-02-09 14:55:22 +0100

    py zp primjer 06.03

---
+++ python/zp/primjer_06.03.py
@@ -0,0 +1,15 @@
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''Ovo je string u vise linja.
+Ovo je drugal linija.'''
+print(s)
+
+s = 'Ovo je string. \
+Ovo je nastavak string-a'
+print(s)
+
+print\
+       (i)
