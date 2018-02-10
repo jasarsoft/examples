965d4a5f12580ad500deabae59cfb061643b5883
Edin Jašarević <edinjasar14@gmail.com>
2018-02-10 15:48:12 +0100

    py zp primjer 11.05

---
+++ python/zp/primjer_11.05.py
@@ -0,0 +1,7 @@
+listakupovine = ['jabuka', 'manog', 'sargarepa', 'banana']
+
+print(listakupovine[::1])
+print(listakupovine[::2])
+print(listakupovine[::3])
+print(listakupovine[::-1])
+print(listakupovine[::-2])
