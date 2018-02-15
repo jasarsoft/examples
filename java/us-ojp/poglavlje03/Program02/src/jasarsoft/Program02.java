304956455bbd2f7e2c1902bf0491b4eb2e29a1f6
Edin Jašarević <edinjasar14@gmail.com>
2018-02-15 22:58:08 +0100

    java us-ojp pog03 pr02

---
+++ java/us-ojp/poglavlje03/Program02/src/jasarsoft/Program02.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+import java.util.*;
+
+public class Program02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double kamatnaStopa = 20.0 / 12; //kamatnaStopa = 1.666...
+        System.out.println("Mjesecna kamata: " + kamatnaStopa);
+        System.out.println();
+        System.out.printf("Mjesecna kamata: %5.2f\n", kamatnaStopa);         
+    }
+    
+}
