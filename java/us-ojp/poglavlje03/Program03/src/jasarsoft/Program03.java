ad50dfaa553b74d3475f8e3940355fa811b28897
Edin Jašarević <edinjasar14@gmail.com>
2018-02-15 22:58:59 +0100

    java us-ojp pog03 pr03

---
+++ java/us-ojp/poglavlje03/Program03/src/jasarsoft/Program03.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+
+public class Program03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n = 23;
+        
+        System.out.printf("1. broj: %d\n", n);
+        System.out.printf("2. broj: %8d\n", n);
+        System.out.printf("Kvadrat broja %d je %8d\n", n, n*n);
+    }
+    
+}
