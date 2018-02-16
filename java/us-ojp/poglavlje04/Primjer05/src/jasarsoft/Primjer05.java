3a36e59ee8645a16223c349dc952aa9661060acc
Edin Jašarević <edinjasar14@gmail.com>
2018-02-16 08:11:18 +0100

    java us-ojp pog04 pr05

---
+++ java/us-ojp/poglavlje04/Primjer05/src/jasarsoft/Primjer05.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+public class Primjer05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for (int broj = 0; broj < 10; broj = broj + 1)
+            System.out.print(broj + " "); //tijelo petlje
+        
+        System.out.println(); //pomjeranje kursora u novi red
+    }
+    
+}
