fdfd83225666260c00d9bb462e090a784a535e7c
Edin Jašarević <edinjasar14@gmail.com>
2018-03-02 00:09:35 +0100

    cpp dcpp pog03 pr03

---
+++ cpp/dcpp/Poglavlje 03/Primjer 03/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Pojavom jezika C++ ostvaren je "
+		"tisucljetni san svih programera" << endl;
+
+	system("pause");
+	return 0;
+}
