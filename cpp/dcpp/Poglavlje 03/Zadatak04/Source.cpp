3482717192282894e24ec460e6679a11a1751a63
Edin Jašarević <edinjasar14@gmail.com>
2018-03-09 15:55:36 +0100

    cpp dcpp pog03 zad04

---
+++ cpp/dcpp/Poglavlje 03/Zadatak04/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Zadatak.  Odredite ?to æe se ispisati na zaslonu po izvoðenju sljedeæeg kôda:
+*/
+
+int main()
+{
+	float a = 2.71;
+	float b = (int)a;
+	cout << b << endl; //2
+
+	system("pause");
+	return 0;
+}
