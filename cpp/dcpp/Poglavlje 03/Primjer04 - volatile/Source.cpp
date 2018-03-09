1a70031e1c7cea28443ab2099c5d7a65fec73c22
Edin Jašarević <edinjasar14@gmail.com>
2018-03-09 15:55:52 +0100

    cpp dcpp pog03 pr04

---
+++ cpp/dcpp/Poglavlje 03/Primjer04 - volatile/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+*/
+
+int main()
+{
+	volatile int izadjiVan = 0;
+	while (!izadjiVan) {
+		// cekaj dok se vrijednost izadjiVan ne postavi na 1
+	}
+
+	system("pause");
+	return 0;
+}
