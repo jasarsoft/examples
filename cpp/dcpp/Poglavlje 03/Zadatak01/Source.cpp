e67712bdd2df3d24dfa12f385a9eeb6021284e58
Edin Jašarević <edinjasar14@gmail.com>
2018-03-09 15:55:06 +0100

    cpp dcpp pog03 zad01

---
+++ cpp/dcpp/Poglavlje 03/Zadatak01/Source.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+	Sto ce ispisati sljedece naredbe:
+	int a = 10;
+	float b = 10.;
+	cout << a / 3 << endl;
+	cout << b / 3 << endl;
+*/
+
+int main()
+{
+	int a = 10;
+	float b = 10;
+
+	cout << a / 3 << endl; // 3
+	cout << b / 3 << endl; //3.33
+
+	system("pause");
+	return 0;
+}
