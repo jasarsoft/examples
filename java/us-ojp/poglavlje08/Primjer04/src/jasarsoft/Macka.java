3f6f4f574277de74fab6ef29f89a7de68c4a549a
Edin Jašarević <edinjasar14@gmail.com>
2018-03-10 20:29:23 +0100

    java us-ojp pog09 pr04

---
+++ java/us-ojp/poglavlje08/Primjer04/src/jasarsoft/Macka.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+public class Macka extends KucniLjubimac {
+    public void koSamJa(){
+        System.out.println("Ja sam macka.");
+    }
+}
---
+++ java/us-ojp/poglavlje08/Primjer04/src/jasarsoft/Primjer04.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+public class Primjer04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KucniLjubimac ljubimac1 = new KucniLjubimac();
+        KucniLjubimac ljubimac2 = new Pas();
+        KucniLjubimac ljubimac3 = new Macka();
+        
+        ljubimac1.koSamJa();
+        ljubimac2.koSamJa();
+        ljubimac3.koSamJa();
+    }
+    
+}
