f16fa7f3dd6063c63325fb480b14b33bb2a90144
Edin Jašarević <edinjasar14@gmail.com>
2018-03-10 20:40:11 +0100

    java us-ojp pog09 pr04

---
+++ java/us-ojp/poglavlje09/Primjer04/src/jasarsoft/KlasicanRadio.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+public interface KlasicanRadio extends Radio {
+    double B92= 92.5;
+    void izaberiStanicu();
+}
