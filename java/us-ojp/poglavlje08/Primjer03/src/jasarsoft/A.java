9d8507f86e540b5703894d2dc94e914bc50cd565
Edin Jašarević <edinjasar14@gmail.com>
2018-03-10 20:28:51 +0100

    java us-ojp pog08 pr03

---
+++ java/us-ojp/poglavlje08/Primjer03/src/jasarsoft/A.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+public class A {
+    public void m(){
+        System.out.println("m()");
+    }
+}
---
+++ java/us-ojp/poglavlje08/Primjer03/src/jasarsoft/B.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+
+/**
+ *
+ * @author jasarsoft
+ */
+public class B extends A {
+    public void m(int x){
+        System.out.println("m(int x)");
+    }
+    
+    public void m(String y){
+        System.out.println("m(String y)");
+    }
+}
