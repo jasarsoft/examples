edae6681195e655b328ef546e05ebcd05e98dad9
Edin Jašarević <edinjasar14@gmail.com>
2018-03-12 14:36:50 +0100

    java us-ojp pog10 prazan okvir

---
+++ java/us-ojp/poglavlje10/PrazanOkvir/src/jasarsoft/PrazanOkvir.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jasarsoft;
+import javax.swing.*;
+/**
+ *
+ * @author jasarsoft
+ */
+public class PrazanOkvir {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame okvir = new JFrame("Prazan okvir");
+        okvir.setSize(300, 300);
+        okvir.setLocation(100, 150);
+        okvir.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okvir.setVisible(true);
+    }
+    
+}
