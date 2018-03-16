d8c3679e29384029a18c6e4cdc6688a9540ec5fe
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 16:10:24 +0100

    cs - ss pog02 pr07

---
+++ cs/ss07/Poglavlje02/Primjer07/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Primjer07
+{
+    class TempClass
+    {
+        public int Counter;
+
+        public TempClass(int x)
+        {
+            Counter = x;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int val = 5;
+
+            Console.WriteLine("Prije poziva funkcije: " + val);
+
+            PromjeniVal(val);
+            Console.WriteLine("Poslije poziva funkcije: " + val);
+            Console.WriteLine();
+
+            TempClass tmp = new TempClass(5);
+            Console.WriteLine("Prije poziva funkcije: " + tmp.Counter);
+
+            PromjeniRef(tmp);
+            Console.WriteLine("Polije poziva funkcije: " + tmp.Counter);
+        }
+
+        static void PromjeniVal(int value)
+        {
+            value = 10;
+        }
+
+        static void PromjeniRef(TempClass value)
+        {
+            value.Counter = 10;
+        }
+    }
+}
