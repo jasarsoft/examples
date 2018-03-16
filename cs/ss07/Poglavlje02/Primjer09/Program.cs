2189c4a0bbb4c93458a765f2e80efbdd2711b6b7
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 16:12:43 +0100

    cs - ss pog02 pr09 boxing

---
+++ cs/ss07/Poglavlje02/Primjer09/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Primjer09
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 100;
+            object o = i;
+            i = 200;
+
+            Console.WriteLine("Tip int: {0}", i);
+            Console.WriteLine("Tip object: {0}", o);
+        }
+    }
+}
