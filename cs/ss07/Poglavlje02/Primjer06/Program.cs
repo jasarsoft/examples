559020d4e000c6b3b2829ce2251cf002c641fb39
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 15:43:11 +0100

    cs - ss pog02 pr06

---
+++ cs/ss07/Poglavlje02/Primjer06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            float f = (float)Math.Cos(0.00001);
+            Console.WriteLine("Float: " + f);
+
+            double d = Math.Cos(0.00001);
+            Console.WriteLine("Double: " + d);
+
+            decimal m = (decimal)Math.Cos(0.00001);
+            Console.WriteLine("Decimal: " + m);
+        }
+    }
+}
