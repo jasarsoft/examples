4b5337c958829ff84b48ec065af98d9f05f877f3
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 14:39:27 +0100

    cs - ss primjer03

---
+++ cs/ss07/poglavlje01/Primjer03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+                return;
+
+            int r1 = Convert.ToInt32(args[0]);
+            int r2 = Convert.ToInt32(args[1]);
+
+            Console.WriteLine("{0} Ohm", r1 * r2 / (r1 + r2));
+        }
+    }
+}
