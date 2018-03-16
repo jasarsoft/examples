1381f17f5a63e6707fe1263f10ec7b048a4d0f56
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 16:21:32 +0100

    cs - ss pog02 pr11 toString

---
+++ cs/ss07/Poglavlje02/Primjer11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primjer11
+{
+    class TempClass
+    {
+        private int x;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int i = 35;
+            TempClass tmp = new TempClass();
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(tmp.ToString());
+        }
+    }
+}
