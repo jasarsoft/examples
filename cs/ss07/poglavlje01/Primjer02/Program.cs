19b2d41672cd7c2712121febd2a721fed69fae53
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 14:33:01 +0100

    cs - ss primjer02

---
+++ cs/ss07/poglavlje01/Primjer02/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
