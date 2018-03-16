87c189a46f7b22788bd0cfcf1affce3691395aa7
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 14:58:49 +0100

    cs - ss pog02 pr02

---
+++ cs/ss07/Poglavlje02/Primjer02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (uint counter = 10; counter >= 0; counter--)
+                Console.WriteLine(counter);
+        }
+    }
+}
