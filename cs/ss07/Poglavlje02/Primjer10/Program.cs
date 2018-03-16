16a2f17c87efa8c825e61f881a1a4e40d05b1fa8
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 16:19:29 +0100

    cs - ss pog02 pr10 unboxing

---
+++ cs/ss07/Poglavlje02/Primjer10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 100;
+            object o = i;
+
+            int j = (int)o;
+
+            Console.WriteLine("Tip int: {0}", j);
+        }
+    }
+}
