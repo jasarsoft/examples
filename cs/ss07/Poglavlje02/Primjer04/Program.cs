b7b5c2572eb6e587551ed51ec26594607b9d1220
Edin Jašarević <edinjasar14@gmail.com>
2018-03-16 15:07:12 +0100

    cs - ss pog02 pr04

---
+++ cs/ss07/Poglavlje02/Primjer04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 10;
+
+            for(int y = 0; y < 10; y++)
+            {
+                int z = 20;
+            }
+
+            //Console.WriteLine("y = " + y); //nece se prevesti
+            //Console.WriteLine("z = " + z); //nece se prevesti
+            Console.WriteLine("x = " + x); //ok
+        }
+    }
+}
