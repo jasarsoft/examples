68e633a5906ac70c98147094f187c51b689a0a78
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 11:16:47 +0100

    cs - ss pog03 pr03

---
+++ cs/ss07/Poglavlje03/Primjer03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Program
+    {
+        static void Main()
+        {
+            double result, kut = Math.PI / 6;
+
+            result = kut < Math.PI ? Math.Sin(kut) : Math.Cos(kut);
+
+            Console.WriteLine(result);
+        }
+    }
+}
