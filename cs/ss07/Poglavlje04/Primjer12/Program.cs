306b9b82f36bc5e002903914efaa58a99e25dc14
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:43:53 +0100

    cs - ss pog04 pr12 while

---
+++ cs/ss07/Poglavlje04/Primjer12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Primjer12
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x;
+
+            Random nekiBroj = new Random();
+
+            while ((x = nekiBroj.Next(100)) != 50)
+                Console.WriteLine(x);
+        }
+    }
+}
