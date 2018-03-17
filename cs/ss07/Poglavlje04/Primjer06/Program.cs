93ca63e9ad89748765d28bf74629c76e09114737
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:16:46 +0100

    cs - ss pog04 pr06

---
+++ cs/ss07/Poglavlje04/Primjer06/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text;
+            int exponent = 6;
+
+            switch (exponent)
+            {
+                case -12:
+                    text = "piko";
+                    break;
+                case -9:
+                    text = "nano";
+                    break;
+                case -6:
+                    text = "mikro";
+                    break;
+                case -3:
+                    text = "mili";
+                    break;
+                case 3:
+                    text = "kilo";
+                    break;
+                case 6:
+                    text = "mega";
+                    break;
+                case 9:
+                    text = "giga";
+                    break;
+                case 12:
+                    text = "tera";
+                    break;
+                default:
+                    text = exponent < 0 ? "premala velicina" : "prevelika velicina";
+                    break;
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
