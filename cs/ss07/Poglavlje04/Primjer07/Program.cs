086700882c9e687e14e870a6d4262196e114af8d
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:33:25 +0100

    cs - ss pog04 pr07

---
+++ cs/ss07/Poglavlje04/Primjer07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Primjer07
+{
+    class Program
+    {
+        static void Main()
+        {
+            int zbroj = 0, max = 1000;
+
+            for (int i = 1; i <= max; i++)
+                zbroj += i;
+
+            Console.WriteLine("Zbroj prvih {0} brojeva je {1}", max, zbroj);
+        }
+    }
+}
