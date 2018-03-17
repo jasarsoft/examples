1212442b1d2b40e6243b81ec3f2dca619d740cee
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:33:35 +0100

    cs - ss pog04 pr08

---
+++ cs/ss07/Poglavlje04/Primjer08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primjer08
+{
+    class Program
+    {
+        static void Main()
+        {
+            string s = "HyperTextMarkupLanguage";
+
+            for (int i = 0; i < s.Length && s[i] != 'M'; i++)
+                Console.Write(s[i]);
+        }
+    }
+}
