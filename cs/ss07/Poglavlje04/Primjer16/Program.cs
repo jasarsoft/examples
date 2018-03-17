5ba2dc98a45e8be854d962c6dc9d2e53326a220a
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:56:06 +0100

    cs - ss pog04 pr16 for-for-break

---
+++ cs/ss07/Poglavlje04/Primjer16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primjer16
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i, j;
+
+            for(i = 0; i < 100; i++)
+            {
+                for(j = 0; j < 100; j++)
+                {
+                    if (i + j == 10)
+                        break;
+                }
+            }
+        }
+    }
+}
