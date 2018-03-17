11881d36b10806ec0f58895b8cf99b83ea553c63
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:54:13 +0100

    cs - ss pog04 pr15 break

---
+++ cs/ss07/Poglavlje04/Primjer15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Primjer15
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i;
+            long faktorijel = 1;
+
+            for (i = 1; i < 100; i++)
+            {
+                if(faktorijel > 1000000)
+                {
+                    faktorijel /= (i - 1);
+                    break;
+                }
+
+                faktorijel *= i;
+            }
+
+            Console.WriteLine("{0}! = {1}", i - 2, faktorijel);
+        }
+    }
+}
