7c77e3cc54a388b7dd0014bf4193177fe5049c14
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:33:45 +0100

    cs - ss pog04 pr09

---
+++ cs/ss07/Poglavlje04/Primjer09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Primjer09
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool flagPrim = true;
+            const int MAX = 1000;
+            int brojPrimova = 0;
+
+            for (int i = 1; i < MAX; i++)
+            {
+                for (int j = 2; j < i -1; j++)
+                    if(i % j == 0)
+                    {
+                        flagPrim = false;
+                        break;
+                    }
+
+                if (flagPrim)
+                    brojPrimova++;
+
+                flagPrim = true;
+            }
+
+            Console.WriteLine("Broj prim brojeva do {0} je {1}", MAX, brojPrimova);
+        }
+    }
+}
