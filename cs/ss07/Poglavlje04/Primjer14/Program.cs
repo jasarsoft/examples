1c6341dc7fd2b3c0d75a57fe676106cedbc642b9
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:49:53 +0100

    cs - ss pog04 pr14 do-while

---
+++ cs/ss07/Poglavlje04/Primjer14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Primjer14
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool flagImportant = false;
+            Random nekiBroj = new Random();
+
+            do
+            {
+                if(!flagImportant)
+                {
+                    flagImportant = !flagImportant;
+                    //vrlo vazan kod...
+                }
+
+                //...
+                //...
+            } while (nekiBroj.Next(100) != 50);
+        }
+    }
+}
