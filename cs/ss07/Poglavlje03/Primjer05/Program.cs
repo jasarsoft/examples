197b0a5b8c2e0ddd6d01869bd9218ea95b44c829
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 11:25:11 +0100

    cs - ss pog03 pr05

---
+++ cs/ss07/Poglavlje03/Primjer05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1900; i < 2100; i++)
+            {
+                if (!(i % 400 == 0) && !(i % 4 == 0 && i % 100 != 0))
+                    Console.WriteLine(i + " nije prestupna");
+                else
+                    Console.WriteLine(i + " je prestupna");
+            }
+        }
+    }
+}
