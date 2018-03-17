07e98eeeb18b541613aecaa386e9197a4c986029
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 11:16:26 +0100

    cs - ss pog03 pr02

---
+++ cs/ss07/Poglavlje03/Primjer02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 7, y = 5;
+
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}", 
+                (x += y), (x *= y), (x /= y), (x -= 2), (x %= y));
+        }
+    }
+}
