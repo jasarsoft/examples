f5a50657e3520f9e83f13f634abf6ce43169ce37
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 11:29:14 +0100

    cs - ss pog03 pr06

---
+++ cs/ss07/Poglavlje03/Primjer06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            int flag = 0;
+            bool uvijet = false;
+
+            if (uvijet && VeryImportant())
+                flag = 1;
+
+        }
+
+        static bool VeryImportant()
+        {
+            int x = 0;
+            //obavljanje vrlo vaznog posla
+            return x == 0 ? true : false;
+        }
+    }
+}
