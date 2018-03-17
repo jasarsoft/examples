7bbc7a524f9b7042cb30dfadb1c30b9418cd1af1
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:16:03 +0100

    cs - ss pog04 pr03

---
+++ cs/ss07/Poglavlje04/Primjer03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Program
+    {
+        static void Main()
+        {
+            uint plasman = 1;
+            string doseg;
+
+            if (plasman == 1)
+                doseg = "Svijetski prvak";
+            else if (plasman <= 2)
+                doseg = "Finale";
+            else if (plasman <= 4)
+                doseg = "Polufinale";
+            else if (plasman <= 8)
+                doseg = "Cetvrtfinale";
+            else if (plasman <= 16)
+                doseg = "Osmina finala";
+            else if (plasman <= 32)
+                doseg = "Ispadanje u skupini";
+            else
+                doseg = "Nisu se kvalificirali na Svijetsko prvenstvo";
+
+            Console.WriteLine(doseg);
+        }
+    }
+}
