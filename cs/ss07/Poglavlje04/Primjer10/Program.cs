6335976fac6d04aa21e3c5de22bfec6720459ca3
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:38:54 +0100

    cs - ss pog04 pr10 while

---
+++ cs/ss07/Poglavlje04/Primjer10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Primjer10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            string s = "HAL";
+
+            while(i++ < s.Length)
+            {
+                Console.Write((char)(s[i-1] + 1));
+            }
+        }
+    }
+}
