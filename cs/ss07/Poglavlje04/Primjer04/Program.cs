6372bccf0bc95e7167f76b245733673dd0a2c27b
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:16:22 +0100

    cs - ss pog04 pr04

---
+++ cs/ss07/Poglavlje04/Primjer04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 1;
+            string text;
+
+            if (x >= 0)
+                if (x == 0)
+                    text = "x je 0";
+                else
+                    text = "x je veci od 0";
+            else
+                text = "x je manji od 0";
+
+            Console.WriteLine(text);
+        }
+    }
+}
