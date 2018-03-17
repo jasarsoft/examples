2b320872514eb74fd6e8f89b3133a95810e8be18
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 14:56:51 +0100

    cs - ss pog04 pr01 if

---
+++ cs/ss07/Poglavlje04/Primjer01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 20, y, z;
+
+            if(x == 20)
+            {
+                y = 10;
+                z = 100;
+            }
+            else
+            {
+                y = 11;
+                z = 101;
+            }
+
+            Console.WriteLine(y + z);
+        }
+    }
+}
