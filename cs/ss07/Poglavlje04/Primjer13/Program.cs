df864d65f18c5ac94ed73768b1aee5558738862c
Edin Jašarević <edinjasar14@gmail.com>
2018-03-17 15:46:54 +0100

    cs - ss pog04 pr13

---
+++ cs/ss07/Poglavlje04/Primjer13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primjer13
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool flagImportant = false;
+            Random nekiBroj = new Random();
+
+            while(nekiBroj.Next(100) != 50)
+            {
+                if(flagImportant)
+                {
+                    flagImportant = true;
+                    //vazan kod...
+                }
+
+                //....
+            }
+        }
+    }
+}
