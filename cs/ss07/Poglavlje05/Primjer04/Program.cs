1848cfed8356ad2b930bdfb632b8cb8fe18f0c18
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:44:23 +0100

    cs - ss pog05 pr04

---
+++ cs/ss07/Poglavlje05/Primjer04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            TempClass x = new TempClass();
+            Console.WriteLine(x.BrojInstanci());
+
+            TempClass y = new TempClass();
+            Console.WriteLine(y.BrojInstanci());
+
+            TempClass z = new TempClass();
+            Console.WriteLine(z.BrojInstanci());
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje05/Primjer04/TempClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Primjer04
+{
+    class TempClass
+    {
+        private static int counter;
+
+        public TempClass()
+        {
+            counter++;
+        }
+
+        public int BrojInstanci()
+        {
+            return counter;
+        }
+    }
+}
