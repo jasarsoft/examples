6b5fe32518bc54d9804954747dfb70eae1d988ca
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:40:23 +0100

    cs - ss pog04 pr19

---
+++ cs/ss07/Poglavlje04/Primjer19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer19
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i, j = 0;
+
+            for(i = 0; i < 100; i++)
+            {
+                for(j = 0; j < 100; j++)
+                {
+                    if (i + j == 10)
+                        goto KRAJ;
+                }
+            }
+
+            KRAJ: Console.WriteLine("{0} {1}", i, j);
+        }
+    }
+}
