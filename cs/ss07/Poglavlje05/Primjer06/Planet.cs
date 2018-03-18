b211f8611eef4fd2d55b0031fa221b27d7604c5e
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:44:44 +0100

    cs - ss pog05 pr06

---
+++ cs/ss07/Poglavlje05/Primjer06/Planet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Primjer06
+{
+    class Planet
+    {
+        private bool life;
+        private bool atmosfera;
+        private string nazivPlaneta;
+
+        public Planet(string name, bool lf, bool atm)
+        {
+            nazivPlaneta = name;
+            life = lf;
+            atmosfera = atm;
+        }
+
+        public bool Life
+        {
+            get
+            {
+                return life;
+            }
+            set
+            {
+                life = value;
+            }
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje05/Primjer06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            Planet uranius = new Planet("Uran", false, true);
+            uranius.Life = true;
+
+            Console.WriteLine("Na Uranu {0} izovota", uranius.Life == true ? "ima" : "nema");
+        }
+    }
+}
