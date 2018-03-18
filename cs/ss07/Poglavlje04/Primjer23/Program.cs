819112b0400a5a84e5c1aab404d87bcf6bcbe3cc
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:41:17 +0100

    cs - ss pog04 pr23

---
+++ cs/ss07/Poglavlje04/Primjer23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Primjer23
+{
+    class Program
+    {
+        static void Main()
+        {
+            object[] arr = new object[] { 2001, "Oddisey", 9.5, "weeks" };
+
+            foreach(object o in arr)
+            {
+                Console.Write(o);
+                if (o.GetType() == typeof(string))
+                    Console.WriteLine();
+            }
+        }
+    }
+}
