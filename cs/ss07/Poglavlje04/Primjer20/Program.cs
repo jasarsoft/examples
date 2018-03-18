9e57b189e2d770fcffb1ab983f3462e160333204
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:40:37 +0100

    cs - ss pog04 pr20

---
+++ cs/ss07/Poglavlje04/Primjer20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Primjer20
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i, j = 0;
+            bool flag = false;
+
+            for(i = 0; i < 100 && !flag; i++)
+            {
+                for(j = 0; j < 100; j++)
+                {
+                    if(i + j == 10)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+
+            //i ce biti povecan nakon break, pa ga treba samnjiti za 1
+            Console.WriteLine("{0} {1}", --i, j);
+        }
+    }
+}
