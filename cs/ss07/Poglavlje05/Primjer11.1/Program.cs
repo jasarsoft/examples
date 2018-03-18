16f5d185bd9d3e3227557bcc42786554400804b8
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 12:12:17 +0100

    cs - ss pog05 pr11.1

---
+++ cs/ss07/Poglavlje05/Primjer11.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Primjer11_1
+{
+    interface IComboXP
+    {
+        void Sort();
+    }
+
+    interface IComboVista
+    {
+        void SortAdvanced();
+    }
+
+    class ComboXP : IComboXP
+    {
+        public void Sort()
+        {
+            Console.WriteLine("Sortirano");
+        }
+    }
+
+    class ComboVista : ComboXP, IComboVista
+    {
+        public void SortAdvanced()
+        {
+            Console.WriteLine("Napredno soriranje");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ComboXP[] arr = new ComboXP[4];
+            arr[0] = new ComboXP();
+            arr[1] = new ComboXP();
+            arr[2] = new ComboVista();
+            arr[3] = new ComboVista();
+
+            foreach (ComboXP com in arr)
+            {
+                IComboVista combo = com as IComboVista;
+                if (combo == null)
+                {
+                    IComboXP comboxp = com as IComboXP;
+                    comboxp.Sort();
+                }
+                else
+                    combo.SortAdvanced();
+            }
+        }
+    }
+}
