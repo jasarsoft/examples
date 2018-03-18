eba025616ba733b1927e32f372b104d78b1e216c
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 12:11:56 +0100

    cs - ss pog05 pr10.2

---
+++ cs/ss07/Poglavlje05/Primjer10.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer10._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool flag1 = false, flag2 = false;
+
+            object o = 10;
+            long l = 20;
+
+            if (l is object)
+                flag1 = true;
+
+            if (o is long)
+                flag2 = true;
+
+            Console.WriteLine("flag1 = {0}\nflag2 = {1}", flag1, flag2);
+        }
+    }
+}
