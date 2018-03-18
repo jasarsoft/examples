b81f2bd5cd86c3bf0f1746ae02341a2c1144a6b0
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:44:13 +0100

    cs - ss pog05 pr03

---
+++ cs/ss07/Poglavlje05/Primjer03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Primjer03
+{
+    class Program
+    {
+        private short xCoord;
+        private static short yCoord;
+
+        private void FCoord()
+        {
+            xCoord = 300;
+            yCoord = 400;
+        }
+
+        static void Main()
+        {
+            Program tmp = new Program();
+            tmp.xCoord = 100;               //ok
+            //tmp.yCoord = 200;               //greska
+
+            //Program.xCoord = 100;           //greska
+            Program.yCoord = 100;           //ok
+
+            //FCoord();                       //greska
+        }
+    }
+}
