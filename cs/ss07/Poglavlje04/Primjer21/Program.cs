3dd6c0b7e11fd5bee3ba8bbfe20df9261f801876
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:40:51 +0100

    cs - ss pog04 pr21

---
+++ cs/ss07/Poglavlje04/Primjer21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primjer21
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[] { 1, 3, 5, 7, 9 };
+
+            foreach (int i in arr)
+                Console.Write(i + " ");
+        }
+    }
+}
