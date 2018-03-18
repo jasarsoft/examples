d278442cf4d19e5ef63bb61a5951b8af5766beb8
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:44:03 +0100

    cs - ss pog05 pr02

---
+++ cs/ss07/Poglavlje05/Primjer02/Planet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Planet
+    {
+        private bool life;
+        public bool atmosfera;
+        private string nazivPlaneta;
+
+        public Planet(string name, bool life, bool atm)
+        {
+            nazivPlaneta = name;
+            this.life = life;
+            atmosfera = atm;
+        }
+
+        public void PormjeniLife(bool life)
+        {
+            this.life = life;
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje05/Primjer02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            Planet uranus = new Planet("Uran", false, true);
+        }
+    }
+}
