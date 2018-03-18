dcc94387841ad3148486dc65c1f4ad00f0d84742
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:41:30 +0100

    cs - ss pog04 pr24

---
+++ cs/ss07/Poglavlje04/Primjer24/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Primjer24
+{
+    /* Vise linijski
+     * komentar */
+    class Program
+    {
+        static void Main()
+        {
+            int i, x = 0;
+
+            for (i = 1; i <= 100; i++)
+                x += i; //jednolinijski komentar
+        }
+    }
+}
