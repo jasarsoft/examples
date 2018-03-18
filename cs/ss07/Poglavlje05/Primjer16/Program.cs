8b7f0d9da206ae8fe14938e7663a831dc01e9691
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 12:54:35 +0100

    cs - ss pog05 pr16

---
+++ cs/ss07/Poglavlje05/Primjer16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Westinghouse
+{
+    class Generator
+    {
+        private float powerMW;
+
+        public Generator(float f)
+        {
+            powerMW = f;
+        }
+    }
+}
+
+namespace Koncar
+{
+    class Generator
+    {
+        private float snagaMW;
+
+        public Generator(float f)
+        {
+            snagaMW = f;
+        }
+    }
+}
+
+namespace Primjer16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Westinghouse.Generator w = new Westinghouse.Generator(100);
+        }
+    }
+}
