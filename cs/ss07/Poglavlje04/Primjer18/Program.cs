e141d72c9a10a020e76d56cbb690d4abb288138e
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:40:02 +0100

    cs - ss pog04 pr18

---
+++ cs/ss07/Poglavlje04/Primjer18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer18
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i, j = 0;
+
+            for(i = 0; i < 100; i++)
+            {
+                for(j = 0; j < 100; j++)
+                {
+                    if (i + j == 10)
+                        break;
+                }
+            }
+
+            Console.WriteLine("{0} {1}", i, j);
+        }
+    }
+}
