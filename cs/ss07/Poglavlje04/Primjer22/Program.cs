7a0bf6d558635510f7e7bc8aa3d64139bd004214
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:41:04 +0100

    cs - ss pog04 pr22

---
+++ cs/ss07/Poglavlje04/Primjer22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Primjer22
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] arr = new string[] { "To", "be", "or", "not", "to", "be" };
+
+            foreach (string s in arr)
+                Console.Write(s + " ");
+        }
+    }
+}
