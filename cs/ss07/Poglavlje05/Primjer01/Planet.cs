d9dad69fe8c6b8ec47733931c01f1ef13d850ef2
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:43:52 +0100

    cs - ss pog05 pr01

---
+++ cs/ss07/Poglavlje05/Primjer01/Planet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Planet
+    {
+        private bool life;
+        private bool atmosfera;
+        private sbyte brojSatelita;
+        private int promjer;
+        private float brzinaOkoOsi;
+        private float brzinaOkoSunca;
+        private string nazivPlaneta;
+
+        public Planet(string name, sbyte brojsat)
+        {
+            nazivPlaneta = name;
+            brojSatelita = brojsat;
+        }
+
+        public Planet(string name, bool life, bool atm)
+        {
+            nazivPlaneta = name;
+            this.life = life;
+            atmosfera = atm;
+        }
+
+        public Planet(string name, bool life, bool atm, sbyte brojsat, int promjer, float brzina1, float brzina2)
+        {
+            nazivPlaneta = name;
+            this.life = life;
+            atmosfera = atm;
+            brojSatelita = brojsat;
+            this.promjer = promjer;
+            brzinaOkoOsi = brzina1;
+            brzinaOkoSunca = brzina2;
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje05/Primjer01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Planet earth = new Planet("Zemlja", true, true, 1, 6370, 0.464F, 29.7F);
+            Planet mercury = new Planet("Merkur", false, false);
+            Planet mars = new Planet("Mars", 2);
+        }
+    }
+}
