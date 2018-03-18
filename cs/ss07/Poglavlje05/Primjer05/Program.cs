54132de7c4cd46a3138f85c01a296d1f7e5256a5
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:44:33 +0100

    cs - ss pog05 pr05

---
+++ cs/ss07/Poglavlje05/Primjer05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Prije kreiranje objekta");
+            TempClass x = new TempClass(5);
+            Console.WriteLine("Poslije kreiranje objekta");
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje05/Primjer05/TempClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Primjer05
+{
+    class TempClass
+    {
+        private int x;
+
+        static TempClass()
+        {
+            Console.WriteLine("Staticki konstruktor");
+        }
+
+        public TempClass(int x)
+        {
+            this.x = x;
+            Console.WriteLine("Kreiranje objekta");
+        }
+    }
+}
