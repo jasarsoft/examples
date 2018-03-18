7809e1ae6c75ee1b1d899898b54300d71b92ed98
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 12:53:59 +0100

    cs - ss pog05 pr14

---
+++ cs/ss07/Poglavlje05/Primjer14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Primjer14
+{
+    class Vanjska
+    {
+        private int x = 100;
+
+        public class Unutarnja
+        {
+            public void fIn()
+            {
+                Vanjska v = new Vanjska();
+                Console.WriteLine(v.x);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Vanjska.Unutarnja u = new Vanjska.Unutarnja();
+            u.fIn();
+        }
+    }
+}
