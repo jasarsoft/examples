f95230223e7dff3da06e05ad0583338a72cb59fb
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 12:53:21 +0100

    cs - ss pog05 pr12

---
+++ cs/ss07/Poglavlje05/Primjer12/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Primjer12
+{
+    struct SKoordinate
+    {
+        public int xCoord, yCoord;
+
+        public SKoordinate(int x, int y)
+        {
+            xCoord = x;
+            yCoord = y;
+        }
+    }
+
+    class CKoordinate
+    {
+        public int xCoord, yCoord;
+
+        public CKoordinate(int x, int y)
+        {
+            xCoord = x;
+            yCoord = y;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            SKoordinate s = new SKoordinate(800, 600);
+            CKoordinate c = new CKoordinate(1024, 768);
+
+            PromjeniXYs(s);
+            PromjeniXYc(c);
+
+            Console.WriteLine("s: " + s.xCoord + ", " + s.yCoord);
+            Console.WriteLine("c: " + c.xCoord + ", " + c.yCoord);
+        }
+
+        static void PromjeniXYs(SKoordinate sTmp)
+        {
+            sTmp.xCoord = 100;
+            sTmp.yCoord = 200;
+        }
+
+        static void PromjeniXYc(CKoordinate cTmp)
+        {
+            cTmp.xCoord = 100;
+            cTmp.yCoord = 200;
+        }
+    }
+}
