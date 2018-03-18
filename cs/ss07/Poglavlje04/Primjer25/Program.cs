5818f9e11f8c2d33075f8f8a8ac6ae4dd556cb31
Edin Jašarević <edinjasar14@gmail.com>
2018-03-18 10:41:43 +0100

    cs - ss pog04 pr25

---
+++ cs/ss07/Poglavlje04/Primjer25/Program.cs
@@ -0,0 +1,24 @@
+﻿#define NAPON
+#define STRUJA
+
+using System;
+
+
+namespace Primjer25
+{
+    class Program
+    {
+        static void Main()
+        {
+            #if (NAPON && STRUJA)
+                Console.WriteLine("Otpor se moze izracunati");
+            #elif (NAPON && !STRUJA)
+                Console.WriteLine("Otpor se ne moze izracunati");
+            #elif (!NAPON && STRUJA)
+                Console.WriteLine("Otpor se ne moze izracunati");
+            #elif (!NAPON && !STRUJA)
+                Console.WriteLine("Otpor se ne moze izracunati");
+            #endif
+        }
+    }
+}
