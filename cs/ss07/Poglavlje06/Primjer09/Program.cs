1794610f0016ecb14118ae559e74d803c9ea0810
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:27:06 +0100

    cs - ss pog06 pr09 rekurzija

---
+++ cs/ss07/Poglavlje06/Primjer09/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Primjer09
+{
+    class Program
+    {
+        static long Faktorijel(int n)
+        {
+            long result;
+
+            if (n == 1)
+                return 1;
+
+            result = Faktorijel(n - 1) * n;
+
+            return result;
+        }
+
+        static void Main()
+        {
+            int i = 10;
+            Console.WriteLine("{0}! = {1}", i, Faktorijel(i));
+        }
+    }
+}
