c64382edc3db5bb9a16571b6dade535946c1ec4d
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:18:12 +0100

    cs - ss pog06 pr07 out parametar

---
+++ cs/ss07/Poglavlje06/Primjer07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Primjer07
+{
+    class Program
+    {
+        static void Promjeni(out int i)
+        {
+            i = 11;
+            return;
+        }
+
+        static void Main()
+        {
+            int x;
+            Promjeni(out x);
+            Console.WriteLine(x);
+        }
+    }
+}
