3fad1caadf75735634abf4ff67cabecb2263111d
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:14:25 +0100

    cs - ss pog06 pr01 metod

---
+++ cs/ss07/Poglavlje06/Primjer01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static double KompleksnaVrijednost(double real, double im)
+        {
+            double d = Math.Sqrt(real * real + im * im);
+            return d;
+        }
+        static void Main()
+        {
+            double real = 6.7, imagine = 5.3;
+            double apsolute = KompleksnaVrijednost(real, imagine);
+
+            Console.WriteLine(apsolute);
+
+        }
+    }
+}
