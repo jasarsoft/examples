94edab8b91cbdc20ce982336308315fd2a4171d7
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:17:45 +0100

    cs - ss pog06 pr06 proslijedjivanje reference na klasu

---
+++ cs/ss07/Poglavlje06/Primjer06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Promjeni1(StringBuilder sb)
+        {
+            sb = new StringBuilder("Linux");
+            return;
+        }
+
+        static void Promjeni2(ref StringBuilder sb)
+        {
+            sb = new StringBuilder("Linux");
+            return;
+        }
+
+        static void Main()
+        {
+            StringBuilder sb1 = new StringBuilder("Windows");
+            Promjeni1(sb1);
+            Console.WriteLine(sb1);
+
+            StringBuilder sb2 = new StringBuilder("Windows");
+            Promjeni2(ref sb2);
+            Console.WriteLine(sb2);
+        }
+    }
+}
