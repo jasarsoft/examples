ef8cb79527db46b4eb97231c5955af18e686e727
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:18:50 +0100

    cs - ss pog06 pr08 niz argumenata (params)

---
+++ cs/ss07/Poglavlje06/Primjer08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Primjer08
+{
+    class Program
+    {
+        static double UseParams(params double[] otpori)
+        {
+            if (otpori.Length == 0)
+                return 0;
+            else if (otpori.Length == 1)
+                return otpori[0];
+
+            double ukpniOtpor = otpori[0] * otpori[1] / (otpori[0] + otpori[1]);
+
+            for (int i = 2; i < otpori.Length; i++)
+            {
+                ukpniOtpor = ukpniOtpor * otpori[i] / (ukpniOtpor + otpori[i]);
+            }
+
+            return ukpniOtpor;
+        }
+
+        static void Main()
+        {
+            double r = UseParams(10);
+            Console.WriteLine("{0} Ohm", r);
+
+            r = UseParams(10, 10);
+            Console.WriteLine("{0} Ohm", r);
+
+            r = UseParams(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+            Console.WriteLine("{0} Ohm", r);
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje06/Primjer08/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Primjer08")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Primjer08")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2fc17c7-7af0-4413-bcd3-ac9d40642347")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
