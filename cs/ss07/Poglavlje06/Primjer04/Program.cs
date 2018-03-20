0d392c9f9d5d5c20cf757ebbea3ef586750709da
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:16:15 +0100

    cs - ss pog06 pr04 proslijedjivanje po vrijednosti

---
+++ cs/ss07/Poglavlje06/Primjer04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Promjeni(int i)
+        {
+            i = 11;
+            return;
+        }
+
+        static void Main()
+        {
+            int x = 10;
+            Console.WriteLine(x);
+            Promjeni(x);
+            Console.WriteLine(x);
+        }
+    }
+}
