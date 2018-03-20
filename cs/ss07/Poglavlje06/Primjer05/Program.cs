d024ce7b1c2d245faaf02534475c1ac115f9bd3a
Edin Jašarević <edinjasar14@gmail.com>
2018-03-20 17:16:42 +0100

    cs - ss pog06 pr05 proslijedjivanje po referenci

---
+++ cs/ss07/Poglavlje06/Primjer05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Promjeni(ref int i)
+        {
+            i = 11;
+            return;
+        }
+
+        static void Main()
+        {
+            int x = 10;
+            Console.WriteLine(x);
+            Promjeni(ref x);
+            Console.WriteLine(x);
+        }
+    }
+}
