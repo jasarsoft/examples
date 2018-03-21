b13806e2c9de3548e8ea89afcb2800fd9744dc9d
Edin Jašarević <edinjasar14@gmail.com>
2018-03-21 21:02:50 +0100

    cs - ss pog07 pr01 const

---
+++ cs/ss07/Poglavlje07/Primjer01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Planet
+    {
+        public const double Gravity = 9.80665;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            double masa = 100;
+            double tezina = masa * Planet.Gravity;
+
+            Console.WriteLine("Tezina = " + tezina + " N");
+        }
+    }
+}
