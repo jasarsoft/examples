823fcf560fcc8aadaf4e6d9cbd7ea8a92a34adbd
Edin Jašarević <edinjasar14@gmail.com>
2018-03-21 21:03:23 +0100

    cs - ss pog07 pr02 readonly

---
+++ cs/ss07/Poglavlje07/Primjer02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Planet
+    {
+        private readonly double gravity;
+
+        public Planet(double d)
+        {
+            gravity = d;
+        }
+
+        public double Gravity
+        {
+            get { return gravity; }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            double masa = 100;
+            Planet earth = new Planet(9.80665);
+            Planet jupiter = new Planet(22.94756);
+
+            double tezinaZemlja = masa * earth.Gravity;
+            double tezinaJupiter = masa * earth.Gravity;
+
+            Console.WriteLine("Tezina na Zemlji = {0} N", tezinaZemlja);
+            Console.WriteLine("Tezina na Jupiteru = {0} N", tezinaJupiter);
+        }
+    }
+}
