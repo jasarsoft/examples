881ac6d08b7c39dd13f27c559f20d6eedce7d8fc
Edin Jašarević <edinjasar14@gmail.com>
2018-03-21 21:05:01 +0100

    cs - ss pog07 pr05 index

---
+++ cs/ss07/Poglavlje07/Primjer05/ComboBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primjer05
+{
+    class ComboBox
+    {
+        private string[] stavke = new string[100];
+
+        public ComboBox(string[] stavke)
+        {
+            int i = 0;
+            foreach (string s in stavke)
+                this.stavke[i++] = s;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                return (index >= 0 || index <= 100) ? stavke[index] : null;
+            }
+            set
+            {
+                if (index >= 0 || index <= 100)
+                    stavke[index] = value;
+            }
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje07/Primjer05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] str = new string[]
+            {
+                "First",
+                "Second",
+                "Third",
+                "Fourth",
+                "Fifth"
+            };
+
+            ComboBox cb = new ComboBox(str);
+            Console.WriteLine(cb[4]);
+            cb[4] = "Peti";
+            Console.WriteLine(cb[4]);
+        }
+    }
+}
