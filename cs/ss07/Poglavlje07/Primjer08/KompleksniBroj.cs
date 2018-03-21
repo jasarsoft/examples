17ce08f3507a959d61121740099c05d4c4bb5dfc
Edin Jašarević <edinjasar14@gmail.com>
2018-03-21 21:06:11 +0100

    cs - ss pog07 pr08 operator

---
+++ cs/ss07/Poglavlje07/Primjer08/KompleksniBroj.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Primjer08
+{
+    class KompleksniBroj
+    {
+        private double real;
+        private double imagine;
+
+        public KompleksniBroj(double real, double imagine)
+        {
+            this.real = real;
+            this.imagine = imagine;
+        }
+
+        public static KompleksniBroj operator+(KompleksniBroj ls, KompleksniBroj rs)
+        {
+            return new KompleksniBroj(ls.real + rs.real, ls.imagine + rs.imagine);
+        }
+
+        public static KompleksniBroj operator-(KompleksniBroj ls, KompleksniBroj rs)
+        {
+            return new KompleksniBroj(ls.real - rs.real, ls.imagine - rs.imagine);
+        }
+
+        public static bool operator ==(KompleksniBroj ls, KompleksniBroj rs)
+        {
+            return (ls.real * ls.real + ls.imagine + ls.imagine) ==
+                    (rs.real * rs.real + rs.imagine * rs.imagine) ? true : false;
+        }
+
+        public static bool operator !=(KompleksniBroj ls, KompleksniBroj rs)
+        {
+            return (ls.real * ls.real + ls.imagine + ls.imagine) !=
+                    (rs.real * rs.real + rs.imagine * rs.imagine) ? true : false;
+        }
+
+        public static KompleksniBroj operator ++(KompleksniBroj x)
+        {
+            return new KompleksniBroj(x.real + 1, x.imagine + 1);
+        }
+
+        public static KompleksniBroj operator --(KompleksniBroj kb)
+        {
+            return new KompleksniBroj(kb.real - 1, kb.imagine - 1);
+        }
+
+        /*public static implicit operator double (KompleksniBroj kb)
+        {
+            return Math.Sqrt(kb.real * kb.real + kb.imagine * kb.imagine);
+        }*/
+
+        public override string ToString()
+        {
+            return real.ToString() + " + " + imagine.ToString() + "i";
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje07/Primjer08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Primjer08
+{
+    class Program
+    {
+        static void Main()
+        {
+            KompleksniBroj kb1 = new KompleksniBroj(30, 40);
+            KompleksniBroj kb2 = new KompleksniBroj(10, 25);
+            KompleksniBroj kb3 = kb1 + kb2;
+
+            Console.WriteLine(kb3);
+            kb3++;
+            Console.WriteLine(kb3);
+            Console.WriteLine(kb1 - kb2);
+            Console.WriteLine(kb1 == kb2);
+        }
+    }
+}
