068b78ff9a7c199c7097ebc7ed3219a2e5ac7bb7
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 14:48:09 +0100

    cs - ss pog08 pr02 niz tablica

---
+++ cs/ss07/Poglavlje08/Primjer02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            TablicaM tm = new TablicaM(100, 100);
+            Console.WriteLine(tm.Pomnozi(47, 74));
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje08/Primjer02/TablicaM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class TablicaM
+    {
+        private int[,] tab;
+        private int dimX, dimY;
+
+        public TablicaM(int x, int y)
+        {
+            dimX = x;
+            dimY = y;
+            tab = new int[x, y];
+
+            for (int i = 0; i < x; i++)
+                for (int j = 0; j < y; j++)
+                    tab[i, j] = (i + 1) * (j + 1);
+        }
+
+        public int Pomnozi(int x, int y)
+        {
+            if (x >= 1 && x <= dimX && y >= 1 && y <= dimY)
+                return tab[x - 1, y - 1];
+            else
+                return 0;
+        }
+    }
+}
