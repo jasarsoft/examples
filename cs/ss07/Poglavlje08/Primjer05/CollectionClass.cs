7c53d2a6d3f90d1aaf66e3c3dd487374bb9f219e
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 14:49:20 +0100

    cs - ss pog08 pr05 collection

---
+++ cs/ss07/Poglavlje08/Primjer05/CollectionClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace Primjer05
+{
+    class CollectionClass
+    {
+        double[] elementi;
+
+        public CollectionClass()
+        {
+            elementi = new double[] { 1.1, 2.2, 3.3, 4.4 };
+        }
+
+        public EnumeratorClass GetEnumerator()
+        {
+            return new EnumeratorClass(this);
+        }
+
+        public class EnumeratorClass
+        {
+            int index = -1;
+            CollectionClass kolekcija;
+
+            public EnumeratorClass(CollectionClass kolekcija)
+            {
+                this.kolekcija = kolekcija;
+            }
+
+            public double Current
+            {
+                get
+                {
+                    return (kolekcija.elementi[index]);
+                }
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                return index < kolekcija.elementi.Length;
+            }
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje08/Primjer05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Main()
+        {
+            CollectionClass col = new CollectionClass();
+
+            foreach (double d in col)
+                Console.WriteLine(d);
+        }
+    }
+}
