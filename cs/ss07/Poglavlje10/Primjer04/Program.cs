21f0c7d5c584564ade533b4068e129fa6a52434c
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 18:59:28 +0100

    cs - ss pog10 pr04 rethrowing

---
+++ cs/ss07/Poglavlje10/Primjer04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Overflow();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Dodatana obrada iznimke...");
+            }
+        }
+
+        static void Overflow()
+        {
+            int[] arr = new int[10];
+
+            try
+            {
+                arr[10] = 1;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Obrada iznimke...");
+                throw;
+            }
+        }
+    }
+}
