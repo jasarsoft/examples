43c19cf39e35b06d1af25e5004de33a4fc299adf
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 14:49:42 +0100

    cs - ss pog08 pr05.1 collection with interface

---
+++ cs/ss07/Poglavlje08/Primjer05.1/CollectionClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace Primjer05._1
+{
+    class CollectionClass : IEnumerable
+    {
+        double[] elementi;
+
+        public CollectionClass()
+        {
+            elementi = new double[] { 1.1, 2.2, 3.3, 4.4 };
+        }
+
+        public EnumeratorClass GetEnumerator()
+        {
+            return new EnumeratorClass(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public class EnumeratorClass : IEnumerator
+        {
+            int index = -1;
+            CollectionClass kolekcija;
+
+            public EnumeratorClass(CollectionClass kolekcija)
+            {
+                this.kolekcija = kolekcija;
+            }
+
+            public double Current
+            {
+                get
+                {
+                    return (kolekcija.elementi[index]);
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return this.Current;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                return index < kolekcija.elementi.Length;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
