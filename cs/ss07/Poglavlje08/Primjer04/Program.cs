6de681541a3631f172f4ac0438d047b422227d6a
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 14:49:00 +0100

    cs - ss pog08 pr04 array list

---
+++ cs/ss07/Poglavlje08/Primjer04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList arr = new ArrayList();
+
+            for (int i = 0; i < 7; i++)
+                arr.Add(i);
+
+            for (int i = 0; i < 7; i++)
+                arr.Add(0);
+
+            for (int i = 0; i < 14; i++)
+                Console.Write(arr[i] + " ");
+        }
+    }
+}
