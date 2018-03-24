9682d530fa019860de9fc8d83170025a0397f58b
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 16:32:21 +0100

    cs - ss pog09 pr03 event

---
+++ cs/ss07/Poglavlje09/Primjer03/Operater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Operater
+    {
+        public event Signal Signalizirano;
+
+        public void DolazniSignal(int vrsta, long broj)
+        {
+            Signalizirano(vrsta, broj);
+        }
+    }
+}
