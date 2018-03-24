8d35881cb55d9800ff15fc3bb628f5a5c0be8cae
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 20:27:28 +0100

    cs - ss pog10 pr06 checked

---
+++ cs/ss07/Poglavlje10/Primjer06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte x = 127, y;
+
+            try
+            {
+                y = checked(++x);
+            }
+            catch (OverflowException e)
+            {
+                y = x;
+            }
+
+            Console.WriteLine(y);
+        }
+    }
+}
