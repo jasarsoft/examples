b5e5cf2f36d6e1fa35bcd24e911aa778ed840a83
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 16:04:25 +0100

    cs - ss pog09 pr02 multicast delegate

---
+++ cs/ss07/Poglavlje09/Primjer02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        public delegate void Del(double d);
+
+        static void Main()
+        {
+            Del d1 = new Del(F1);
+            d1 += new Del(F2);
+            d1(5);
+
+            d1 -= new Del(F2);
+            d1(6);
+        }
+
+        static void F1(double d)
+        {
+            Console.WriteLine(d);
+        }
+
+        static void F2(double d)
+        {
+            Console.WriteLine(d * 2);
+        }
+    }
+}
