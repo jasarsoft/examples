09454eabfc3175c451540025c5ef9619d09652a2
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 14:47:30 +0100

    cs - ss pog08 pr01 niz

---
+++ cs/ss07/Poglavlje08/Primjer01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] redniBrojevi1 = new string[]
+            {
+                "First",
+                "Second",
+                "Third",
+                "Fourth",
+                "Fifth"
+            };
+
+            Array.Sort(redniBrojevi1);
+            for (int i = 0; i < redniBrojevi1.Length; i++)
+                Console.Write(redniBrojevi1[i] + " ");
+            Console.WriteLine();
+
+            Array redniBrojevi2 = Array.CreateInstance(typeof(String), 5);
+            redniBrojevi2.SetValue("First", 0);
+            redniBrojevi2.SetValue("Second", 1);
+            redniBrojevi2.SetValue("Third", 2);
+            redniBrojevi2.SetValue("Fourth", 3);
+            redniBrojevi2.SetValue("Fifth", 4);
+
+            Array.Reverse(redniBrojevi2);
+            for (int i = 0; i < redniBrojevi2.Length; i++)
+                Console.Write(redniBrojevi2.GetValue(i) + " ");
+            Console.WriteLine();
+        }
+    }
+}
