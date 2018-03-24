ed80e6af586babc4eface802d3cf31471379f259
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 20:31:44 +0100

    cs - ss pog10 unchecked

---
+++ cs/ss07/Poglavlje10/Primjer07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Primjer07
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 2000000000, y = 2, z;
+
+            try
+            {
+                z = checked(x * y);
+            }
+            catch (OverflowException e)
+            {
+                z = unchecked(x * y);
+                Console.WriteLine(z);
+            }
+        }
+    }
+}
