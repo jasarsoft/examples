0602dd2d1f1fd6429a781fb0dd72bb2998472461
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 14:48:38 +0100

    cs - ss pog08 pr03 dvodimenzionalni niz

---
+++ cs/ss07/Poglavlje08/Primjer03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte[][] arr = new sbyte[3][];
+            arr[0] = new sbyte[7];
+            arr[1] = new sbyte[5];
+            arr[2] = new sbyte[9];
+
+            sbyte x = 1;
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = 0; j < arr[i].Length; j++)
+                    arr[i][j] = x++;
+
+            for (int i = 0; i < arr.Length; i++)
+                Array.Reverse(arr[i]);
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                    Console.Write(arr[i][j] + ", ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
