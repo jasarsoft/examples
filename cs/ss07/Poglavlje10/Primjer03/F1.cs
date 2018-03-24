ddafde15a02d6962ca6c553228f8cc57c374d156
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 17:49:35 +0100

    cs - ss pog10 pr03 exception throw

---
+++ cs/ss07/Poglavlje10/Primjer03/F1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class F1
+    {
+        public void UnesiParametar()
+        {
+            try
+            {
+                double d = Convert.ToDouble(Console.ReadLine());
+                PodesiMotor(d);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Nedozvoljena vrijednost");
+            }
+        }
+
+        public static void PodesiMotor(double d)
+        {
+            if (d < 50 || d > 60)
+                throw new Exception();
+            else
+                Console.WriteLine(d + " je OK");
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje10/Primjer03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Program
+    {
+        static void Main()
+        {
+            F1 reanult = new F1();
+
+            reanult.UnesiParametar();
+        }
+    }
+}
