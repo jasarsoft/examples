29c1a71ed1ea04fe86180b2a728589d556346b06
Edin Jašarević <edinjasar14@gmail.com>
2018-03-24 17:37:21 +0100

    cs - ss pog10 pr01 try-catch

---
+++ cs/ss07/Poglavlje10/Primjer01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 1;
+            Random r = new Random();
+
+            for(int i = 0; i < 100; i++)
+            {
+                int y = r.Next(100);
+
+                try
+                {
+                    int z = x / y;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Pokusaj djeljenja sa nulom");
+                }
+            }
+
+
+        }
+    }
+}
