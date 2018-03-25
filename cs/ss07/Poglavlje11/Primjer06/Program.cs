8a48d5fddda898226cb5db8d054b0883d8933dc1
Edin Jašarević <edinjasar14@gmail.com>
2018-03-25 17:07:04 +0200

    cs - ss pog11 pr06 reflection assembly

---
+++ cs/ss07/Poglavlje11/Primjer06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            Assembly skup = Assembly.Load("Primjer06");
+            Type[] tipovi = skup.GetTypes();
+
+            foreach(Type t in tipovi)
+            {
+                Console.WriteLine(t.Name);
+                ConstructorInfo[] arr = t.GetConstructors();
+
+                foreach (ConstructorInfo ci in arr)
+                    Console.WriteLine(ci);
+            }
+        }
+    }
+}
