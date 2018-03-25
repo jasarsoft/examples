f78c146ac8b6796ca99836a847339e99059c6ea0
Edin Jašarević <edinjasar14@gmail.com>
2018-03-25 17:03:34 +0200

    cs - ss pog11 pr05 reflection object method

---
+++ cs/ss07/Poglavlje11/Primjer05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type t = Type.GetType("System.Collections.ArrayList");
+            object o = Activator.CreateInstance(t);
+
+            t.InvokeMember("Add", BindingFlags.InvokeMethod, null, o, new object[] { "Sve je relativno" });
+            t.InvokeMember("Add", BindingFlags.InvokeMethod, null, o, new object[] { "Nista nije apsulutno" });
+
+            ArrayList a = (ArrayList)o;
+            foreach (string s in a)
+                Console.WriteLine(s);
+        }
+    }
+}
