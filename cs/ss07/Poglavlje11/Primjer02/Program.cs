2e5eb6970b146d67bc40f3241ae2dcf0b9b9efdd
Edin Jašarević <edinjasar14@gmail.com>
2018-03-25 15:55:20 +0200

    cs - ss pog11 pr02 dllimport attribute

---
+++ cs/ss07/Poglavlje11/Primjer02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Primjer02
+{
+    class Program
+    {
+        [DllImportAttribute("user32.dll")]
+        static extern int MessageBoxA(int i, string s1, string s2, int t);
+
+        static int Main()
+        {
+            return MessageBoxA(0, "API je pozvan", "Win32", 0);
+        }
+    }
+}
