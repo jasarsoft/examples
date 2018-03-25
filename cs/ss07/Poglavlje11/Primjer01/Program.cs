7cd3814fa1c82745e83153807d80abfba99c8626
Edin Jašarević <edinjasar14@gmail.com>
2018-03-25 15:51:56 +0200

    cs - ss pog11 pr01 attribute obsolete

---
+++ cs/ss07/Poglavlje11/Primjer01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Primjer01
+{
+    class Program
+    {
+        [Obsolete("Metoda FCPlusPlus je zastarjela, koristite FCSharp", false)]
+        static void FCPlusPlus()
+        {
+            //int *arr = (int *) malloc(100 * sizeof(int));
+        }
+
+        static void FCSharp()
+        {
+            int[] arr = new int[100];
+        }
+
+        static void Main()
+        {
+            FCPlusPlus();
+        }
+    }
+}
