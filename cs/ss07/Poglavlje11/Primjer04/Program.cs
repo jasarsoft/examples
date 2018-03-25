869c81e8a4fda6fa17df45e0ce55da32ed098339
Edin Jašarević <edinjasar14@gmail.com>
2018-03-25 16:57:50 +0200

    cs - ss pog11 pr04 reflection call static method

---
+++ cs/ss07/Poglavlje11/Primjer04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type t = Type.GetType("System.Console");
+            Type[] argArr = new Type[1];
+            argArr[0] = Type.GetType("System.String");
+
+            MethodInfo cout = t.GetMethod("WriteLine", argArr);
+            Object[] argumenti = new object[1];
+
+            argumenti[0] = "Obracun ko OK Korala";
+            cout.Invoke(t, argumenti);
+        }
+    }
+}
