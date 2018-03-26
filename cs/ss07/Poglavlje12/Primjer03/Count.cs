ac6b9351009b217e55de2548ba97c8c3c6074061
Edin Jašarević <edinjasar14@gmail.com>
2018-03-26 16:31:44 +0200

    cs - ss pog12 pr03 sinhronizacija niti

---
+++ cs/ss07/Poglavlje12/Primjer03/Count.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Primjer03
+{
+    class Count
+    {
+        public static int counter = 100;
+
+        public void Counting1()
+        {
+            while(Count.counter > 0)
+            {
+                int x = Count.counter;
+                x--;
+                Thread.Sleep(5);
+                Count.counter = x;
+                Console.WriteLine(Count.counter);
+            }
+        }
+
+        public void Counting2()
+        {
+            while(Count.counter > 0)
+            {
+                int x = Count.counter;
+                x--;
+                Thread.Sleep(5);
+                Count.counter = x;
+                Console.WriteLine(Count.counter);
+            }
+        }
+    }
+}
