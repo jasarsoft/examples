1fe4874589e61305ff809a5a12a88d2686c380ba
Edin Jašarević <edinjasar14@gmail.com>
2018-03-26 16:08:41 +0200

    cs - ss pog12 pr02 thread sleep

---
+++ cs/ss07/Poglavlje12/Primjer02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 60; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i + 1);
+            }
+        }
+    }
+}
