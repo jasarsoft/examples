5c794fa9a807a402071c01198bc5f8f2a0f17a45
Edin Jašarević <edinjasar14@gmail.com>
2018-03-26 16:03:52 +0200

    cs - ss pog12 pr01 thread

---
+++ cs/ss07/Poglavlje12/Primjer01/NitBrojeva.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Primjer01
+{
+    class NitBrojeva
+    {
+        private Thread t1;
+        private Thread t2;
+
+        public void PozoviNiti()
+        {
+            ThreadStart odds = new ThreadStart(NeparniBrojevi);
+            ThreadStart evens = new ThreadStart(ParniBrojevi);
+
+            t1 = new Thread(odds);
+            t2 = new Thread(evens);
+
+            t1.Start();
+            t2.Start();
+        }
+
+        private void NeparniBrojevi()
+        {
+            for (int i = 1; i < 100000; i += 2)
+                Console.WriteLine(i);
+        }
+
+        private void ParniBrojevi()
+        {
+            for (int i = 2; i < 100000; i += 2)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje12/Primjer01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            NitBrojeva n = new NitBrojeva();
+
+            n.PozoviNiti();
+        }
+    }
+}
