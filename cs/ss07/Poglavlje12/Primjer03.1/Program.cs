744953dee0a04751ef5ff652163d7daa97c87147
Edin Jašarević <edinjasar14@gmail.com>
2018-03-26 16:34:34 +0200

    cs - ss pog12 pr03.1 thread lock

---
+++ cs/ss07/Poglavlje12/Primjer03.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Primjer03._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Count odbrojavanje = new Count();
+
+            Thread t1 = new Thread(new ThreadStart(odbrojavanje.Counting2));
+            t1.Start();
+
+            Thread t2 = new Thread(new ThreadStart(odbrojavanje.Counting2));
+            t2.Start();
+        }
+    }
+}
