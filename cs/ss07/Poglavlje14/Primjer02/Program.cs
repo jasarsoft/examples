3db452cf044fe828c3769a0567d38984344327a9
Edin Jašarević <edinjasar14@gmail.com>
2018-03-27 11:25:37 +0200

    cs - ss pog14 pr02 unsafe

---
+++ cs/ss07/Poglavlje14/Primjer02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Primjer02
+{
+    class Program
+    {
+        unsafe static void Main()
+        {
+            double* p = stackalloc double[100];
+
+            for (int i = 0; i < 100; i++)
+                *p++ = i + 1;
+
+            p -= 100;
+
+            for (int i = 0; i < 100; i++)
+                Console.WriteLine(*(p + i));
+        }
+
+        unsafe public static void PromjeniNiz(double *p)
+        {
+            for (int i = 0; i < 100; i++)
+                *p++ *= 2;
+        }
+    }
+}
