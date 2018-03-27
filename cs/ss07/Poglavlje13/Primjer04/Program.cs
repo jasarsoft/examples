fee74c9155818ed8c33a1c1f7de30e8f2620b613
Edin Jašarević <edinjasar14@gmail.com>
2018-03-27 11:09:59 +0200

    cs - ss pog13 pr04 read/write binary file

---
+++ cs/ss07/Poglavlje13/Primjer04/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Primjer04
+{
+    class Program
+    {
+        private const string big = "bignumber.bin";
+
+        static void Main()
+        {
+            Stream izlazniTok = File.OpenWrite(big);
+            BinaryWriter fWrite = null;
+
+            try
+            {
+                fWrite = new BinaryWriter(izlazniTok);
+                fWrite.Write("milijun");
+                int i = 1000000;
+                fWrite.Write(i);
+
+                fWrite.Write("milijarada");
+                i = 1000000000;
+                fWrite.Write(i);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fWrite != null)
+                    fWrite.Close();
+            }
+
+            Stream ulazniTok = File.OpenRead(big);
+            BinaryReader fRead = null;
+
+            try
+            {
+                fRead = new BinaryReader(ulazniTok);
+
+                Console.WriteLine(fRead.ReadString());
+                Console.WriteLine(fRead.ReadInt32());
+                Console.WriteLine(fRead.ReadString());
+                Console.WriteLine(fRead.ReadInt32());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fRead != null)
+                    fRead.Close();
+            }
+
+            FileInfo f = new FileInfo(big);
+            Console.WriteLine(f.Length + " bytes");
+        }
+    }
+}
