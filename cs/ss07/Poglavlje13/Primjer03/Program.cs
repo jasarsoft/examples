14187384ed3e69f8795016db264e23e3ede96d3f
Edin Jašarević <edinjasar14@gmail.com>
2018-03-27 11:09:33 +0200

    cs - ss pog13 pr03 read/write text file

---
+++ cs/ss07/Poglavlje13/Primjer03/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+namespace Primjer03
+{
+    class Program
+    {
+        private const string big = "bignumber.txt";
+
+        static void Main()
+        {
+            StreamWriter fWrite = null;
+
+            try
+            {
+                fWrite = new StreamWriter(big, false, System.Text.Encoding.ASCII);
+                fWrite.WriteLine("milijun");
+                int i = 1000000;
+                fWrite.WriteLine(i);
+
+                fWrite.WriteLine("milijarada");
+                i = 1000000000;
+                fWrite.WriteLine(i);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fWrite != null)
+                    fWrite.Close();
+            }
+
+            StreamReader fRead = null;
+            try
+            {
+                fRead = new StreamReader(big, System.Text.Encoding.ASCII);
+                string s;
+                while ((s = fRead.ReadLine()) != null)
+                    Console.WriteLine(s);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (fRead != null)
+                    fRead.Close();
+            }
+
+            FileInfo f = new FileInfo(big);
+            Console.WriteLine(f.Length + " bytes");
+        }
+    }
+}
