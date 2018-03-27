eed6499cf007ccfac6b67a5bc0df076241a56d53
Edin Jašarević <edinjasar14@gmail.com>
2018-03-27 11:36:59 +0200

    cs - ss pog14 pr03 unsafe fixed

---
+++ cs/ss07/Poglavlje14/Primjer03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Primjer03
+{
+    class Program
+    {
+        private int x = 50;
+
+        unsafe static void Main()
+        {
+            Program t = new Program();
+
+            fixed (int *p = &t.x)
+            {
+                
+                Console.WriteLine(*p);
+                GC.Collect();
+                Console.WriteLine(*p);
+            }
+        }
+    }
+}
