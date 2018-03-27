0bf924ca8b387ac7475f03ceb2482c1d5c6a5b11
Edin Jašarević <edinjasar14@gmail.com>
2018-03-27 11:08:14 +0200

    cs - ss pog13 pr01 directory

---
+++ cs/ss07/Poglavlje13/Primjer01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            string spices = "Spice Girls";
+            string[] spicesGirls =
+            {
+                "Melanie",
+                "Geri",
+                "Victoria",
+                "Emma",
+                "Melanie"
+            };
+
+            DirectoryInfo dir = new DirectoryInfo(spices);
+            if (dir.Exists == false)
+                dir.Create();
+
+            foreach (string s in spicesGirls)
+                dir.CreateSubdirectory(s);
+
+            Console.WriteLine("Provjerite jesu li mape kreirane prije nego se izbrisu.");
+            Thread.Sleep(20000);
+            dir.Delete(true);
+        }
+    }
+}
