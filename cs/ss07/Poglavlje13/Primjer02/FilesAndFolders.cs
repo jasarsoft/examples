5c1a5bd8941959ba880349ca67ca5c350918cf31
Edin Jašarević <edinjasar14@gmail.com>
2018-03-27 11:08:59 +0200

    cs - ss pog13 pr02 files and folders

---
+++ cs/ss07/Poglavlje13/Primjer02/FilesAndFolders.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Primjer02
+{
+    class FilesAndFolders
+    {
+        private DirectoryInfo dir;
+        private int maxFiles = 0;
+        private string hasMostFiles = "";
+        private string systemFolder = Environment.GetEnvironmentVariable("SystemRoot");
+
+        public FilesAndFolders(string root)
+        {
+            dir = new DirectoryInfo(root);
+        }
+
+        public int MaxFiles
+        {
+            get { return maxFiles; }
+            set { maxFiles = value; }
+        }
+
+        public string HasMostFiles
+        {
+            get { return hasMostFiles; }
+            set { hasMostFiles = value; }
+        }
+
+        public void OpenDirectory()
+        {
+            DirectoryInfo[] directories = null;
+            FileInfo[] files = null;
+
+
+            try
+            {
+                directories = dir.GetDirectories();
+                files = dir.GetFiles();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Nemam pristup za: " + dir.FullName);
+                return;
+            }
+
+            if(String.Compare(dir.FullName, 0, systemFolder, 0, systemFolder.Length, true) != 0
+                && files.Length > maxFiles)
+            {
+                maxFiles = files.Length;
+                hasMostFiles = dir.FullName;
+            }
+
+            foreach(DirectoryInfo nextDir in directories)
+            {
+                dir = nextDir;
+                OpenDirectory();
+            }
+        }
+
+
+    }
+}
---
+++ cs/ss07/Poglavlje13/Primjer02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Primjer02
+{
+    class Program
+    {
+        static void Main()
+        {
+            FilesAndFolders ff = new FilesAndFolders("c:\\");
+
+            ff.OpenDirectory();
+
+            Console.WriteLine();
+            Console.WriteLine("{0} datoteka u mapi {1}", ff.MaxFiles, ff.HasMostFiles);
+        }
+    }
+}
