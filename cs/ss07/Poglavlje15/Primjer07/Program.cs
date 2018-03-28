d30757ae2df972eccf4c2361c0cdd2a2afdb545e
Edin Jašarević <edinjasar14@gmail.com>
2018-03-28 11:21:38 +0200

    cs - ss pog15 pr07 oledb delete

---
+++ cs/ss07/Poglavlje15/Primjer07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Primjer07
+{
+    class Program
+    {
+        static void Main()
+        {
+            string ship = "Brod4";
+            string database = "brodovi.accdb";
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                                        "Data Source=" + database +
+                                                        ";Persist Security Info=True");
+
+            try
+            {
+                conn.Open();
+                string sql = "DELETE FROM Table1 WHERE Naziv = " + "\'" + ship + "\'";
+
+                OleDbCommand command = new OleDbCommand(sql, conn);
+                command.ExecuteNonQuery();
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
