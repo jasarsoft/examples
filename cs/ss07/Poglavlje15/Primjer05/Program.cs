b53d5f1035d7fbad6eb87ed3d6e079b57c6ab433
Edin Jašarević <edinjasar14@gmail.com>
2018-03-28 11:17:30 +0200

    cs - ss pog15 pr05 read oledb

---
+++ cs/ss07/Poglavlje15/Primjer05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Primjer05
+{
+    class Program
+    {
+        static void Main()
+        {
+            string database = "brodovi.accdb";
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                                        "Data Source=" + database +
+                                                        ";Persist Security Info=True");
+
+            try
+            {
+                conn.Open();
+                OleDbCommand command = new OleDbCommand("SELECT * FROM Table1", conn);
+                OleDbDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                    if ((bool)reader["NaDnu"])
+                        Console.WriteLine(reader["Naziv"] + " - " + reader["Drzava"]);
+
+                reader.Close();
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
