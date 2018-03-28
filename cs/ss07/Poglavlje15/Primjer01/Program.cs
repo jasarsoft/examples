1cacabc112282462a0a9e121562748c676fb7e66
Edin Jašarević <edinjasar14@gmail.com>
2018-03-28 11:14:43 +0200

    cs - ss pog15 pr01 xml document

---
+++ cs/ss07/Poglavlje15/Primjer01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+
+namespace Primjer01
+{
+    class Program
+    {
+        static void Main()
+        {
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.Load("tecajnalista.xml");
+            }
+            catch (XmlException e)
+            {
+                Console.WriteLine("Dokument nije dobro oblikovan\n" + e.Message);
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine("Ne postoji datoteka tecajnalista.xml");
+            }
+
+            XmlNodeList nodes = doc.GetElementsByTagName("Valuta");
+
+            foreach (XmlNode node in nodes)
+                Console.WriteLine("{0} {1} = {2} kn", node["Jedinica"].InnerText,
+                                                      node["Oznaka"].InnerText,
+                                                      node["Iznos"].InnerText);
+
+
+        }
+    }
+}
