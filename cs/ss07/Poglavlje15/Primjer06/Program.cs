9b2b25c3b0a4cdf617c4d6fcd2765494161052d9
Edin Jašarević <edinjasar14@gmail.com>
2018-03-28 11:21:10 +0200

    cs - ss pog15 pr06 update oledb

---
+++ cs/ss07/Poglavlje15/Primjer06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Primjer06
+{
+    class Program
+    {
+        static void Main()
+        {
+            string database = "brodovi.accdb";
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                                        "Data Source=" + database +
+                                                        ";Persist Security Info=True");
+
+            try
+            {
+                conn.Open();
+                string sql = "UPDATE Table1 SET " + "NaDnu='0'" + " WHERE Naziv = 'Brod4'";
+
+                OleDbCommand command = new OleDbCommand(sql, conn);
+                command.ExecuteNonQuery();
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
