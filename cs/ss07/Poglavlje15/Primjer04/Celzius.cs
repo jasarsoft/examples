0515ab1e74b60ce6626163d0176d4548f31aef44
Edin Jašarević <edinjasar14@gmail.com>
2018-03-28 11:16:33 +0200

    cs - ss pog15 pr04 xml comment

---
+++ cs/ss07/Poglavlje15/Primjer04/Celzius.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Primjer04
+{
+    /// <summary>
+    /// Klasa za rad sa temperaturama  
+    /// </summary>
+    /// <remarks>
+    /// Klasa Celzius nema definiran konstruktor.
+    /// Temperatura se zadaje preko svojstva.
+    /// </remarks>
+    class Celzius
+    {
+        /// <summary>
+        /// Varijabla sadrzaje trenutnu temperaturu koja se zadaje preko svojstva
+        /// </summary>
+        private double trenutnaTemp;
+
+        /// <summary>
+        /// Get/Set svojstvo za varijablu trenutnaTemparautra
+        /// </summary>
+        /// <value>Vrijednost koja se zadaje mora biti tipa double</value>
+        public double TrenutnaTemp
+        {
+            get
+            {
+                return trenutnaTemp;
+            }
+            set
+            {
+                trenutnaTemp = value;
+            }
+        }
+
+        /// <summary>
+        /// Metoda vraca vrijednost temperature u Klevinima
+        /// za zadanu vrijednost u Celziusima
+        /// </summary>
+        /// <param name="tempCelzius">Temperatura koja se pretvara u Kelvine</param>
+        /// <returns>Vraca se double vrijednost temeprature u Klevinima</returns>
+        public double Kelvin(double tempCelzius)
+        {
+            return tempCelzius + 273.15;
+        }
+    }
+}
---
+++ cs/ss07/Poglavlje15/Primjer04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Primjer04
+{
+    class Program
+    {
+        static void Main()
+        {
+            Celzius c = new Celzius();
+
+            c.TrenutnaTemp = 25;
+            Console.WriteLine(c.Kelvin(26.85) + "K");
+        }
+    }
+}
