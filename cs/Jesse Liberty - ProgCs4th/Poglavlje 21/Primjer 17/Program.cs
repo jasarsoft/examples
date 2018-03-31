c67dc3c88ef9f173781ddcd60d6ea7f5e64efb30
Edin Jašarević <edinjasar14@gmail.com>
2018-03-31 14:32:41 +0200

    cs jlp4 pog21 pr17 pisanje izoliranog spremista

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+
+            app.Run();
+        }
+
+        private void Run()
+        {
+            //stvara tok konfiguracijske datoteke
+            IsolatedStorageFileStream configFile = new IsolatedStorageFileStream("tester.cfg", FileMode.Create);
+
+            //stvar pisac koji ce pisati u toku
+            StreamWriter writer = new StreamWriter(configFile);
+            //upisuje podatke u konfiguracijsku datoteku
+            string output;
+            System.DateTime currentTime = System.DateTime.Now;
+            output = "Last access: " + currentTime.ToString();
+            writer.WriteLine(output);
+            output = "Last position = 27, 35";
+            writer.WriteLine(output);
+
+            //prazni medjupreznik i posprema
+            writer.Close();
+            configFile.Close();
+        }
+    }
+}
