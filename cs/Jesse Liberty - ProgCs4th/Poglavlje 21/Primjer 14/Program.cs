87116b55ce24bb2f872d18d4218b2976f4698a07
Edin Jašarević <edinjasar14@gmail.com>
2018-03-31 13:19:56 +0200

    cs jl pog21 pr14 citanje web stranice

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 14/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primjer_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stvara WebRequest za odredjenu stranicu
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://jesseliberty.com/");
+
+            //salje web zahtjev za WebResponse koji ucahurava tu stranicu
+            HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            //uzima stream reader od oggovora
+            StreamReader streamReader = new StreamReader(webResponse.GetResponseStream(), Encoding.ASCII);
+
+            try
+            {
+                string outputString;
+                outputString = streamReader.ReadToEnd();
+                Console.WriteLine(outputString);
+            }
+            catch
+            {
+                Console.WriteLine("Exception reading from Web page");
+            }
+
+            streamReader.Close();
+        }
+    }
+}
