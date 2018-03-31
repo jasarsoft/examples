93d5c3af1179ca682f019e82cf95343d4d2f1f8c
Edin Jašarević <edinjasar14@gmail.com>
2018-03-31 14:33:33 +0200

    cs jsp4 pog21 pr18 citanje izoliranog spremista

---
+++ cs/Jesse Liberty - ProgCs4th/Poglavlje 21/Primjer 18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Primjer_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+
+            app.Run();
+        }
+
+        private void Run()
+        {
+            //otvara tok konfiguracijske datoteke
+            IsolatedStorageFileStream configFile = new IsolatedStorageFileStream("tester.cfg", FileMode.Open);
+
+            //stvar standardni citac toka
+            StreamReader reader = new StreamReader(configFile);
+
+            //cita datoteku i prikazuje
+            string theEntry;
+            do
+            {
+                theEntry = reader.ReadLine();
+                Console.WriteLine(theEntry);
+            } while (theEntry != null);
+
+            reader.Close();
+            configFile.Close();
+        }
+    }
+}
