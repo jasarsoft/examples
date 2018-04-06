a63e0846ee74c268d730e9dc7f44114d6785dd19
Edin Jašarević <edinjasar14@gmail.com>
2018-04-06 15:20:19 +0200

    cs jlp4 pog22 pr12 pokazivaci

---
+++ cs/jlp4/Poglavlje 22/Primjer 12/APIFileReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace UsingPointers
+{
+    class APIFileReader
+    {
+        const uint GenericRead = 0x80000000;
+        const uint OpenExisting = 3;
+        const uint UseDefault = 0;
+        int fileHandle;
+
+        [DllImport("karnel32", SetLastError = true)]
+        static extern unsafe int CreateFile(
+            string filename,
+            uint desiredAccess,
+            uint shareMode,
+            uint attributes,
+            uint creationDisposition,
+            uint flagAndAttributes,
+            uint templateFile);
+
+        [DllImport("karnel32", SetLastError = true)]
+        static extern unsafe bool ReadFile(
+            int hFile,
+            void* lpBuffer,
+            int nBytesToRead,
+            int* nBytesRead,
+            int overlapped);
+
+        //konstruktor otvara postojecu datotkeu i postavlja clan za identifikator datoteke
+        public APIFileReader(string filename)
+        {
+            fileHandle = CreateFile(
+                filename,           //ime datoteke
+                GenericRead,        //desiredAccess
+                UseDefault,         //shareMode
+                UseDefault,         //atributi
+                OpenExisting,       //creationDisposition
+                UseDefault,         //flagsAndAttributes
+                UseDefault);        //templateFile
+        }
+
+        public unsafe int Read(byte[] buffer, int index, int count)
+        {
+            int bytesRead = 0;
+            fixed(byte* bytePointer = buffer)
+            {
+                ReadFile(
+                    fileHandle,             //hFile
+                    bytePointer + index,    //lpBuffer
+                    count,                  //nBytesToRead
+                    &bytesRead,             //nBytesRead
+                    0);                     //overlapped
+            }
+
+            return bytesRead;
+        }
+    }
+}
---
+++ cs/jlp4/Poglavlje 22/Primjer 12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace UsingPointers
+{
+    class Program
+    {
+        static void Main()
+        {
+            //stvara instancu APIFileReader
+            //i prosljedjuje ime postojece datoteke
+            APIFileReader fileReader = new APIFileReader("myTestFile.txt");
+
+            //stvara medjuspremink i ASCII koder
+            const int bufferSize = 128;
+            byte[] buffer = new byte[bufferSize];
+            ASCIIEncoding asciiEncoder = new ASCIIEncoding();
+
+            //ucitava datoteku u medjuspremik i prikazjue na konzolu
+            while(fileReader.Read(buffer, 0, bufferSize) != 0)
+            {
+                Console.WriteLine("{0}", asciiEncoder.GetString(buffer));
+            }
+        }
+    }
+}
