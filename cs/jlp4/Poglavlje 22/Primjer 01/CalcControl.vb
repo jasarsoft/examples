47b7e38ce0ad57a3e7872e0826b16d57270db9dd
Edin Jašarević <edinjasar14@gmail.com>
2018-04-06 15:38:00 +0200

    cs jlp4 pog22 pr01 com class

---
+++ cs/jlp4/Poglavlje 22/Primjer 01/CalcControl.vb
@@ -0,0 +1,39 @@
+﻿<ComClass(CalcControl.ClassId, CalcControl.InterfaceId, CalcControl.EventsId)> _
+Public Class CalcControl
+
+#Region "COM GUIDs"
+    ' These  GUIDs provide the COM identity for this class 
+    ' and its COM interfaces. If you change them, existing 
+    ' clients will no longer be able to access the class.
+    Public Const ClassId As String = "d0f51560-76c4-4eec-b623-d2d833d2cc80"
+    Public Const InterfaceId As String = "5adffa73-ba81-428b-9190-75d134d18efe"
+    Public Const EventsId As String = "8294ff87-5c81-4390-b8b3-792805e0f3c6"
+#End Region
+
+    ' A creatable COM class must have a Public Sub New() 
+    ' with no parameters, otherwise, the class will not be 
+    ' registered in the COM registry and cannot be created 
+    ' via CreateObject.
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+    Public Function Add(left As Double, right As Double)
+        Add = left + right
+    End Function
+
+    Public Function Substract(left As Double, right As Double)
+        Substract = left - right
+    End Function
+
+    Public Function Multiply(left As Double, right As Double)
+        Multiply = left * right
+    End Function
+
+    Public Function Divide(left As Double, right As Double)
+        Divide = left / right
+    End Function
+
+End Class
+
+
---
+++ cs/jlp4/Poglavlje 22/Primjer 01/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Primjer 01")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Primjer 01")> 
+<Assembly: AssemblyCopyright("Copyright ©  2018")> 
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("44d2529d-9ca0-47c5-8f85-cdcde5bac1c0")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
