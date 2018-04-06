63d4ca6e31dd45a315644abb6bd6a7d023558eb5
Edin Jašarević <edinjasar14@gmail.com>
2018-04-06 15:48:38 +0200

    cs jlp4 pog22 pr08 dll

---
+++ cs/jlp4/Poglavlje 22/Primjer 08/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Primjer_08
+{
+    //[assembly:AssemblyKeyFile("test.kex")]
+    public class Calculator
+    {
+        public Calculator()
+        {
+
+        }
+
+        public double Add(Double left, double right)
+        {
+            return left + right;
+        }
+
+        public double Substract(double left, double right)
+        {
+            return left - right;
+        }
+
+        public double Multiply(double left, double right)
+        {
+            return left * right;
+        }
+
+        public double Divide(double left, double right)
+        {
+            return left / right;
+        }
+    }
+}
