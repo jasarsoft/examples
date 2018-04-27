674a0daac41f12c040dee041033c12d4bed04f73
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 19:37:09 +0200

    referenta vrijednsot

---
+++ cs/Mihalo Pupin/Vjezba 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_15
+{
+    class Program
+    {
+        static void DupliranaVred(ref int prom)
+        {
+            prom *= 2;
+            Console.WriteLine("duplirana promjenjiva = {0}", prom);
+        }
+
+        static void Main(string[] args)
+        {
+            int prom = 5;
+            Console.WriteLine("Promjenjiva = {0}", prom);
+            DupliranaVred(ref prom);
+            Console.WriteLine("Promjenjiva = {0}", prom);
+        }
+    }
+}
