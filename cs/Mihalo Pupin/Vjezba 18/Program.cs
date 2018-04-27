af1c32dcfa96ea079fc735947e3b154cc92933d0
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 22:31:11 +0200

    delegat

---
+++ cs/Mihalo Pupin/Vjezba 18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Timers;
+
+namespace Vjezba_18
+{
+    class Program
+    {
+        static int brojac = 0;
+
+        static string displayString = "String poslije zavrsetka prikaza teksta, a prekid polije 100ms.";
+
+        static void Main(string[] args)
+        {
+            Timer mojTimer = new Timer(100);
+            mojTimer.Elapsed += new ElapsedEventHandler(WriteChar);
+            mojTimer.Start();
+            Console.ReadLine();
+        }
+
+        static void WriteChar(object source, ElapsedEventArgs e)
+        {
+            Console.Write(displayString[brojac++ % displayString.Length]);
+        }
+    }
+}
