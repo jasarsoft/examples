265d22c7a58e61bb8377eaf958690ff368084ce8
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 19:17:09 +0200

    opseg vazanja parametra

---
+++ cs/Mihalo Pupin/Vjezba 12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_12
+{
+    class Program
+    {
+        static void Write()
+        {
+            Console.WriteLine("mojString = {0}", mojString);
+        }
+
+        static void Main(string[] args)
+        {
+            string mojString = "String koji je definisan u f-ju Main()";
+            Write();
+        }
+    }
+}
