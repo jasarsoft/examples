5eaca9f4ae0f2db5f14e2387ff0c900192ac86ca
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 18:58:04 +0200

    primjer sa petljama

---
+++ cs/Mihalo Pupin/Vjezba 08/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double realKoord, imagKoord;
+            double realPom, imagPom, realPom2, arg;
+            int iteracija;
+
+            for(imagKoord = 1.2; imagKoord >= -1.2; imagKoord -= 0.05)
+            {
+                for(realKoord = -0.6; realKoord <= 1.77; realKoord += 0.03)
+                {
+                    iteracija = 0;
+                    realPom = realKoord;
+                    imagPom = imagKoord;
+                    arg = (realKoord * realKoord) + (imagKoord * imagKoord);
+
+                    while((arg < 4) && (iteracija < 40))
+                    {
+                        realPom2 = (realPom * realPom) - (imagPom * imagPom) - realKoord;
+                        imagPom = (2 * realPom * imagPom) - imagKoord;
+                        realPom = realPom2;
+                        arg = (realPom * realPom) + (imagPom * imagPom);
+                        iteracija += 1;
+                    }
+
+                    switch(iteracija % 4)
+                    {
+                        case 0:
+                            Console.Write(".");
+                            break;
+                        case 1:
+                            Console.Write("o");
+                            break;
+                        case 2:
+                            Console.Write("O");
+                            break;
+                        case 3:
+                            Console.Write("@");
+                            break;
+                    }
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
