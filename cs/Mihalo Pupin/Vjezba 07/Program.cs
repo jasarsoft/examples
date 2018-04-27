9ddf11be13715cbc81b30f0a0537093d26cd4e5c
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 18:29:40 +0200

    while petlja

---
+++ cs/Mihalo Pupin/Vjezba 07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balans, intRata, ciljaniBalans;
+
+            Console.Write("Koja je tvoja trenutna balans cijena?");
+            balans = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Koja je tvoja godisnja kamatna stopa (u %)?");
+            intRata = 1 + Convert.ToDouble(Console.ReadLine()) / 100;
+            Console.Write("Koji balans bih zelio da dobijes?");
+            ciljaniBalans = Convert.ToDouble(Console.ReadLine());
+
+            int brojGodina = 0;
+            while (balans < ciljaniBalans)
+            {
+                balans *= intRata;
+                ++brojGodina;
+            }
+
+            Console.WriteLine("Za {0} godin{1} imate balans od {2}.", brojGodina, brojGodina == 1 ? "u" : "a", balans);
+        }
+    }
+}
