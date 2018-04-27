c867259092e93d3327aeda5b7b414454f614dc5e
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 19:09:50 +0200

    fukcija sa parametrima

---
+++ cs/Mihalo Pupin/Vjezba 11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_11
+{
+    class Program
+    {
+        static int sumProm(params int[] vals)
+        {
+            int sum = 0;
+            foreach(int prom in vals)
+            {
+                sum += prom;
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int sum = sumProm(1, 5, 2, 9, 8);
+            Console.WriteLine("Sumirana vrijednost je = {0}", sum);
+        }
+    }
+}
