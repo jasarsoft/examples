cd2a2488554384c25abac236ba3358c687fb074e
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 19:30:43 +0200

    podrucje vazenja promjenjive

---
+++ cs/Mihalo Pupin/Vjezba 14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string text = " ";
+            for(i = 0; i < 10; i++)
+            {
+                text = "Linija " + Convert.ToString(i);
+                Console.WriteLine("{0}", text);
+            }
+            Console.WriteLine("Posljednji text definisan u petlji: {0}", text);
+        }
+    }
+}
