38b745d2b94472ff2a825da80c007c9dae207bb1
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 19:24:22 +0200

    parametar vazenja

---
+++ cs/Mihalo Pupin/Vjezba 13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_13
+{
+    class Program
+    {
+        static void Write()
+        {
+            string mojString = "String definisan u Write()";
+            Console.WriteLine("Sada je u Write()");
+            Console.WriteLine("mojString = {0}", mojString);
+        }
+
+        static void Main(string[] args)
+        {
+            string mojString = "String definisan u Main()";
+            Write();
+            Console.WriteLine("\nSada je u Main()");
+            Console.Write("mojString = {0}", mojString);
+        }
+    }
+}
