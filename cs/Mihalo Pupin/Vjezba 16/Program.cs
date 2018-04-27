ad7d179c3b0d224ecb8e24ea5ddd417d400375b4
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 19:43:58 +0200

    argumenit aplikacije

---
+++ cs/Mihalo Pupin/Vjezba 16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} argumenta komande linije:", args.Length);
+            foreach (string arg in args)
+                Console.Write(arg);
+        }
+    }
+}
