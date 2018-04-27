87f35a5d37b4f63671140a13e62fbfa8ccd1d19d
Edin Jašarević <edinjasar14@gmail.com>
2018-04-27 18:59:10 +0200

    primjer fukcije

---
+++ cs/Mihalo Pupin/Vjezba 09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vjezba_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+        }
+
+        static void Write()
+        {
+            Console.WriteLine("Tekst izlaz iz funkcije!");
+        }
+    }
+}
