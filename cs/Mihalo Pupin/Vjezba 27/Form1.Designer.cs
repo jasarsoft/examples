5ff24ad504061e674d81a9a417f359efb80a17d9
Edin Jašarević <edinjasar14@gmail.com>
2018-04-29 23:35:45 +0200

    listview control

---
+++ cs/Mihalo Pupin/Vjezba 27/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Vjezba_27
+{
+    partial class fclsMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fclsMain));
+            this.lblTrenutnaPutanja = new System.Windows.Forms.Label();
+            this.btnNazad = new System.Windows.Forms.Button();
+            this.groPregledMod = new System.Windows.Forms.GroupBox();
+            this.rdoLargeIcon = new System.Windows.Forms.RadioButton();
+            this.rdoSmallIcon = new System.Windows.Forms.RadioButton();
+            this.rdoList = new System.Windows.Forms.RadioButton();
+            this.rdoDetails = new System.Windows.Forms.RadioButton();
+            this.ilMaleIkone = new System.Windows.Forms.ImageList(this.components);
+            this.ilVelikeIkone = new System.Windows.Forms.ImageList(this.components);
+            this.lwFajloviFolder = new System.Windows.Forms.ListView();
+            this.groPregledMod.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTrenutnaPutanja
+            // 
+            this.lblTrenutnaPutanja.Location = new System.Drawing.Point(16, 8);
+            this.lblTrenutnaPutanja.Name = "lblTrenutnaPutanja";
+            this.lblTrenutnaPutanja.Size = new System.Drawing.Size(528, 16);
+            this.lblTrenutnaPutanja.TabIndex = 0;
+            // 
+            // btnNazad
+            // 
+            this.btnNazad.Location = new System.Drawing.Point(240, 256);
+            this.btnNazad.Name = "btnNazad";
+            this.btnNazad.Size = new System.Drawing.Size(75, 23);
+            this.btnNazad.TabIndex = 1;
+            this.btnNazad.Text = "Nazad";
+            this.btnNazad.UseVisualStyleBackColor = true;
+            this.btnNazad.Click += new System.EventHandler(this.btnNazad_Click);
+            // 
+            // groPregledMod
+            // 
+            this.groPregledMod.Controls.Add(this.rdoDetails);
+            this.groPregledMod.Controls.Add(this.rdoList);
+            this.groPregledMod.Controls.Add(this.rdoSmallIcon);
+            this.groPregledMod.Controls.Add(this.rdoLargeIcon);
+            this.groPregledMod.Location = new System.Drawing.Point(424, 32);
+            this.groPregledMod.Name = "groPregledMod";
+            this.groPregledMod.Size = new System.Drawing.Size(120, 128);
+            this.groPregledMod.TabIndex = 2;
+            this.groPregledMod.TabStop = false;
+            this.groPregledMod.Text = "Pregled Mod";
+            // 
+            // rdoLargeIcon
+            // 
+            this.rdoLargeIcon.AutoSize = true;
+            this.rdoLargeIcon.Location = new System.Drawing.Point(8, 24);
+            this.rdoLargeIcon.Name = "rdoLargeIcon";
+            this.rdoLargeIcon.Size = new System.Drawing.Size(83, 17);
+            this.rdoLargeIcon.TabIndex = 0;
+            this.rdoLargeIcon.TabStop = true;
+            this.rdoLargeIcon.Text = "Velike ikone";
+            this.rdoLargeIcon.UseVisualStyleBackColor = true;
+            this.rdoLargeIcon.CheckedChanged += new System.EventHandler(this.rdoLargeIcon_CheckedChanged);
+            // 
+            // rdoSmallIcon
+            // 
+            this.rdoSmallIcon.AutoSize = true;
+            this.rdoSmallIcon.Location = new System.Drawing.Point(8, 48);
+            this.rdoSmallIcon.Name = "rdoSmallIcon";
+            this.rdoSmallIcon.Size = new System.Drawing.Size(77, 17);
+            this.rdoSmallIcon.TabIndex = 1;
+            this.rdoSmallIcon.TabStop = true;
+            this.rdoSmallIcon.Text = "Male ikone";
+            this.rdoSmallIcon.UseVisualStyleBackColor = true;
+            this.rdoSmallIcon.CheckedChanged += new System.EventHandler(this.rdoSmallIcon_CheckedChanged);
+            // 
+            // rdoList
+            // 
+            this.rdoList.AutoSize = true;
+            this.rdoList.Location = new System.Drawing.Point(8, 72);
+            this.rdoList.Name = "rdoList";
+            this.rdoList.Size = new System.Drawing.Size(79, 17);
+            this.rdoList.TabIndex = 2;
+            this.rdoList.TabStop = true;
+            this.rdoList.Text = "List pregled";
+            this.rdoList.UseVisualStyleBackColor = true;
+            this.rdoList.CheckedChanged += new System.EventHandler(this.rdoList_CheckedChanged);
+            // 
+            // rdoDetails
+            // 
+            this.rdoDetails.AutoSize = true;
+            this.rdoDetails.Location = new System.Drawing.Point(8, 96);
+            this.rdoDetails.Name = "rdoDetails";
+            this.rdoDetails.Size = new System.Drawing.Size(90, 17);
+            this.rdoDetails.TabIndex = 3;
+            this.rdoDetails.TabStop = true;
+            this.rdoDetails.Text = "Detalj pregled";
+            this.rdoDetails.UseVisualStyleBackColor = true;
+            this.rdoDetails.CheckedChanged += new System.EventHandler(this.rdoDetails_CheckedChanged);
+            // 
+            // ilMaleIkone
+            // 
+            this.ilMaleIkone.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilMaleIkone.ImageStream")));
+            this.ilMaleIkone.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilMaleIkone.Images.SetKeyName(0, "folder.ico");
+            this.ilMaleIkone.Images.SetKeyName(1, "file.ico");
+            // 
+            // ilVelikeIkone
+            // 
+            this.ilVelikeIkone.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilVelikeIkone.ImageStream")));
+            this.ilVelikeIkone.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilVelikeIkone.Images.SetKeyName(0, "folder.ico");
+            this.ilVelikeIkone.Images.SetKeyName(1, "file.ico");
+            // 
+            // lwFajloviFolder
+            // 
+            this.lwFajloviFolder.LargeImageList = this.ilVelikeIkone;
+            this.lwFajloviFolder.Location = new System.Drawing.Point(12, 12);
+            this.lwFajloviFolder.Name = "lwFajloviFolder";
+            this.lwFajloviFolder.Size = new System.Drawing.Size(406, 238);
+            this.lwFajloviFolder.SmallImageList = this.ilMaleIkone;
+            this.lwFajloviFolder.TabIndex = 3;
+            this.lwFajloviFolder.UseCompatibleStateImageBehavior = false;
+            this.lwFajloviFolder.View = System.Windows.Forms.View.List;
+            this.lwFajloviFolder.ItemActivate += new System.EventHandler(this.lwFajloviFolder_ItemActivate);
+            // 
+            // fclsMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 287);
+            this.Controls.Add(this.lwFajloviFolder);
+            this.Controls.Add(this.groPregledMod);
+            this.Controls.Add(this.btnNazad);
+            this.Controls.Add(this.lblTrenutnaPutanja);
+            this.Name = "fclsMain";
+            this.Text = "Kontrola Listview";
+            this.groPregledMod.ResumeLayout(false);
+            this.groPregledMod.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTrenutnaPutanja;
+        private System.Windows.Forms.Button btnNazad;
+        private System.Windows.Forms.GroupBox groPregledMod;
+        private System.Windows.Forms.RadioButton rdoDetails;
+        private System.Windows.Forms.RadioButton rdoList;
+        private System.Windows.Forms.RadioButton rdoSmallIcon;
+        private System.Windows.Forms.RadioButton rdoLargeIcon;
+        private System.Windows.Forms.ImageList ilMaleIkone;
+        private System.Windows.Forms.ImageList ilVelikeIkone;
+        private System.Windows.Forms.ListView lwFajloviFolder;
+    }
+}
+
---
+++ cs/Mihalo Pupin/Vjezba 27/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Vjezba_27
+{
+    public partial class fclsMain : Form
+    {
+        //polje koje ce sadrzavati predhodne foldere
+        private System.Collections.Specialized.StringCollection folderCol;
+
+        public fclsMain()
+        {
+            InitializeComponent();
+
+            //inicijalizujemo ListView i kolekciju foldera
+            folderCol = new System.Collections.Specialized.StringCollection();
+            KreirajHeaderlistView();
+            FilujListView(@"C:\");
+            folderCol.Add(@"C:\");
+        }
+
+        private void KreirajHeaderlistView()
+        {
+            ColumnHeader colHeader;
+
+            //prvo zaglavlje
+            colHeader = new ColumnHeader();
+            colHeader.Text = "Naziv fajla";
+            this.lwFajloviFolder.Columns.Add(colHeader); //ubacijemo zaglavlje
+
+            colHeader = new ColumnHeader();
+            colHeader.Text = "Velicina fajla";
+            this.lwFajloviFolder.Columns.Add(colHeader);
+
+            colHeader = new ColumnHeader();
+            colHeader.Text = "Posljenji prstup";
+            this.lwFajloviFolder.Columns.Add(colHeader);
+        }
+
+        private void FilujListView(string root)
+        {
+            try
+            {
+                //dve lokalne promjenljive za kreiranje elemenata za ubacivanje
+                ListViewItem lvi;
+                ListViewItem.ListViewSubItem lvsi;
+
+                //ako nema korenog folder nista ne moze ubaciti
+                if (root.CompareTo("") == 0)
+                    return;
+
+                //uzimamo informacije o koren folderu
+                System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(root);
+                //citamo fajlove i folder iz korenskog folder
+                DirectoryInfo[] dirs = dir.GetDirectories(); //folderi
+                FileInfo[] files = dir.GetFiles(); //fajlovi
+
+                //brisemo ListView. Zovemo metodu Clear kolecije Items, a ne samoe kontrole ListView.
+                //Metoda Clear kontrole ListView brise sve ukljucujuci i zaglavlja kolona, a mi hocmeo
+                //da izbrisemo samo elemente iz prikaza
+                this.lwFajloviFolder.Items.Clear();
+
+                //podesimo oznaku na trenutnu putanju
+                this.lblTrenutnaPutanja.Text = root;
+
+                //stopiramo oznaku na trenutnu putanju
+                this.lwFajloviFolder.BeginUpdate();
+
+
+                //prodjimo kroz folder korenskog foldera i ubacimo ih
+                foreach(DirectoryInfo di in dirs)
+                {
+                    //kreiramo glavni ListViewItem
+                    lvi = new ListViewItem();
+                    lvi.Text = di.Name; //ime foldera
+                    lvi.ImageIndex = 0; //index ikone foldera je 0
+                    lvi.Tag = di.FullName; //podesimo tag na putanju foldera
+
+                    //kreiramo dva podelementa
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = ""; //folder nema velicinu i zato je ova kolona prazna
+                    lvi.SubItems.Add(lvsi); //dodajemo podelemente u ListViewItem
+
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = di.LastAccessTime.ToString(); //posljenja kolona koji je pristup
+                    lvi.SubItems.Add(lvsi); //dodajemo podelemente u ListViewItem
+
+                    this.lwFajloviFolder.Items.Add(lvi);
+                }
+
+                //prolazimo kroz sve fajlove korenskog foldera
+                foreach(FileInfo fi in files)
+                {
+                    //kreiramo glavni ListViewItem
+                    lvi = new ListViewItem();
+                    lvi.Text = fi.Name; //ime fajla
+                    lvi.ImageIndex = 1; //ikona kojom prikazujemo folder ima index 1
+                    lvi.Tag = fi.FullName; //podesimo tag na putanju foldera
+
+                    //kreiramo dva podelementa
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = fi.Length.ToString(); //velicina fajla
+                    lvi.SubItems.Add(lvsi); //dodajemo podelemente kolkciji SubItems
+
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = fi.LastAccessTime.ToString(); //posljenja kolona koji je pristup
+                    lvi.SubItems.Add(lvsi); //dodajemo podelemente u ListViewItem
+
+                    this.lwFajloviFolder.Items.Add(lvi);
+                }
+
+                //omogucimo prikaz novi elemenata;
+                //sada se prikazuju novi elementi
+                this.lwFajloviFolder.EndUpdate();
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Error: " + er.Message);
+            }
+        }
+
+        private void lwFajloviFolder_ItemActivate(object sender, EventArgs e)
+        {
+            ///konvertujemo parametar sender u tip ListView i citamo svojstvo tag za priv izabrani elemente
+            ListView lw = (ListView)sender;
+            string filename = lw.SelectedItems[0].Tag.ToString();
+
+            if(lw.SelectedItems[0].ImageIndex != 0)
+            {
+                try
+                {
+                    //pkusamo da pokreno fajl
+                    System.Diagnostics.Process.Start(filename);
+                }
+                catch (Exception)
+                {
+                    //ako poksaj nije uspjeo napustamo metodu
+                    return;
+                }
+            }
+            else
+            {
+                //ubacijuemo elemnte
+                FilujListView(filename);
+                folderCol.Add(filename);
+            }
+        }
+
+        private void btnNazad_Click(object sender, EventArgs e)
+        {
+            if(folderCol.Count > 1)
+            {
+                FilujListView(folderCol[folderCol.Count - 2].ToString());
+                folderCol.RemoveAt(folderCol.Count - 1);
+            }
+            else
+            {
+                FilujListView(folderCol[0].ToString());
+            }
+        }
+
+        private void rdoLargeIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                this.lwFajloviFolder.View = View.LargeIcon;
+        }
+
+        private void rdoSmallIcon_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                this.lwFajloviFolder.View = View.SmallIcon;
+        }
+
+        private void rdoList_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                this.lwFajloviFolder.View = View.List;
+        }
+
+        private void rdoDetails_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                this.lwFajloviFolder.View = View.Details;
+        }
+    }
+}
