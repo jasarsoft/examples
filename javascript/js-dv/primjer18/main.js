d25d6c3bfa5453a673b6636639b2ece9da03c1b2
Edin Jašarević <edinjasar14@gmail.com>
2018-05-01 19:28:42 +0200

    galerija

---
+++ javascript/js-dv/primjer18/main.js
@@ -0,0 +1,11 @@
+var current = document.getElementsById('current');
+var slike = document.getElementsByClassName('thumb');
+
+for(var i = 0; i < slike.length; i++){
+    slike[i].addEventListener('click', display);
+}
+
+function display(){
+    var sl = this.getAttribute('src');
+    current.setAttribute('src', sl);
+}
