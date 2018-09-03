94bbf2a3d8eb58602067963515764e11dd0d0361
Edin Jašarević <edinjasar14@gmail.com>
2018-09-03 23:47:07 +0200

    js-kpk pog17 | operatori

---
+++ cs/js_kpk/Poglavlje17/PrimjerSat/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimjerSat
+{
+    class Program
+    {
+        struct Sat
+        {
+            private int vrijednost;
+
+            public Sat(int pocetnaVrijednost)
+            {
+                vrijednost = pocetnaVrijednost;
+            }
+
+            public static Sat operator+ (Sat lop, Sat dop)
+            {
+                return new Sat(lop.vrijednost + dop.vrijednost);
+            }
+
+            public static Sat operator+ (Sat lop, int dop)
+            {
+                return lop + new Sat(dop);
+            }
+
+            public static Sat operator+ (int lop, Sat dop)
+            {
+                return new Sat(lop) + dop;
+            }
+
+            public static Sat operator++ (Sat arg)
+            {
+                arg.vrijednost++;
+                return arg;
+            }
+
+            public static bool operator== (Sat lop, Sat dop)
+            {
+                return lop.vrijednost == dop.vrijednost;
+            }
+
+            public static bool operator!=(Sat lop, Sat dop)
+            {
+                return lop.vrijednost != dop.vrijednost;
+            }
+
+            public static implicit operator int (Sat iz)
+            {
+                return iz.vrijednost;
+            }
+        }
+
+        public static void Metod(int parametar)
+        {
+            Console.WriteLine(parametar);
+        }
+
+        static void Main(string[] args)
+        {
+            Sat rucak = new Sat(12);
+            Program.Metod(rucak); //implicitna konverzija iz Sat u int
+            Program.Metod((int)rucak); //eksplicitna konverzija Sat u int
+        }
+    }
+}
