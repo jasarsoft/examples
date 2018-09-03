ae3e65e6643e6a34c583b096d1da1c7781aa17ab
Edin Jašarević <edinjasar14@gmail.com>
2018-09-03 23:45:20 +0200

    js-kpk pog11 | util class (min/max)

---
+++ cs/js_kpk/Poglavlje11/PrimjerUtil/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrimjerUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = Util.Min(4, 9);
+            Console.WriteLine(min);
+
+            min = Util.Min(7, 5, 10);
+            Console.WriteLine(min);
+        }
+    }
+}
---
+++ cs/js_kpk/Poglavlje11/PrimjerUtil/Util.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrimjerUtil
+{
+    class Util
+    {
+        public static int Min(params int[] niz)
+        {
+            if (niz == null || niz.Length == 0)
+            {
+                throw new ArgumentException("Min argument");
+            }
+
+            int trenutniMin = niz[0];
+            foreach (int i in niz)
+            {
+                trenutniMin = Min(trenutniMin, i);
+            }
+
+            return trenutniMin;
+        }
+
+        public static int Min(int lhs, int rhs)
+        {
+            return lhs < rhs ? lhs : rhs;
+        }
+    }
+}
