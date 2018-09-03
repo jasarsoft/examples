f1f979c204f1c86e96122839360201442dc9f82a
Edin Jašarević <edinjasar14@gmail.com>
2018-09-03 23:43:31 +0200

    js-kpk pog10 | stack list

---
+++ cs/js_kpk/Poglavlje10/PrimjerStack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace PrimjerStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack brojevi = new Stack();
+
+            //popuniti stek
+            foreach (int broj in new int[] { 9, 3, 7, 2})
+            {
+                brojevi.Push(broj);
+                Console.WriteLine(broj + " je stavljen na stek");
+            }
+            //proci kroz stek
+            foreach (int broj in brojevi)
+            {
+                Console.WriteLine(broj);
+            }
+            //isprazni stek
+            while (brojevi.Count != 0)
+            {
+                int broj = (int)brojevi.Pop();
+                Console.WriteLine(broj + " je uklonjen sa steka");
+            }
+        }
+    }
+}
