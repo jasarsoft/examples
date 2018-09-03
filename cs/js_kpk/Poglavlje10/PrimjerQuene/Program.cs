4c6499ccce32829bd239de83d32e9267eec3292a
Edin Jašarević <edinjasar14@gmail.com>
2018-09-03 23:42:50 +0200

    js-kpk pog10 | quene array

---
+++ cs/js_kpk/Poglavlje10/PrimjerQuene/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+
+namespace PrimjerQuene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue brojevi = new Queue();
+
+            //popuniti red za cekanje
+            foreach(int broj in new int[] { 9, 3, 7, 3})
+            {
+                brojevi.Enqueue(broj);
+                Console.WriteLine(broj + " je dodat u red");
+            }
+            //proci kroz red
+            foreach (int broj in brojevi)
+            {
+                Console.WriteLine(broj);
+            }
+            //isprazniti red
+            while(brojevi.Count != 0)
+            {
+                int broj = (int)brojevi.Dequeue();
+                Console.WriteLine(broj + " je izbacen iz reda");
+            }
+        }
+    }
+}
