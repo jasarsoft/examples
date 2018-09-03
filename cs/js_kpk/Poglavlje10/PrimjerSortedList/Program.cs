138974397bba384934433d275bb4903906f6b527
Edin Jašarević <edinjasar14@gmail.com>
2018-09-03 23:43:14 +0200

    js-kpk pog10 | sorted list

---
+++ cs/js_kpk/Poglavlje10/PrimjerSortedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace PrimjerSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList map = new SortedList();
+
+            //popuniti sortiranu listu
+            string[] kljucevi = { "ovo", "su", "kljucevi" };
+            for (int i = 0; i != kljucevi.Length; i++)
+            {
+                string kljuc = kljucevi[i];
+                int vrijednost = i;
+                map[kljuc] = vrijednost; //dodaje i kljuc i vrijednost
+            }
+            //proci kroz listu pomocu iskaza foreach
+            foreach (DictionaryEntry element in map)
+            {
+                string kljuc = (string)element.Key;
+                int vrijednost = (int)element.Value;
+                Console.WriteLine("Kljuc: {0}, Vrijednost: {1}", kljuc, vrijednost);
+            }
+        }
+    }
+}
