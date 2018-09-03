07f386c747907ee553fcf7eedce3d71130d28eab
Edin Jašarević <edinjasar14@gmail.com>
2018-09-03 23:42:35 +0200

    js-kpk pog10 | array list

---
+++ cs/js_kpk/Poglavlje10/PrimjerArrayList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace PrimjerArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList brojevi = new ArrayList();
+
+            //popunite niz tipa ArrayList
+            foreach (int broj in new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
+            {
+                brojevi.Add(broj);
+            }
+
+            //uklonite prvi element sa vrijednostu 7 (cetvrti element, index 5)
+            brojevi.Remove(7);
+            //uklonite element koji je sada sedmi, index 8
+            brojevi.RemoveAt(8);
+            //proci kroz preostali 10 elemenata iskazom for
+            for (int i = 0; i != brojevi.Count; i++)
+            {
+                int broj = (int)brojevi[i];
+                Console.WriteLine(broj);
+            }
+            //proci kroz preostali 10 brojeva pomocu iskaza foreach
+            foreach (int broj in brojevi)
+            {
+                Console.WriteLine(broj);
+            }
+        }
+    }
+}
