f714884acb0e5ec72d105077b3c574fc333cfcab
Edin Jašarević <edinjasar14@gmail.com>
2018-10-03 15:15:59 +0200

    js_kpk pog23 | login control

---
+++ cs/js_kpk/Poglavlje23/LoginControl/Login.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace LoginControl
+{
+    partial class Login
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.userNameText = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.loginError = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.loginError)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.Location = new System.Drawing.Point(32, 48);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(100, 23);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "user name label";
+            this.userNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Location = new System.Drawing.Point(32, 128);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(100, 23);
+            this.passwordLabel.TabIndex = 1;
+            this.passwordLabel.Text = "pasword label";
+            this.passwordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // passwordText
+            // 
+            this.passwordText.Location = new System.Drawing.Point(140, 128);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.PasswordChar = '*';
+            this.passwordText.Size = new System.Drawing.Size(220, 20);
+            this.passwordText.TabIndex = 2;
+            this.passwordText.Text = "password";
+            // 
+            // userNameText
+            // 
+            this.userNameText.Location = new System.Drawing.Point(140, 48);
+            this.userNameText.Name = "userNameText";
+            this.userNameText.Size = new System.Drawing.Size(220, 20);
+            this.userNameText.TabIndex = 3;
+            this.userNameText.Text = "user name";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(150, 220);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(100, 36);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginClick);
+            // 
+            // loginError
+            // 
+            this.loginError.ContainerControl = this;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.userNameText);
+            this.Controls.Add(this.passwordText);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.Name = "Login";
+            this.Size = new System.Drawing.Size(400, 300);
+            ((System.ComponentModel.ISupportInitialize)(this.loginError)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.TextBox userNameText;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.ErrorProvider loginError;
+    }
+}
---
+++ cs/js_kpk/Poglavlje23/LoginControl/Login.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoginControl
+{
+    public partial class Login : UserControl
+    {
+        public event EventHandler loginSuccess;
+        public event EventHandler loginFail;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        public string UserNameLabel
+        {
+            get { return userNameLabel.Text; }
+            set { userNameLabel.Text = value; }
+        }
+
+        public string PasswordLabel
+        {
+            get { return passwordLabel.Text; }
+            set { passwordLabel.Text = value; }
+        }
+
+        public string LoginBottonText
+        {
+            get { return loginButton.Text; }
+            set { loginButton.Text = value; }
+        }
+
+        [Browsable(false)]
+        public string UserName
+        {
+            set { userNameText.Text = value; }
+        }
+
+        [Browsable(false)] //nece se prikazivatu u prozoru properties
+        public string Password
+        {
+            set { passwordText.Text = value; }
+        }
+
+        private void loginClick(object sender, EventArgs e)
+        {
+            if(userNameText.Text.Length == 0)
+            {
+                loginError.SetError(userNameText, "Please enter a user name");
+                return;
+            }
+            if(passwordText.Text.Length == 0)
+            {
+                loginError.SetError(passwordText, "Please enter apassword");
+                return;
+            }
+            if(UserNameAndPasswordAreValid(userNameText.Text, passwordText.Text))
+            {
+                if (loginSuccess != null)
+                    loginSuccess(this, new EventArgs());
+            }
+            else
+            {
+                if (loginFail != null)
+                    loginFail(this, new EventArgs());
+            }
+        }
+
+        private bool UserNameAndPasswordAreValid(string userName, string password)
+        {
+            return password.Equals("TrustMe");
+        }
+    }
+}
