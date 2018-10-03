041afa8e94fc3ee31f7aa40b6a9e1aa1d48d1aa4
Edin Jašarević <edinjasar14@gmail.com>
2018-10-03 15:16:27 +0200

    js_kpk pog23 | subclassed control

---
+++ cs/js_kpk/Poglavlje23/SubclassedControl/ToogleButton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SubclassedControl
+{
+    public class ToogleButton : CheckBox
+    {
+        private string checkedText;
+        private string uncheckedText;
+        private Color checkedColor;
+        private Color uncheckedColor;
+
+        public ToogleButton()
+        {
+            this.Appearance = Appearance.Button;
+            this.checkedText = "Checked";
+            this.uncheckedText = "Unchecked";
+            this.checkedColor = Color.Gray;
+            this.uncheckedColor = this.BackColor;
+        }
+
+        public string CheckedText
+        {
+            get { return checkedText; }
+            set { checkedText = value; }
+        }
+
+        public string UncheckedText
+        {
+            get { return uncheckedText; }
+            set { checkedText = value; }
+        }
+
+        public Color CheckedColor
+        {
+            get { return checkedColor; }
+            set { checkedColor = value; }
+        }
+
+        public Color UncheckedColor
+        {
+            get { return uncheckedColor; }
+            set { uncheckedColor = value; }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            if(this.Checked)
+            {
+                this.Text = this.checkedText;
+                this.BackColor = this.checkedColor;
+            }
+            else
+            {
+                this.Text = this.uncheckedText;
+                this.BackColor = this.uncheckedColor;
+            }
+        }
+    }
+}
