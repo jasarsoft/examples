0b5fc93494e05077c722d6af89612fb0e771e32c
Edin Jašarević <edinjasar14@gmail.com>
2018-10-05 00:51:12 +0200

    us-ojp v03 pr01 | tipovi

---
+++ java/us-ojp/video03/Primjer01/src/jasarsoft/Primjer01.java
@@ -0,0 +1,26 @@
+
+package jasarsoft;
+
+public class Primjer01 {
+
+    public static void main(String[] args) {
+        int godinaRodjenjaStudenta;
+        int godinaRodjenjaProfesora = 1981;
+        
+        godinaRodjenjaStudenta = 1988;
+        
+        double pi = 3.1415;
+        
+        boolean daLiJeStudentPolozioIspit = true;
+        
+        char slovo = 'M';
+        
+        String imeStudenta = "Pera";
+        String prezimeStudenta;
+        
+        prezimeStudenta = "Peric";
+        
+        String b = "5";
+    }
+    
+}
