91b6908b9581235d46e137503ff5df2d68a3c96e
Edin Jašarević <edinjasar14@gmail.com>
2018-10-05 00:53:04 +0200

    us-ojp v03 pr03 | numericke operacije

---
+++ java/us-ojp/video03/Primjer03/src/jasarsoft/Primjer03.java
@@ -0,0 +1,28 @@
+
+package jasarsoft;
+
+public class Primjer03 {
+
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 3;
+        int c;
+        
+        c = a * b;
+        System.out.println(c);
+        
+        c = a * a * 4;
+        System.out.println(c);
+        
+        c = a / b;
+        System.out.println(c);
+        
+        double d = a /b;
+        System.out.println(d);
+        
+        d = 1.0 * a /b;
+        System.out.println(d);
+    }
+    
+}
