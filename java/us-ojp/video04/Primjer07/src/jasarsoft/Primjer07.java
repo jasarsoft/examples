8e5dd2d92fff7630b07fc08be68c36640a67474c
Edin Jašarević <edinjasar14@gmail.com>
2018-10-06 16:44:53 +0200

    us-ojp v04 pr07 | skraceni oblik op na var
    This reverts commit f318932862d27da3ac33a11c0d71b0f9eff9b6d2.

---
+++ java/us-ojp/video04/Primjer07/src/jasarsoft/Primjer07.java
@@ -0,0 +1,14 @@
+
+package jasarsoft;
+
+public class Primjer07 {
+
+    public static void main(String[] args) {
+        
+        int x = 777;
+        
+        x %= 10;
+        
+        System.out.println("Ostatak pri djeljenju broja 777 je 10 je " + x);
+    }
+}
