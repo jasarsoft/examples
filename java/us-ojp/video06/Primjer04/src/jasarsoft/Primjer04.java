3071c3d1409d0332412c72518fa9c92ccb710e42
Edin Jašarević <edinjasar14@gmail.com>
2018-10-06 17:02:22 +0200

    us-ojp v06 pr04 | for (inkrement)

---
+++ java/us-ojp/video06/Primjer04/src/jasarsoft/Primjer04.java
@@ -0,0 +1,17 @@
+
+package jasarsoft;
+
+import java.util.Scanner;
+
+public class Primjer04 {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        for(int i = 1; i < 100; i *= 2){
+            System.out.println("Broja i je " + i);
+        }
+    }
+    
+}
