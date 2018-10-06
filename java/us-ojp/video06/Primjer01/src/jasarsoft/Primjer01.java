a01834331d01214fbf18d15fbe95f3a2c362ed7d
Edin Jašarević <edinjasar14@gmail.com>
2018-10-06 17:00:24 +0200

    us-ojp v06 pr01 | for

---
+++ java/us-ojp/video06/Primjer01/src/jasarsoft/Primjer01.java
@@ -0,0 +1,21 @@
+
+package jasarsoft;
+
+import java.util.Scanner;
+
+public class Primjer01 {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        for(int i = 0; i < 10; i++){
+            System.out.println("Broja i je " + i);
+        }
+        
+        System.out.println("Kraj petlje");
+        
+        System.out.println("Kraj programa!");
+    }
+    
+}
