7f9adb9391a051fd81cdd247bf7b49e80d78322c
Edin Jašarević <edinjasar14@gmail.com>
2018-10-06 16:46:32 +0200

    us-ojp v04 pr08 | inkrement/dekrement

---
+++ java/us-ojp/video04/Primjer08/src/jasarsoft/Primjer08.java
@@ -0,0 +1,28 @@
+
+package jasarsoft;
+
+
+public class Primjer08 {
+
+    public static void main(String[] args) {
+        
+        int x = 777;
+        
+        ++x;
+        ++x;
+        
+        --x;
+        
+        System.out.println(x);
+        
+        
+        x = 1;
+        System.out.println(x++);
+        System.out.println(x);
+        
+        x = 1;
+        System.out.println(++x);
+        System.out.println(x);
+    }
+    
+}
