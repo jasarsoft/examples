d28913e6c1869134b201eb842a514836e5093e37
Edin Jašarević <edinjasar14@gmail.com>
2018-10-06 16:39:51 +0200

    us-ojp v04 pr06 | konstanta

---
+++ java/us-ojp/video04/Primjer06/src/jasarsoft/Primjer06.java
@@ -0,0 +1,15 @@
+
+package jasarsoft;
+
+
+public class Primjer06 {
+
+    public static void main(String[] args) {
+        final double TE = 5.6;
+        
+        double x = 1.223;
+        double f = Math.pow(x, TE);
+        
+        System.out.println("Specijalni stepen je " + f);
+    }
+}
