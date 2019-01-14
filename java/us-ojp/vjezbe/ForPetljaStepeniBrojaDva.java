25a8cc414f6beb5c9997d88c362d69e56987c248
jasarsoft <edinjasar14@gmail.com>
2019-01-14 21:24:05 +0100

    java vjezbe | for 2

---
+++ java/us-ojp/vjezbe/ForPetljaStepeniBrojaDva.java
@@ -0,0 +1,12 @@
+
+public class ForPetljaStepeniBrojaDva {
+	
+	public static void main (String[] args) {
+		
+		int i, stepen2;
+		for (i = 0, stepen2 = 1; i < 10; i++, stepen2 *= 2)
+		System.out.println("2 na " + i + " stepen = " + stepen2);
+		
+	}
+
+}
