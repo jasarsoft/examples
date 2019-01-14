9e08f10a7e8be7cb09c9d99a5e061cfa8596f06c
jasarsoft <edinjasar14@gmail.com>
2019-01-14 21:23:53 +0100

    java vjezbe | for

---
+++ java/us-ojp/vjezbe/ForPetljaNumerickiKodKaraktera.java
@@ -0,0 +1,11 @@
+
+public class ForPetljaNumerickiKodKaraktera {
+	
+	public static void main (String[] args) {
+		for (int i = 65; i <= 90; i++) {
+			char c = (char)i;
+			System.out.println("i = " + i + " -> c = " + c);
+			}
+	}
+
+}
