59a3d8b6d5138da42135fabc10581e8a2943201d
jasarsoft <edinjasar14@gmail.com>
2019-01-14 21:24:30 +0100

    java vjezbe | math

---
+++ java/us-ojp/vjezbe/Koren.java
@@ -0,0 +1,19 @@
+
+public class Koren {
+	
+	public static void main(String[] args) {
+        int i = 2;
+        double r = Math.sqrt(i);
+        
+        System.out.print("Kvadratni koren od ");
+        System.out.print(i);
+        System.out.print(" je ");
+        System.out.print(r);
+        System.out.println(".");
+
+        i = 5;
+        r = Math.sqrt(i);
+        System.out.println("Kvadratni koren od " + i + " je " + r + ".");       
+    }
+
+}
